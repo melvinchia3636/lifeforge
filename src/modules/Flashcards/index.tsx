eeb542e56017327f9a27d1a28b7157f62790c331
melvinchia3636 <melvinchia623600@gmail.com>
2023-11-29 19:24:23 +0800

    more ui designs done
    Former-commit-id: 94a58c05d3536aa4c3a5d363b281279ea403a5dd [formerly c140c66c860d4cfee0650526be3571c4f9cadfdf] [formerly 97b4dc4c787ecf53f125311f8406f82a6e210dd8 [formerly 97059f48e501aba3a62924e236e7549c17fb39dd]]
    Former-commit-id: 09906bf5dc99ac96e5a48f1c6cbf6b67f20204f6 [formerly edea14eea5a7b98984bad1573fb45f0a5ebcd4ee]
    Former-commit-id: 8531f6daf6a544150d1efa57a4417df060bb9e5f

---
+++ src/modules/Flashcards/index.tsx
@@ -0,0 +1,90 @@
+import { faker } from '@faker-js/faker'
+import { Icon } from '@iconify/react/dist/iconify.js'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ModuleHeader from '../../components/ModuleHeader'
+
+export default function Flashcards(): React.JSX.Element {
+  return (
+    <section className="flex h-full min-h-0 w-full flex-1 flex-col px-12">
+      <ModuleHeader
+        title="Flashcards"
+        desc="Memorizing could be a pain, but not with flashcards."
+      />
+      <div className="mt-8 flex min-h-0 w-full flex-1 flex-col">
+        <search className="flex w-full items-center gap-4 rounded-lg bg-neutral-800/50 p-4">
+          <Icon icon="tabler:search" className="h-5 w-5 text-neutral-500" />
+          <input
+            type="text"
+            placeholder="Search flashcard sets ..."
+            className="w-full bg-transparent text-neutral-100 placeholder:text-neutral-500 focus:outline-none"
+          />
+        </search>
+        <div className="mt-6 grid w-full grid-cols-3 gap-6 overflow-y-auto pb-12">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => (
+              <Link
+                to={`/idea-box/${index}`}
+                key={index}
+                className="relative flex flex-col justify-start gap-6 rounded-lg bg-neutral-800/50 p-8 hover:bg-neutral-800"
+              >
+                <div className="flex flex-col gap-2">
+                  <p className="text-sm font-medium text-neutral-400">
+                    {faker.datatype.number(100)} cards
+                  </p>
+                  <div className="text-xl font-medium text-neutral-50">
+                    {faker.commerce.productName()}
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <progress
+                    className="progress progress-accent h-3 w-full"
+                    value={faker.datatype.number(100)}
+                    max="100"
+                  ></progress>
+                  <p className="text-sm font-medium text-neutral-400">
+                    {faker.datatype.number(100)}% complete
+                  </p>
+                </div>
+                <button className="absolute right-4 top-4 rounded-md p-2 text-neutral-500 hover:bg-neutral-700/30 hover:text-neutral-100">
+                  <Icon icon="tabler:dots-vertical" className="h-5 w-5" />
+                </button>
+              </Link>
+            ))}
+          {Array(4)
+            .fill(0)
+            .map((_, index) => (
+              <Link
+                to={`/idea-box/${index}`}
+                key={index}
+                className="relative flex flex-col justify-start gap-6 rounded-lg bg-neutral-800/50 p-8 hover:bg-neutral-800"
+              >
+                <div className="flex flex-col gap-2">
+                  <p className="text-sm font-medium text-neutral-400">
+                    {faker.datatype.number(100)} cards
+                  </p>
+                  <div className="text-xl font-medium text-neutral-50">
+                    {faker.commerce.productName()}
+                  </div>
+                </div>
+                <div className="flex flex-1 items-center justify-center gap-2 text-teal-500">
+                  <Icon icon="tabler:check" className="h-5 w-5" />
+                  <p className="font-medium">Done</p>
+                </div>
+                <button className="absolute right-4 top-4 rounded-md p-2 text-neutral-500 hover:bg-neutral-700/30 hover:text-neutral-100">
+                  <Icon icon="tabler:dots-vertical" className="h-5 w-5" />
+                </button>
+              </Link>
+            ))}
+          <div className="relative flex h-full flex-col items-center justify-center gap-6 rounded-lg border-2 border-dashed border-neutral-700 p-8">
+            <Icon icon="tabler:plus" className="h-8 w-8 text-neutral-500" />
+            <div className="text-xl font-semibold text-neutral-500">
+              Create new set
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
