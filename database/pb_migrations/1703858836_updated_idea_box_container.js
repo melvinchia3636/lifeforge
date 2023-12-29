b5e927b9655b7df63210272e51275eb280f4f863
melvinchia3636 <melvinchia623600@gmail.com>
2023-12-29 22:22:23 +0800

    linked idea box container page to real data
    Former-commit-id: cb556776cc52ef35a94a64f9ddec2128ae9990a3 [formerly 5eb2c3f9662346db166383aaf4bd4dcb8c7da07e] [formerly 2d00b945fc57f375eab166b911105a6e9a7ae414 [formerly e1d4dcfcf6250583064212c6cc50855402c86543]]
    Former-commit-id: d0beebb96ef07a1ad7f4adc739565178d6c1dda3 [formerly ed294bbee62ff33e4bbff3fa6282938d137b5ac4]
    Former-commit-id: 90815ddd896490ebb7fca331c61a2e8d5d6145b7

---
+++ database/pb_migrations/1703858836_updated_idea_box_container.js
@@ -0,0 +1,165 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("10yi8esudwbgz4n")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "4yyyjcvr",
+    "name": "text_count",
+    "type": "number",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5cdkfbzs",
+    "name": "image_count",
+    "type": "number",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vm4wexsu",
+    "name": "link_count",
+    "type": "number",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "raujcrkq",
+    "name": "icon",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "hvuukigw",
+    "name": "color",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "spxeebso",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("10yi8esudwbgz4n")
+
+  // remove
+  collection.schema.removeField("4yyyjcvr")
+
+  // remove
+  collection.schema.removeField("5cdkfbzs")
+
+  // remove
+  collection.schema.removeField("vm4wexsu")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "raujcrkq",
+    "name": "icon",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "hvuukigw",
+    "name": "color",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "spxeebso",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
