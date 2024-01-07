e2a389c3683aeadd30a398a67edd4fc576e8c026
melvinchia3636 <melvinchia623600@gmail.com>
2024-01-07 22:57:15 +0800

    24w01
    Former-commit-id: 52eb1bea5ea12d8208297489073d8d83ea7f83d2 [formerly 1f4cb8e477fb6ad7c372a9b9d46da9223c0958d8] [formerly e4a1b09ad6efa51160a573cea74ec50a7a1b5002 [formerly d41577e3e4f62e7f8fb8798b9f5866403c0e8961]]
    Former-commit-id: 11e2b2c872adae19c6b0969065055a0eb0db2a25 [formerly 2a6dd78a56d24879a1bcc167345ebbbdf5dbf9e1]
    Former-commit-id: 9cf69bbb8afdebf262fcb55c6e7665851436c46c

--- src/modules/PomodoroTimer/index.tsx
+++ src/modules/PomodoroTimer/index.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable multiline-ternary */
-import React, { useState } from 'react'
+import React from 'react'
 import ModuleHeader from '../../components/ModuleHeader'
 import { Icon } from '@iconify/react/dist/iconify.js'
+import Timer from './components/Timer'
 
 export default function PomodoroTimer(): React.JSX.Element {
-  const [started, setStarted] = useState(false)
-
   return (
     <section className="flex h-full min-h-0 w-full flex-1 flex-col px-12">
       <ModuleHeader
@@ -13,68 +12,7 @@ export default function PomodoroTimer(): React.JSX.Element {
         desc="Increase your productivity by using the Pomodoro technique."
       />
       <div className="mt-8 flex w-full flex-1">
-        <div className="flex min-h-0 w-full flex-1 flex-col items-center justify-center gap-12">
-          <div className="relative flex flex-col items-center justify-center">
-            <div
-              className="radial-progress absolute text-neutral-200 dark:text-neutral-800/50"
-              style={{
-                '--value': '100',
-                '--size': '28rem',
-                '--thickness': '20px'
-              }}
-              role="progressbar"
-            ></div>
-            <div
-              className="radial-progress flex items-center justify-center text-amber-500"
-              style={{
-                '--value': '70',
-                '--size': '28rem',
-                '--thickness': '20px'
-              }}
-              role="progressbar"
-            >
-              <div className="z-[9999] mt-12 flex flex-col items-center gap-4 text-neutral-800 dark:text-neutral-100">
-                <span className="text-7xl font-medium tracking-widest">
-                  02:33
-                </span>
-                <span className="text-lg font-medium uppercase tracking-widest text-amber-500">
-                  short break
-                </span>
-                {started ? (
-                  <span className="text-lg font-medium tracking-widest text-neutral-100">
-                    #8
-                  </span>
-                ) : (
-                  <button
-                    onClick={() => {
-                      setStarted(true)
-                    }}
-                    className="rounded-lg p-4 text-neutral-800 hover:bg-neutral-50 dark:bg-neutral-800/50 dark:text-neutral-100"
-                  >
-                    <Icon icon="tabler:play" className="h-8 w-8 shrink-0" />
-                  </button>
-                )}
-              </div>
-            </div>
-          </div>
-          {started && (
-            <div className="flex items-center gap-6">
-              <button className="flex shrink-0 items-center gap-2 rounded-lg bg-amber-500 p-4 px-6 pr-7 font-semibold uppercase tracking-wider text-neutral-100 dark:text-neutral-800">
-                <Icon icon="tabler:pause" className="h-5 w-5 shrink-0" />
-                <span className="shrink-0">pause session</span>
-              </button>
-              <button
-                onClick={() => {
-                  setStarted(false)
-                }}
-                className="flex shrink-0 items-center gap-2 rounded-lg bg-neutral-200 p-4 px-6 pr-7 font-semibold uppercase tracking-wider text-neutral-500 hover:bg-neutral-300 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:bg-neutral-700/50"
-              >
-                <Icon icon="tabler:square" className="h-5 w-5 shrink-0" />
-                <span className="shrink-0">end session</span>
-              </button>
-            </div>
-          )}
-        </div>
+        <Timer />
         <aside className="mb-12 w-2/6 overflow-y-scroll rounded-lg bg-neutral-50 p-6 dark:bg-neutral-800/50">
           <h1 className="mb-2 flex items-center gap-2 text-2xl font-semibold">
             <Icon icon="tabler:circle-check" className="text-3xl" />
