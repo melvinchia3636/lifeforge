85254d874f284cb65509fe95b1f095b99a41c0a8
melvinchia3636 <melvinchia623600@gmail.com>
2024-01-07 22:06:55 +0800

    24w01
    Former-commit-id: 8d2e1ae51a6d2d08453d3d21951e7999942d7767 [formerly e986a437712a351b018213216ca98f4e5e8835c8] [formerly bfac02460c9603ee06ba92ec2be653756f22a8cf [formerly 4a878a55fd1172803a9bd2ce6c1ffabc49d27aab]]
    Former-commit-id: 815e0df0652f3c20f4ff9d31e969baeacb687a24 [formerly cb267f449fee474412e6c07962f3651387a717fe]
    Former-commit-id: d3bfb2a19526ce1e2796b07eeb36ad765bb8cf76

--- src/Router.tsx
+++ src/Router.tsx
@@ -21,6 +21,7 @@ import CardSet from './modules/Flashcards/components/CardSet'
 import ReferenceBooks from './modules/referenceBooks'
 import UniversityAnalytics from './modules/UniversityAnalytics'
 import Changelog from './modules/Changelog'
+import Personalization from './modules/Personalization'
 
 function AppRouter(): React.JSX.Element {
   const { auth, authLoading } = useContext(AuthContext)
@@ -68,9 +69,10 @@ function AppRouter(): React.JSX.Element {
         <Route path="flashcards/:id" element={<CardSet />} />
         <Route path="reference-books" element={<ReferenceBooks />} />
         <Route path="university-analytics" element={<UniversityAnalytics />} />
-        <Route path="/change-log" element={<Changelog />} />
+        <Route path="personalization" element={<Personalization />} />
+        <Route path="change-log" element={<Changelog />} />
       </Route>
-      <Route path="/auth" element={<Auth />} />
+      <Route path="auth" element={<Auth />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
