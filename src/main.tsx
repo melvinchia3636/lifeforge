954c73f6bce5d0bc7728bd63211f37aacac366a3
melvinchia3636 <melvinchia623600@gmail.com>
2024-02-01 16:35:52 +0800

    24w05

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'react-tooltip/dist/react-tooltip.css'
-import 'react-toastify/dist/ReactToastify.css'
-import 'react-medium-image-zoom/dist/styles.css'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
