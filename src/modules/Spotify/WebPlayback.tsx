c3b3c87f204019fbcd62f7e982a6d44ada031d36
melvinchia3636 <melvinchia623600@gmail.com>
2024-02-04 20:37:21 +0800

    24w05

--- src/modules/Spotify/WebPlayback.tsx
+++ src/modules/Spotify/WebPlayback.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable multiline-ternary */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/naming-convention */
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Icon } from '@iconify/react/dist/iconify.js'
 import { SpotifyContext } from '../../providers/SpotifyProvider'
 import EmptyStateScreen from '../../components/general/EmptyStateScreen'
@@ -9,11 +9,38 @@ import EmptyStateScreen from '../../components/general/EmptyStateScreen'
 function WebPlayback(): React.ReactElement {
   const { player, isPaused, isActive, currentTrack } =
     useContext(SpotifyContext)
+  const [position, setPosition] = useState('0:00')
+
+  function getPlayDuration(): void {
+    if (isActive) {
+      player.getCurrentState().then(state => {
+        if (!state) {
+          console.error(
+            'User is not playing music through the Web Playback SDK'
+          )
+          return '0:00'
+        }
+
+        const { position } = state
+        setPosition(position)
+      })
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getPlayDuration()
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [isActive, player])
 
   return isActive ? (
     <>
       <div className="flex h-full w-full flex-col items-center justify-between py-12">
-        <div className="flex flex-col items-center justify-center ">
+        <div className="flex flex-1 flex-col items-center justify-center ">
           {currentTrack.album.images[0].url && (
             <img
               src={currentTrack.album.images[0].url}
@@ -28,41 +55,83 @@ function WebPlayback(): React.ReactElement {
             {currentTrack.artists[0].name}
           </div>
         </div>
-        <div className="flex items-center justify-center gap-6">
-          <button
-            onClick={() => {
-              player.previousTrack()
-            }}
-            className="rounded-full p-4 hover:bg-bg-200/50 dark:hover:bg-bg-900"
-          >
-            <Icon icon="tabler:player-skip-back-filled" className="text-xl" />
-          </button>
-          <button
-            onClick={() => {
-              player.togglePlay()
-            }}
-            className={`rounded-full p-4 ${
-              isPaused
-                ? 'bg-custom-500 text-bg-100 dark:text-bg-900'
-                : 'bg-bg-200 dark:bg-bg-700/50'
-            }`}
-          >
-            <Icon
-              icon={isPaused ? 'tabler:player-play-filled' : 'tabler:pause'}
-              className="text-2xl"
-            />
-          </button>
-          <button
-            onClick={() => {
-              player.nextTrack()
-            }}
-            className="rounded-full p-4 hover:bg-bg-200/50 dark:hover:bg-bg-900"
-          >
-            <Icon
-              icon="tabler:player-skip-forward-filled"
-              className="text-xl"
-            />
-          </button>
+        <div className="mt-8 flex w-full flex-col items-center justify-center gap-4">
+          <div className="flex w-full flex-col">
+            <div className="flex items-center justify-center gap-6">
+              <p className="hidden w-20 justify-end sm:flex">{`${Math.floor(
+                position / 60000
+              )}:${`${Math.floor((position % 60000) / 1000)}`.padStart(
+                2,
+                '0'
+              )}`}</p>
+              <input
+                type="range"
+                min="1"
+                max={currentTrack.duration_ms}
+                value={position}
+                onChange={e => {
+                  player.seek(Number(e.target.value))
+                }}
+                className="w-full sm:w-96"
+                id="myRange"
+              />
+              <p className="hidden w-20 justify-start sm:flex">{`${Math.floor(
+                currentTrack.duration_ms / 60000
+              )}:${`${Math.floor(
+                (currentTrack.duration_ms % 60000) / 1000
+              )}`.padStart(2, '0')}`}</p>
+            </div>
+            <div className="flex w-full items-center justify-between sm:hidden">
+              <p className="text-sm text-bg-500">
+                {`${Math.floor(position / 60000)}:${`${Math.floor(
+                  (position % 60000) / 1000
+                )}`.padStart(2, '0')}`}
+              </p>
+              <p className="text-sm text-bg-500">
+                {`${Math.floor(
+                  currentTrack.duration_ms / 60000
+                )}:${`${Math.floor(
+                  (currentTrack.duration_ms % 60000) / 1000
+                )}`.padStart(2, '0')}`}
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center justify-center gap-6">
+            <button
+              onClick={() => {
+                player.previousTrack()
+              }}
+              className="rounded-full p-4 hover:bg-bg-200/50 dark:hover:bg-bg-900"
+            >
+              <Icon icon="tabler:player-skip-back-filled" className="text-xl" />
+            </button>
+            <button
+              onClick={() => {
+                player.togglePlay()
+              }}
+              className={`rounded-full p-4 ${
+                isPaused
+                  ? 'bg-custom-500 text-bg-100 dark:text-bg-900'
+                  : 'bg-bg-200 dark:bg-bg-700/50'
+              }`}
+            >
+              <Icon
+                icon={isPaused ? 'tabler:player-play-filled' : 'tabler:pause'}
+                className="text-2xl"
+              />
+            </button>
+            <button
+              onClick={() => {
+                player.nextTrack()
+              }}
+              className="rounded-full p-4 hover:bg-bg-200/50 dark:hover:bg-bg-900"
+            >
+              <Icon
+                icon="tabler:player-skip-forward-filled"
+                className="text-xl"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </>
