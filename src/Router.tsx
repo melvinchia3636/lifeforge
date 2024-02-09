f1dd08e0b26e543a8d7291cfd67ab082649273ef
melvinchia3636 <melvinchia623600@gmail.com>
2024-02-09 08:06:16 +0800

    24w06
    Former-commit-id: 1476ec56e780b264e13d7ca225a1924654fecb99 [formerly be93ebd47bb421f4d1cb1b05240edcd71d92b7b3] [formerly ad94288cf1160df0f496baaefd27f16a271411a7 [formerly 5b01a97f6a8861c5fc1f66101659f3953167a89f]]
    Former-commit-id: 6deca0aa97fe5b2f5bc98882c25960690137474a [formerly 00a0675430ef6995c6a6e1bdd540a1cf46e00e6e]
    Former-commit-id: 7f3f058c0a72a28dece37dce18697fc9122018e0

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable multiline-ternary */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import React, { useContext, useEffect } from 'react'
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
@@ -7,13 +8,11 @@ import MainApplication from './MainApplication'
 import { AuthContext } from './providers/AuthProvider'
 import TodoList from './modules/TodoList'
 import Calendar from './modules/Calendar'
-import Projects from './modules/Projects'
-import Kanban from './modules/Projects/components/Kanban'
+import Projects from './modules/ProjectsM'
+import Kanban from './modules/ProjectsM/components/Kanban'
 import NotFound from './components/general/NotFound'
 import IdeaBox from './modules/IdeaBox'
 import Ideas from './modules/IdeaBox/components/Ideas'
-import Snippets from './modules/Snippets'
-import Resources from './modules/Resources'
 import CodeTime from './modules/CodeTime'
 import PomodoroTimer from './modules/PomodoroTimer'
 import Flashcards from './modules/Flashcards'
@@ -23,14 +22,279 @@ import Changelog from './modules/Changelog'
 import Notes from './modules/Notes'
 import NotesCategory from './modules/Notes/Workspace'
 import NotesSubject from './modules/Notes/Subject'
-import Settings from './modules/Settings'
 import Personalization from './modules/Personalization'
 import ServerStatus from './modules/ServerStatus'
 import Spotify from './modules/Spotify'
 import Modules from './modules/Modules'
+import { titleToPath } from './components/Sidebar/components/SidebarItem'
+
+interface IRoutesItem {
+  name: string
+  icon: string
+  routes: React.ReactElement[]
+  subsection?: string[][]
+  togglable: boolean
+}
+interface IRoutes {
+  title: string
+  items: IRoutesItem[]
+}
+
+export const ROUTES: IRoutes[] = [
+  {
+    title: '',
+    items: [
+      {
+        name: 'Dashboard',
+        icon: 'tabler:dashboard',
+        routes: [
+          <Route key="dashboard" path="dashboard" element={<Dashboard />} />
+        ],
+        togglable: false
+      }
+    ]
+  },
+  {
+    title: 'Productivity',
+    items: [
+      {
+        name: 'Projects (M)',
+        icon: 'tabler:clipboard',
+        routes: [
+          <Route key="projects-m" path="projects-m" element={<Projects />} />,
+          <Route
+            key="projects-m-id"
+            path="projects-m/:id"
+            element={<Kanban />}
+          />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Projects (K)',
+        icon: 'tabler:clipboard',
+        routes: [
+          <Route key="projects-k" path="projects-k" element={<Projects />} />,
+          <Route
+            key="projects-k-id"
+            path="projects-k/:id"
+            element={<Kanban />}
+          />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Idea Box',
+        icon: 'tabler:bulb',
+        routes: [
+          <Route key="idea-box" path="idea-box" element={<IdeaBox />} />,
+          <Route key="idea-box-id" path="idea-box/:id" element={<Ideas />} />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Todo List',
+        icon: 'tabler:list-check',
+        routes: [
+          <Route key="todo-list" path="todo-list" element={<TodoList />} />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Calendar',
+        icon: 'tabler:calendar',
+        routes: [
+          <Route key="calendar" path="calendar" element={<Calendar />} />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Spotify',
+        icon: 'tabler:brand-spotify',
+        routes: [<Route key="spotify" path="spotify" element={<Spotify />} />],
+        togglable: true
+      },
+      {
+        name: 'Code Time',
+        icon: 'tabler:code',
+        routes: [
+          <Route key="code-time" path="code-time" element={<CodeTime />} />
+        ],
+        togglable: true
+      }
+    ]
+  },
+  {
+    title: 'Storage',
+    items: [
+      {
+        name: 'Photos',
+        icon: 'tabler:camera',
+        routes: [],
+        togglable: true
+      }
+    ]
+  },
+  {
+    title: 'Study',
+    items: [
+      {
+        name: 'Pomodoro Timer',
+        icon: 'tabler:clock-bolt',
+        routes: [
+          <Route
+            key="pomodoro-timer"
+            path="pomodoro-timer"
+            element={<PomodoroTimer />}
+          />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Flashcards',
+        icon: 'tabler:cards',
+        routes: [
+          <Route key="flashcards" path="flashcards" element={<Flashcards />} />,
+          <Route
+            key="flashcards-id"
+            path="flashcards/:id"
+            element={<CardSet />}
+          />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Notes',
+        icon: 'tabler:notebook',
+        routes: [
+          <Route key="notes" path="notes" element={<Notes />} />,
+          <Route
+            key="notes-workspace"
+            path="notes/:workspace"
+            element={<NotesCategory />}
+          />,
+          <Route
+            key="notes-subject"
+            path="notes/:workspace/:subject/*"
+            element={<NotesSubject />}
+          />
+        ],
+        togglable: true
+      },
+      {
+        name: 'Reference Books',
+        icon: 'tabler:books',
+        routes: [
+          <Route
+            key="reference-books"
+            path="reference-books"
+            element={<ReferenceBooks />}
+          />
+        ],
+        togglable: true
+      }
+    ]
+  },
+  {
+    title: 'Finance',
+    items: [
+      {
+        name: 'Wallet',
+        icon: 'tabler:currency-dollar',
+        subsection: [
+          ['Balance', 'tabler:wallet'],
+          ['Transactions', 'tabler:arrows-exchange'],
+          ['Budgets', 'tabler:coin'],
+          ['Reports', 'tabler:chart-bar']
+        ],
+        routes: [],
+        togglable: true
+      },
+      {
+        name: 'Wish List',
+        icon: 'tabler:heart',
+        routes: [],
+        togglable: true
+      }
+    ]
+  },
+  {
+    title: 'Confidential',
+    items: [
+      {
+        name: 'Contacts',
+        icon: 'tabler:users',
+        routes: [],
+        togglable: true
+      },
+      {
+        name: 'Passwords',
+        icon: 'tabler:key',
+        routes: [],
+        togglable: true
+      }
+    ]
+  },
+  {
+    title: 'Settings',
+    items: [
+      {
+        name: 'Personalization',
+        icon: 'tabler:palette',
+        routes: [
+          <Route
+            key="personalization"
+            path="personalization"
+            element={<Personalization />}
+          />
+        ],
+        togglable: false
+      },
+      {
+        name: 'Data Backup',
+        icon: 'tabler:database',
+        routes: [
+          <Route key="data-backup" path="data-backup" element={<></>} />
+        ],
+        togglable: false
+      },
+      {
+        name: 'Modules',
+        icon: 'tabler:plug',
+        routes: [<Route key="modules" path="modules" element={<Modules />} />],
+        togglable: false
+      },
+      {
+        name: 'Server Status',
+        icon: 'tabler:server',
+        routes: [
+          <Route
+            key="server-status"
+            path="server-status"
+            element={<ServerStatus />}
+          />
+        ],
+        togglable: false
+      }
+    ]
+  },
+  {
+    title: '',
+    items: [
+      {
+        name: 'Change Log',
+        icon: 'tabler:file-text',
+        routes: [
+          <Route key="change-log" path="change-log" element={<Changelog />} />
+        ],
+        togglable: false
+      }
+    ]
+  }
+]
 
 function AppRouter(): React.JSX.Element {
-  const { auth, authLoading } = useContext(AuthContext)
+  const { auth, authLoading, userData } = useContext(AuthContext)
   const location = useLocation()
   const navigate = useNavigate()
 
@@ -60,30 +324,17 @@ function AppRouter(): React.JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<MainApplication />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="todo-list" element={<TodoList />} />
-        <Route path="calendar" element={<Calendar />} />
-        <Route path="spotify" element={<Spotify />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="projects/:id" element={<Kanban />} />
-        <Route path="idea-box" element={<IdeaBox />} />
-        <Route path="idea-box/:id" element={<Ideas />} />
-        <Route path="snippets" element={<Snippets />} />
-        <Route path="snippets/snippet/:id" element={<Snippets />} />
-        <Route path="resources" element={<Resources />} />
-        <Route path="code-time" element={<CodeTime />} />
-        <Route path="pomodoro-timer" element={<PomodoroTimer />} />
-        <Route path="flashcards" element={<Flashcards />} />
-        <Route path="flashcards/:id" element={<CardSet />} />
-        <Route path="reference-books" element={<ReferenceBooks />} />
-        <Route path="notes" element={<Notes />} />
-        <Route path="notes/:workspace" element={<NotesCategory />} />
-        <Route path="notes/:workspace/:subject/*" element={<NotesSubject />} />
-        <Route path="personalization" element={<Personalization />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="modules" element={<Modules />} />
-        <Route path="server-status" element={<ServerStatus />} />
-        <Route path="change-log" element={<Changelog />} />
+        {userData ? (
+          ROUTES.flatMap(e => e.items)
+            .filter(
+              item =>
+                !item.togglable ||
+                userData.enabledModules.includes(titleToPath(item.name))
+            )
+            .map(item => item.routes)
+        ) : (
+          <Route path="*" element={<NotFound />} />
+        )}
       </Route>
       <Route path="auth" element={<Auth />} />
       <Route path="*" element={<NotFound />} />
