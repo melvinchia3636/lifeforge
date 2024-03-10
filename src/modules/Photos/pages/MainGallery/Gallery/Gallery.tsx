0c6be14338d374390418b8dc51c30538ea0fca1c
melvinchia3636 <melvinchia623600@gmail.com>
2024-03-10 08:32:20 +0800

    24w10

--- src/modules/Photos/pages/MainGallery/Gallery/Gallery.tsx
+++ src/modules/Photos/pages/MainGallery/Gallery/Gallery.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable multiline-ternary */
 /* eslint-disable @typescript-eslint/indent */
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import DateGroup from './DateGroup'
 import { PhotosContext } from '../../../../../providers/PhotosProvider'
 import BottomBar from '../../../components/BottomBar'
 
 function Gallery(): React.ReactElement {
-  const { photos } = useContext(PhotosContext)
-  const [selectedPhotos, setSelectedPhotos] = useState<string[]>([])
+  const { photos, selectedPhotos, setSelectedPhotos } =
+    useContext(PhotosContext)
 
   useEffect(() => {
     return () => {
