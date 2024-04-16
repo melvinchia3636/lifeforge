e5d5459dbbc1fc236d8798f5931167e2accf2fab
melvinchia3636 <melvinchia623600@gmail.com>
2024-04-16 19:01:03 +0800

    24w16

---
+++ src/modules/Repositories/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ModuleHeader from '@components/ModuleHeader'
+import ModuleWrapper from '@components/ModuleWrapper'
+
+function Repositories(): React.ReactElement {
+  return (
+    <ModuleWrapper>
+      <ModuleHeader title="Repositories" desc="" />
+    </ModuleWrapper>
+  )
+}
+
+export default Repositories
