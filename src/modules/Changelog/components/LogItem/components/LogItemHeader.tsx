7e309e4bf1d296c13dba7ac0f7777b18b336aed7
melvinchia3636 <melvinchia623600@gmail.com>
2024-05-24 16:05:13 +0800

    24w21
    Former-commit-id: ccd774b1e2595fbaf77ff259fe16eaa39e597a30 [formerly ea5852f3b19156a2e1b62f39e73a80a11884af01] [formerly 18d989c189f4233d7bd28934974ac6577f154bd8 [formerly b2a65b2989b143692c32fe4a8ae78f28e5cb1209]]
    Former-commit-id: 28a886570bfb8a6877804a0af7d12c83eabdf7f0 [formerly a42f9c944b9d96dd5723526f0c869637bac08c87]
    Former-commit-id: 62158529840450b187a1ab5def5baf3e09d43251

--- src/modules/Changelog/components/LogItem/components/LogItemHeader.tsx
+++ src/modules/Changelog/components/LogItem/components/LogItemHeader.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import React from 'react'
 import { type IChangeLogVersion } from '@typedec/Changelog'
 
@@ -7,21 +8,18 @@ function LogItemHeader({
   entry: IChangeLogVersion
 }): React.ReactElement {
   return (
-    <h3 className="mb-2 flex flex-col gap-2 text-2xl font-semibold sm:flex-row sm:items-end">
-      Ver. {entry.version}{' '}
-      <span className="mb-0.5 text-sm">
-        (
-        {entry.date_range
-          .map(date =>
-            new Date(date).toLocaleDateString('en-US', {
-              year: 'numeric',
-              month: 'short',
-              day: 'numeric'
-            })
-          )
-          .join(' - ')}
-        )
+    <h3 className="mb-2 flex flex-col items-center gap-2 text-2xl font-semibold sm:flex-row">
+      <span className="w-24 text-right text-sm text-bg-500">
+        {moment(entry.date_range[1]).format('MMM D, YYYY')}
       </span>
+      <div
+        className={`mx-4 h-4 w-4 rounded-full border-2 bg-bg-950 ${
+          moment(entry.date_range[1]).isAfter(moment())
+            ? 'border-custom-500'
+            : 'border-bg-600'
+        } outline outline-4 outline-bg-950`}
+      />
+      Ver. {entry.version}
     </h3>
   )
 }
