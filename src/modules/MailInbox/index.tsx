82e0c0daa44bf2c63c8f240a1457db22b1c2e911
melvinchia3636 <melvinchia623600@gmail.com>
2024-05-27 08:00:35 +0800

    24w21

---
+++ src/modules/MailInbox/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ModuleHeader from '@components/Module/ModuleHeader'
+import ModuleWrapper from '@components/Module/ModuleWrapper'
+
+function MailInbox(): React.ReactElement {
+  return (
+    <ModuleWrapper>
+      <ModuleHeader title="Mail Inbox" desc="..." />
+    </ModuleWrapper>
+  )
+}
+
+export default MailInbox
