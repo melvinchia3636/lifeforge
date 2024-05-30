5565c7214e7e604652749373ee657a1d7080d854
melvinchia3636 <melvinchia623600@gmail.com>
2024-05-30 19:00:27 +0800

    24w22
    Former-commit-id: ffae3759db5859869281bc3cb2ca30af34bfbd8e [formerly c25738f7e2e7609078f594da3b1f683f27ee4840] [formerly 941f42d5bc72a17b8a7f9038e1e3ab0fdcc64850 [formerly 2f41bb33136b8a5d8c38dafe1e234754a7c06f9b]]
    Former-commit-id: 5ebd026f34649c00feb64b18391078f701f40b70 [formerly e6f447d8ce6f39f27fa84f952e4cbbac18a53334]
    Former-commit-id: 230639444b1d42e854030f8e821d3383e18a1b15

--- src/modules/MailInbox/index.tsx
+++ src/modules/MailInbox/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import ModuleHeader from '@components/Module/ModuleHeader'
 import ModuleWrapper from '@components/Module/ModuleWrapper'
+import APIComponentWithFallback from '@components/Screens/APIComponentWithFallback'
+import useFetch from '@hooks/useFetch'
 
 function MailInbox(): React.ReactElement {
+  const [mails] = useFetch<any>('mail-inbox/list')
+
   return (
     <ModuleWrapper>
       <ModuleHeader title="Mail Inbox" desc="..." />
+      <APIComponentWithFallback data={mails}>
+        {typeof mails !== 'string' && (
+          <ul className="divide-y-2 divide-bg-900">
+            {mails.map((mail: any) => (
+              <li key={mail.id} className="flex flex-col gap-1 p-4">
+                <span className="text-sm text-bg-500">
+                  {mail.from.split(' ')[0].replace(/(^")|("$)/g, '') ??
+                    'No sender'}
+                </span>
+                {mail.subject === '' ? 'No subject' : mail.subject}
+              </li>
+            ))}
+          </ul>
+        )}
+      </APIComponentWithFallback>
     </ModuleWrapper>
   )
 }
