de413699007a3f262f787e004ec68c3af31bf6d8
melvinchia3636 <melvinchia623600@gmail.com>
2024-06-02 10:36:12 +0800

    24w23

--- src/modules/Changelog/components/LogItem/index.tsx
+++ src/modules/Changelog/components/LogItem/index.tsx
@@ -7,7 +7,7 @@ function LogItem({ entry }: { entry: IChangeLogVersion }): React.ReactElement {
   return (
     <li
       key={entry.version}
-      className="space-y-6 rounded-lg bg-bg-900 p-6 shadow-custom sm:!bg-transparent sm:shadow-none"
+      className="space-y-6 rounded-lg bg-bg-50 p-6 shadow-custom dark:bg-bg-900 sm:!bg-transparent sm:shadow-none"
     >
       <LogItemHeader entry={entry} />
       <LogItemContent entry={entry} />
