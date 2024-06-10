8296027bcbb0871c2e6c1c934869665e2ba1507b
melvinchia3636 <melvinchia623600@gmail.com>
2024-06-10 23:52:48 +0800

    24w24

--- src/modules/Photos/components/ImageObject.tsx
+++ src/modules/Photos/components/ImageObject.tsx
@@ -234,75 +234,76 @@ function ImageObject({
         selected ? 'bg-custom-500/20' : 'bg-bg-200 dark:bg-bg-800'
       } transition-all ${selectedPhotosLength > 0 && 'cursor-pointer'}`}
     >
-      {(ready || beingDisplayedInAlbum) && (
-        <>
-          <div
-            className={`size-full transition-all duration-300 ${
-              selected && 'p-4'
-            } ${selectedPhotosLength > 0 ? 'pointer-events-none' : ''}`}
-          >
-            <Zoom
-              zoomMargin={100}
-              ZoomContent={props => (
-                <CustomZoomContent
-                  {...props}
-                  data={details}
-                  setPhotos={setPhotos}
-                  beingDisplayedInAlbum={beingDisplayedInAlbum}
-                  refreshAlbumData={refreshAlbumData}
-                  refreshPhotos={refreshPhotos}
-                />
-              )}
-              zoomImg={{
-                src: photo.src.split('?')[0]
-              }}
+      {(ready || beingDisplayedInAlbum) &&
+        photo.src.endsWith('/undefined?thumb=0x300') === true && (
+          <>
+            <div
+              className={`size-full transition-all duration-300 ${
+                selected && 'p-4'
+              } ${selectedPhotosLength > 0 ? 'pointer-events-none' : ''}`}
             >
-              {/* @ts-expect-error cannot fix */}
-              <LazyLoadImage
-                alt=""
-                src={photo.src}
-                className={`relative size-full object-cover ${
-                  selected && 'rounded-md'
-                }`}
-                delayTime={300}
-                delayMethod="debounce"
-                threshold={50}
-                useIntersectionObserver={false}
-              />
-            </Zoom>
-          </div>
-          {!selected && (
-            <div className="pointer-events-none absolute top-0 h-12 w-full bg-gradient-to-t from-transparent to-black/50 opacity-0 transition-all group-hover/image:opacity-100" />
-          )}
-          <button
-            onClick={toggleSelected}
-            className={`group/select-button flex-center absolute left-2.5 top-2.5 size-6 rounded-full transition-all  ${
-              selected
-                ? 'flex bg-custom-500 opacity-100'
-                : 'hidden bg-bg-200 opacity-50 hover:!bg-bg-100 hover:!opacity-100 group-hover/image:flex'
-            }`}
-          >
-            <Icon
-              icon="tabler:check"
-              className={`stroke-bg-900 stroke-[2px] text-bg-800 transition-all ${
-                !selected &&
-                'group-hover/select-button:stroke-bg-900 group-hover/select-button:text-bg-800'
+              <Zoom
+                zoomMargin={100}
+                ZoomContent={props => (
+                  <CustomZoomContent
+                    {...props}
+                    data={details}
+                    setPhotos={setPhotos}
+                    beingDisplayedInAlbum={beingDisplayedInAlbum}
+                    refreshAlbumData={refreshAlbumData}
+                    refreshPhotos={refreshPhotos}
+                  />
+                )}
+                zoomImg={{
+                  src: photo.src.split('?')[0]
+                }}
+              >
+                {/* @ts-expect-error cannot fix */}
+                <LazyLoadImage
+                  alt=""
+                  src={photo.src}
+                  className={`relative size-full object-cover ${
+                    selected && 'rounded-md'
+                  }`}
+                  delayTime={300}
+                  delayMethod="debounce"
+                  threshold={50}
+                  useIntersectionObserver={false}
+                />
+              </Zoom>
+            </div>
+            {!selected && (
+              <div className="pointer-events-none absolute top-0 h-12 w-full bg-gradient-to-t from-transparent to-black/50 opacity-0 transition-all group-hover/image:opacity-100" />
+            )}
+            <button
+              onClick={toggleSelected}
+              className={`group/select-button flex-center absolute left-2.5 top-2.5 size-6 rounded-full transition-all  ${
+                selected
+                  ? 'flex bg-custom-500 opacity-100'
+                  : 'hidden bg-bg-200 opacity-50 hover:!bg-bg-100 hover:!opacity-100 group-hover/image:flex'
               }`}
-            />
-          </button>
-          <div className="absolute right-2 top-2 flex items-center gap-2 text-bg-200 opacity-50">
-            {details.has_raw && (
-              <Icon icon="tabler:letter-r" className="size-5" />
-            )}
-            {!beingDisplayedInAlbum && details.is_in_album && (
-              <Icon icon="tabler:library-photo" className="size-5" />
-            )}
-            {details.is_favourite && (
-              <Icon icon="tabler:star" className="size-5" />
-            )}
-          </div>
-        </>
-      )}
+            >
+              <Icon
+                icon="tabler:check"
+                className={`stroke-bg-900 stroke-[2px] text-bg-800 transition-all ${
+                  !selected &&
+                  'group-hover/select-button:stroke-bg-900 group-hover/select-button:text-bg-800'
+                }`}
+              />
+            </button>
+            <div className="absolute right-2 top-2 flex items-center gap-2 text-bg-200 opacity-50">
+              {details.has_raw && (
+                <Icon icon="tabler:letter-r" className="size-5" />
+              )}
+              {!beingDisplayedInAlbum && details.is_in_album && (
+                <Icon icon="tabler:library-photo" className="size-5" />
+              )}
+              {details.is_favourite && (
+                <Icon icon="tabler:star" className="size-5" />
+              )}
+            </div>
+          </>
+        )}
     </div>
   )
 }
