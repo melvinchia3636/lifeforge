1c65ef5647e2355758fac2aab90835e3ff203c99
melvinchia3636 <melvinchia623600@gmail.com>
2024-06-11 07:02:32 +0800

    24w24
    Former-commit-id: 5e3eea2c1b942e19ae1b587c86a91ce62a73ca2f [formerly 6fb3eac8c810d2d00104472964319d39b6065105] [formerly d04e876ca424d25615c44508230efd4cd37e5c1e [formerly 0c236ed29791fdcfc68b114b6fbe2928bba17e55]]
    Former-commit-id: 2f76f7a81e13e16bafddf5595de8db42d48682cf [formerly 6fa58b6af0d45651abaf1deffb6fb9bfbc507497]
    Former-commit-id: 630b781b895c6741168091db712569f9d2c29947

---
+++ src/modules/TodoList/components/tasks/TaskItem/components/SubtaskItem.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useTodoListContext } from '@providers/TodoListProvider'
+import type { ITodoSubtask } from '@typedec/TodoList'
+import APIRequest from '@utils/fetchData'
+import TaskCompletionCheckbox from './TaskCompletionCheckbox'
+
+function SubtaskItem({
+  entry,
+  parentId
+}: {
+  entry: ITodoSubtask
+  parentId: string
+}): React.ReactElement {
+  const { setEntries, entries, refreshEntries } = useTodoListContext()
+
+  async function toggleSubTaskCompletion(): Promise<void> {
+    if (typeof entries !== 'string' && setEntries !== undefined) {
+      setEntries(
+        entries.map(e =>
+          e.id === parentId
+            ? {
+                ...e,
+                subtasks: e.subtasks.map(subtask =>
+                  subtask.id === entry.id
+                    ? {
+                        ...subtask,
+                        done: !subtask.done
+                      }
+                    : subtask
+                )
+              }
+            : e
+        )
+      )
+    }
+
+    await APIRequest({
+      endpoint: `todo-list/subtask/toggle/${entry.id}`,
+      method: 'PATCH',
+      failureInfo: "Couldn't update the task. Please try again.",
+      onFailure: () => {
+        if (refreshEntries !== undefined) {
+          refreshEntries()
+        }
+      }
+    })
+  }
+
+  return (
+    <div className="flex items-center justify-between rounded-md bg-bg-900 p-6">
+      {entry.title}
+      <TaskCompletionCheckbox
+        toggleTaskCompletion={() => {
+          toggleSubTaskCompletion().catch(console.error)
+        }}
+        entry={entry}
+      />
+    </div>
+  )
+}
+
+export default SubtaskItem
