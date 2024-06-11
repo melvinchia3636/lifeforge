1a92183eb2869d10b5eb6f9959b856bc9efe0a9f
melvinchia3636 <melvinchia623600@gmail.com>
2024-06-12 01:54:26 +0800

    24w24

---
+++ src/modules/Wallet/pages/Transactions/views/TableView/ColumnVisibilityToggle.tsx
@@ -0,0 +1,54 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import MenuItem from '@components/ButtonsAndInputs/HamburgerMenu/MenuItem'
+import SidebarDivider from '@components/Sidebar/components/SidebarDivider'
+import { toCamelCase } from '@utils/strings'
+
+function ColumnVisibilityToggle({
+  visibleColumn,
+  setVisibleColumn
+}: {
+  visibleColumn: string[]
+  setVisibleColumn: React.Dispatch<React.SetStateAction<string[]>>
+}): React.ReactElement {
+  const { t } = useTranslation()
+  return (
+    <>
+      <SidebarDivider noMargin />
+      <span className="flex items-center gap-4 p-4 text-bg-500">
+        <Icon icon="tabler:eye" className="size-5" />
+        Columns Visibility
+      </span>
+      <div className="p-4 pt-0">
+        <ul className="flex flex-col divide-y divide-bg-700 overflow-hidden rounded-md bg-bg-700/50">
+          {[
+            'Date',
+            'Type',
+            'Ledger',
+            'Asset',
+            'Particular',
+            'Category',
+            'Amount',
+            'Receipt'
+          ].map(column => (
+            <MenuItem
+              key={column}
+              text={t(`table.${toCamelCase(column)}`)}
+              onClick={() => {
+                if (visibleColumn.includes(column)) {
+                  setVisibleColumn(visibleColumn.filter(e => e !== column))
+                } else {
+                  setVisibleColumn([...visibleColumn, column])
+                }
+              }}
+              isToggled={visibleColumn.includes(column)}
+            />
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default ColumnVisibilityToggle
