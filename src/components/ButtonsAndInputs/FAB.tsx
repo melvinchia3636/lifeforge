8d2cce923aaab2335790dd91b2a8f2f6c96835dc
melvinchia3636 <melvinchia623600@gmail.com>
2024-06-17 12:41:53 +0800

    24w25

--- src/components/ButtonsAndInputs/FAB.tsx
+++ src/components/ButtonsAndInputs/FAB.tsx
@@ -15,10 +15,10 @@ function FAB({
       onClick={onclick}
       className={`absolute bottom-6 right-6 z-10 flex items-center gap-2 rounded-lg bg-custom-500 p-4 font-semibold uppercase tracking-wider text-bg-100 shadow-lg hover:bg-custom-600 dark:text-bg-800 ${
         {
-          sm: 'md:hidden',
-          md: 'lg:hidden',
-          lg: 'xl:hidden',
-          xl: '2xl:hidden'
+          sm: 'sm:hidden',
+          md: 'md:hidden',
+          lg: 'lg:hidden',
+          xl: 'xl:hidden'
         }[hideWhen] ?? ''
       }
       `}
