abc9f50efc56f8f935dd0a70c38c7c50b7337464
melvinchia3636 <melvinchia623600@gmail.com>
2024-07-05 20:08:51 +0800

    24w27
    Former-commit-id: 1677db313cfb3d1b52d170f991aaf3222f3106da [formerly 64df495c811639999e8c684a78a3525057e06fa7] [formerly 7755be08e272db5ad3ce9c23fd482d5aa0321178 [formerly 58868d0125281901aed5513ea9bfeee0ab6dade1]]
    Former-commit-id: ceabbd10c032c0932e843f29c7a3c889c5a63bc8 [formerly dd93ed7a6cdbaa9237245d25c3344b4f0f6ba1ea]
    Former-commit-id: 49cb7490885b125fd6bb2363bd904205ff4ae194

--- src/modules/BooksLibrary/index.tsx
+++ src/modules/BooksLibrary/index.tsx
@@ -1,21 +1,18 @@
 import { Icon } from '@iconify/react'
 import React, { useState } from 'react'
 import Button from '@components/ButtonsAndInputs/Button'
-import GoBackButton from '@components/ButtonsAndInputs/GoBackButton'
 import SearchInput from '@components/ButtonsAndInputs/SearchInput'
 import ModuleHeader from '@components/Module/ModuleHeader'
 import ModuleWrapper from '@components/Module/ModuleWrapper'
 import APIComponentWithFallback from '@components/Screens/APIComponentWithFallback'
-import Scrollbar from '@components/Scrollbar'
-import SidebarDivider from '@components/Sidebar/components/SidebarDivider'
-import SidebarItem from '@components/Sidebar/components/SidebarItem'
-import SidebarTitle from '@components/Sidebar/components/SidebarTitle'
 import useFetch from '@hooks/useFetch'
+import Sidebar from './components/Sidebar'
+import GridView from './views/GridView'
+import ListView from './views/ListView'
 
 function BooksLibrary(): React.ReactElement {
   const [searchQuery, setSearchQuery] = useState('')
   const [books] = useFetch<any>('books-library/list')
-  const [sidebarOpen, setSidebarOpen] = useState(false)
   const [view, setView] = useState<'list' | 'grid'>('list')
 
   return (
@@ -25,67 +22,9 @@ function BooksLibrary(): React.ReactElement {
         desc="A collection of reference books that accompany you on your learning journey."
       />
       <div className="mt-6 flex min-h-0 w-full min-w-0 flex-1">
-        <aside
-          className={`absolute ${
-            sidebarOpen ? 'left-0' : 'left-full'
-          } top-0 z-[9999] size-full shrink-0 rounded-lg bg-bg-50 py-4 shadow-custom duration-300 dark:bg-bg-900 xl:static xl:h-[calc(100%-2rem)] xl:w-1/4`}
-        >
-          <Scrollbar>
-            <div className="flex flex-between px-8 py-4 xl:hidden">
-              <GoBackButton
-                onClick={() => {
-                  setSidebarOpen(false)
-                }}
-              />
-            </div>
-            <ul className="flex w-full min-w-0 flex-col">
-              <SidebarItem icon="tabler:list" name="All books" />
-              <SidebarItem icon="tabler:star-filled" name="Starred" />
-              <SidebarDivider />
-              <SidebarTitle name="categories" />
-              {[
-                ['tabler:math-integral-x', 'Calculus'],
-                ['tabler:math-pi', 'Mathematics'],
-                ['tabler:atom', 'Physics'],
-                ['tabler:code', 'Computer Science']
-              ].map(([icon, name], index) => (
-                <li
-                  key={index}
-                  className="relative flex w-full min-w-0 items-center gap-6 px-4 font-medium text-bg-500 transition-all"
-                >
-                  <div className="flex w-full min-w-0 items-center gap-6 whitespace-nowrap rounded-lg p-4 hover:bg-bg-800">
-                    <Icon icon={icon} className="size-5 shrink-0 " />
-                    <div className="w-full min-w-0 truncate">{name}</div>
-                    <span className="text-sm">
-                      {Math.floor(Math.random() * 10)}
-                    </span>
-                  </div>
-                </li>
-              ))}
-              <SidebarDivider />
-              <SidebarTitle name="languages" />
-              {[
-                ['emojione-monotone:flag-for-china', 'Chinese'],
-                ['emojione-monotone:flag-for-united-kingdom', 'English']
-              ].map(([icon, name], index) => (
-                <li
-                  key={index}
-                  className="relative flex items-center gap-6 px-4 font-medium text-bg-500 transition-all"
-                >
-                  <div className="flex w-full items-center gap-6 whitespace-nowrap rounded-lg p-4 hover:bg-bg-800">
-                    <Icon icon={icon} className="size-5 shrink-0" />
-                    <div className="flex w-full flex-between">{name}</div>
-                    <span className="text-sm">
-                      {Math.floor(Math.random() * 10)}
-                    </span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </Scrollbar>
-        </aside>
+        <Sidebar />
         <div className="flex h-full min-h-0 flex-1 flex-col pb-8 xl:ml-8">
-          <div className="flex flex-between">
+          <div className="flex-between flex">
             <h1 className="text-3xl font-semibold sm:text-4xl">
               All Books <span className="text-base text-bg-500">(10)</span>
             </h1>
@@ -133,59 +72,9 @@ function BooksLibrary(): React.ReactElement {
           <APIComponentWithFallback data={books}>
             {books =>
               view === 'grid' ? (
-                <Scrollbar className="mt-6">
-                  <ul className="grid min-h-0 gap-6 px-4 sm:grid-cols-2 md:grid-cols-3">
-                    {books.map((item: any) => (
-                      <li
-                        key={item.id}
-                        className="relative flex flex-col items-start rounded-lg"
-                      >
-                        <div className="flex-center flex h-72 w-full rounded-lg bg-bg-50 p-8 shadow-custom dark:bg-bg-900">
-                          <img
-                            src={`${
-                              import.meta.env.VITE_API_HOST
-                            }/books-library/cover/${item.cover}`}
-                            className="h-full"
-                          />
-                        </div>
-                        <div className="mt-4 text-xl font-medium">
-                          {item.title}
-                        </div>
-                        <div className="mt-2 text-sm font-medium text-bg-500">
-                          {item.authors}
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                </Scrollbar>
+                <GridView books={books} />
               ) : (
-                <Scrollbar className="mt-6">
-                  <ul className="space-y-4 px-4">
-                    {books.map((item: any) => (
-                      <li
-                        key={item.id}
-                        className="relative flex items-center gap-4 rounded-lg bg-bg-50 p-4 shadow-custom dark:bg-bg-900"
-                      >
-                        <div className="flex-center flex size-20 rounded-lg bg-bg-200 p-2 dark:bg-bg-800">
-                          <img
-                            src={`${
-                              import.meta.env.VITE_API_HOST
-                            }/books-library/cover/${item.cover}`}
-                            className="h-full"
-                          />
-                        </div>
-                        <div className="flex min-w-0 flex-1 flex-col">
-                          <div className="text-lg font-semibold">
-                            {item.title}
-                          </div>
-                          <div className="text-sm font-medium text-bg-500">
-                            {item.authors}
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                </Scrollbar>
+                <ListView books={books} />
               )
             }
           </APIComponentWithFallback>
