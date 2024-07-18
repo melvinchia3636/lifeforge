e597c7a1ad4cec00a01adea271095c340c6fe5fd
melvinchia3636 <melvinchia623600@gmail.com>
2024-07-19 05:31:58 +0800

    24w29

--- src/modules/Music/components/MusicList/index.tsx
+++ src/modules/Music/components/MusicList/index.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import APIComponentWithFallback from '@components/Screens/APIComponentWithFallback'
 import { useMusicContext } from '@providers/MusicProvider'
 import MusicListItem from './components/MusicListItem'
+import 'react-virtualized/styles.css'
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer'
+import List from 'react-virtualized/dist/commonjs/List'
 
 function MusicList({
   debouncedSearchQuery
@@ -13,17 +16,35 @@ function MusicList({
   return (
     <APIComponentWithFallback data={musics}>
       {musics => (
-        <>
-          {musics
-            .filter(music =>
-              music.name
-                .toLowerCase()
-                .includes(debouncedSearchQuery.toLowerCase())
-            )
-            .map(music => (
-              <MusicListItem key={music.id} music={music} />
-            ))}
-        </>
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              width={width}
+              height={height}
+              rowCount={
+                musics.filter(music =>
+                  music.name
+                    .toLowerCase()
+                    .includes(debouncedSearchQuery.toLowerCase())
+                ).length
+              }
+              rowHeight={60}
+              rowRenderer={({ index, key, style }) => {
+                const music = musics.filter(music =>
+                  music.name
+                    .toLowerCase()
+                    .includes(debouncedSearchQuery.toLowerCase())
+                )[index]
+
+                return (
+                  <div key={key} style={style}>
+                    <MusicListItem music={music} />
+                  </div>
+                )
+              }}
+            />
+          )}
+        </AutoSizer>
       )}
     </APIComponentWithFallback>
   )
