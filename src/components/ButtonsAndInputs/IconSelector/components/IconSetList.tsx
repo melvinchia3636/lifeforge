42ad0a8556633caf6feab608fd802a223272fb3a
melvinchia3636 <melvinchia623600@gmail.com>
2024-09-13 07:23:25 +0800

    24w37

--- src/components/ButtonsAndInputs/IconSelector/components/IconSetList.tsx
+++ src/components/ButtonsAndInputs/IconSelector/components/IconSetList.tsx
@@ -11,6 +11,7 @@
 import { collections } from '@iconify/collections'
 import { Icon } from '@iconify/react'
 import React, { useState } from 'react'
+import Scrollbar from '@components/Scrollbar'
 import Input from './Input'
 
 const ICON_SETS = {}
@@ -47,7 +48,7 @@ export default function IconSetList({
   const [iconFilterTerm, setIconFilterTerm] = useState('')
 
   return (
-    <div className="overflow-scroll p-8 pb-2 pt-0">
+    <div className="flex h-full min-h-0 flex-1 flex-col pb-2 pt-0">
       <div className="flex w-full flex-col gap-2 sm:flex-row">
         <Input
           value={searchQuery}
@@ -94,83 +95,87 @@ export default function IconSetList({
           />
         </div>
       </div>
-      <div className="mt-12 flex min-h-0 w-full flex-col items-center overflow-scroll">
-        <div className="flex w-full flex-col">
-          {Object.entries(ICON_SETS).map(
-            ([category, iconSets], index) =>
-              Boolean(
-                (selectedCategory === null || selectedCategory === index) &&
-                  iconSets.filter(
-                    iconSet =>
-                      !iconFilterTerm.trim() ||
-                      iconSet.name
-                        .toLowerCase()
-                        .includes(iconFilterTerm.trim().toLowerCase())
-                  ).length
-              ) && (
-                <div key={category} className="mb-6 w-full overflow-hidden">
-                  <div className="relative mb-8 rounded-lg text-center text-2xl font-medium after:absolute after:-bottom-2 after:left-1/2 after:w-8 after:-translate-x-1/2 after:border-b-2 after:border-b-bg-200">
-                    {category}
-                  </div>
-                  <div className="icon-list grid grid-cols-[repeat(auto-fill,minmax(230px,1fr))] flex-wrap gap-4">
-                    {iconSets.map(
+      <Scrollbar autoHeight autoHeightMax="60vh">
+        <div className="mt-12 flex min-h-0 w-full flex-col items-center overflow-scroll">
+          <div className="flex w-full flex-col">
+            {Object.entries(ICON_SETS).map(
+              ([category, iconSets], index) =>
+                Boolean(
+                  (selectedCategory === null || selectedCategory === index) &&
+                    iconSets.filter(
                       iconSet =>
-                        (!iconFilterTerm.trim() ||
-                          iconSet.name
-                            .toLowerCase()
-                            .includes(iconFilterTerm.trim().toLowerCase())) && (
-                          <button
-                            key={iconSet.prefix}
-                            type="button"
-                            onClick={() => {
-                              setCurrentIconSet({ iconSet: iconSet.prefix })
-                            }}
-                            className="sssm:flex-row flex w-full grow flex-col overflow-hidden rounded-md bg-bg-800 shadow-lg"
-                          >
-                            <div className="sssm:w-36 text-bg-00 flex w-full shrink-0 flex-col font-medium">
-                              <div className="sssm:gap-3 flex-center flex size-full gap-5 px-4 py-6 ">
-                                {iconSet.samples.map(sampleIcon => (
-                                  <Icon
-                                    key={sampleIcon}
-                                    icon={`${iconSet.prefix}:${sampleIcon}`}
-                                    className="size-8 shrink-0"
-                                  />
-                                ))}
-                              </div>
-                            </div>
-                            <div className="flex w-full flex-col justify-between px-4 pb-3 text-left">
-                              <h3 className="truncate text-xl font-semibold">
-                                {iconSet.name}
-                              </h3>
-                              <p className="mt-1 truncate text-sm">
-                                By&nbsp;
-                                <span className="underline">
-                                  {iconSet.author.name}
-                                </span>
-                              </p>
-                              <div className="sssm:py-0 flex-between mt-4 flex w-full border-t border-bg-500 pt-4 text-sm">
-                                <p>{iconSet.total} icons</p>
-                                {iconSet.height && (
-                                  <div className="flex items-center">
+                        !iconFilterTerm.trim() ||
+                        iconSet.name
+                          .toLowerCase()
+                          .includes(iconFilterTerm.trim().toLowerCase())
+                    ).length
+                ) && (
+                  <div key={category} className="mb-6 w-full overflow-hidden">
+                    <div className="relative mb-8 rounded-lg text-center text-2xl font-medium after:absolute after:-bottom-2 after:left-1/2 after:w-8 after:-translate-x-1/2 after:border-b-2 after:border-b-bg-200">
+                      {category}
+                    </div>
+                    <div className="icon-list grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] flex-wrap gap-4">
+                      {iconSets.map(
+                        iconSet =>
+                          (!iconFilterTerm.trim() ||
+                            iconSet.name
+                              .toLowerCase()
+                              .includes(
+                                iconFilterTerm.trim().toLowerCase()
+                              )) && (
+                            <button
+                              key={iconSet.prefix}
+                              type="button"
+                              onClick={() => {
+                                setCurrentIconSet({ iconSet: iconSet.prefix })
+                              }}
+                              className="sssm:flex-row flex w-full grow flex-col overflow-hidden rounded-md bg-bg-800 shadow-lg"
+                            >
+                              <div className="sssm:w-36 text-bg-00 flex w-full shrink-0 flex-col font-medium">
+                                <div className="sssm:gap-3 flex-center flex size-full gap-5 px-4 py-6 ">
+                                  {iconSet.samples.map(sampleIcon => (
                                     <Icon
-                                      icon="icon-park-outline:auto-height-one"
-                                      width="20"
-                                      height="20"
+                                      key={sampleIcon}
+                                      icon={`${iconSet.prefix}:${sampleIcon}`}
+                                      className="size-8 shrink-0"
                                     />
-                                    <p className="ml-1">{iconSet.height}</p>
-                                  </div>
-                                )}
+                                  ))}
+                                </div>
                               </div>
-                            </div>
-                          </button>
-                        )
-                    )}
+                              <div className="flex w-full flex-col justify-between px-4 pb-3 text-left">
+                                <h3 className="truncate text-xl font-semibold">
+                                  {iconSet.name}
+                                </h3>
+                                <p className="mt-1 truncate text-sm">
+                                  By&nbsp;
+                                  <span className="underline">
+                                    {iconSet.author.name}
+                                  </span>
+                                </p>
+                                <div className="sssm:py-0 flex-between mt-4 flex w-full border-t border-bg-500 pt-4 text-sm">
+                                  <p>{iconSet.total} icons</p>
+                                  {iconSet.height && (
+                                    <div className="flex items-center">
+                                      <Icon
+                                        icon="icon-park-outline:auto-height-one"
+                                        width="20"
+                                        height="20"
+                                      />
+                                      <p className="ml-1">{iconSet.height}</p>
+                                    </div>
+                                  )}
+                                </div>
+                              </div>
+                            </button>
+                          )
+                      )}
+                    </div>
                   </div>
-                </div>
-              )
-          )}
+                )
+            )}
+          </div>
         </div>
-      </div>
+      </Scrollbar>
     </div>
   )
 }
