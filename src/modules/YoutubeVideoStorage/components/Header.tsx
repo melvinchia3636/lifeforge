94d9d697189e2c68b2b7e55c176198950841f252
melvinchia3636 <melvinchia623600@gmail.com>
2024-09-12 10:52:01 +0800

    24w37
    Former-commit-id: f42f109258011bb9a2d5008b8c6d1e604705709f [formerly dbd093d46932a1419fb4f4684a8d72ae8f029409] [formerly b70a9dde4680b5775c3f38ba6b7d9a89f2dbf5c9 [formerly 4f4e29d0c2bcbf0ac2ae4ce04c758a6a808bd272]]
    Former-commit-id: 7fd6cf14ba58581b80e4726052821b977c1f8589 [formerly 08cfd94eb33c3f9947c9741e2ed8ef8c39a5c9e7]
    Former-commit-id: 40da219337c0fcbf2019bb81b598e4c57f5e21c0

---
+++ src/modules/YoutubeVideoStorage/components/Header.tsx
@@ -0,0 +1,159 @@
+import React, { useEffect, useState } from 'react'
+import Button from '@components/ButtonsAndInputs/Button'
+import MenuItem from '@components/ButtonsAndInputs/HamburgerMenu/MenuItem'
+import SearchInput from '@components/ButtonsAndInputs/SearchInput'
+import ModuleHeader from '@components/Module/ModuleHeader'
+import { type IYoutubeVideoInfo } from '@interfaces/youtube_video_storage_interfaces'
+import APIRequest from '@utils/fetchData'
+import IntervalManager from '@utils/intervalManager'
+import DownloadProcessModal from './DownloadProcessModal'
+
+const intervalManager = IntervalManager.getInstance()
+
+function Header({
+  videosLength,
+  refreshVideos,
+  setIsAddVideosModalOpen,
+  query,
+  setQuery,
+  needsProgressCheck,
+  setNeedsProgressCheck,
+  isAddVideosModalOpen
+}: {
+  videosLength: number
+  refreshVideos: () => void
+  setIsAddVideosModalOpen: (value: boolean) => void
+  query: string
+  setQuery: (value: string) => void
+  needsProgressCheck: boolean
+  setNeedsProgressCheck: (value: boolean) => void
+  isAddVideosModalOpen: boolean
+}): React.ReactElement {
+  const [isDownloadProcessModalOpen, setIsDownloadProcessModalOpen] =
+    useState(false)
+  const [processes, setProcesses] = useState<
+    Record<
+      string,
+      {
+        status: 'completed' | 'failed' | 'in_progress'
+        progress: number
+        metadata: IYoutubeVideoInfo
+      }
+    >
+  >({})
+  const [isFirstTime, setIsFirstTime] = useState(true)
+
+  function checkProgress(): void {
+    console.log('sus')
+    if (!needsProgressCheck && !isFirstTime) return
+    setIsFirstTime(false)
+
+    APIRequest({
+      endpoint: 'youtube-video-storage/video/download-status',
+      method: 'POST',
+      failureInfo: 'Failed to get download status',
+      body: { id: 'all' },
+      callback(data) {
+        if (data.state === 'success') {
+          const processes = data.data as Record<
+            string,
+            {
+              status: 'completed' | 'failed' | 'in_progress'
+              progress: number
+              metadata: IYoutubeVideoInfo
+            }
+          >
+
+          if (
+            (Object.keys(processes).length !== 0 &&
+              !Object.values(processes).some(
+                p => p.status === 'in_progress'
+              )) ||
+            Object.keys(processes).length === 0
+          ) {
+            if (!isFirstTime) {
+              refreshVideos()
+            }
+            setNeedsProgressCheck(false)
+          }
+          setProcesses(processes)
+        }
+      }
+    }).catch(console.error)
+  }
+
+  useEffect(() => {
+    const interval = intervalManager.setInterval(checkProgress, 1000)
+
+    return () => {
+      intervalManager.clearInterval(interval)
+    }
+  }, [needsProgressCheck, isFirstTime, isAddVideosModalOpen])
+
+  return (
+    <>
+      <ModuleHeader
+        icon="tabler:brand-youtube"
+        title="Youtube Video Storage"
+        desc="..."
+        totalItems={videosLength}
+        actionButton={
+          <Button
+            icon="tabler:plus"
+            onClick={() => {
+              refreshVideos()
+              setIsAddVideosModalOpen(true)
+            }}
+          >
+            Add Video
+          </Button>
+        }
+        hasHamburgerMenu
+        hamburgerMenuItems={
+          <MenuItem
+            icon="tabler:refresh"
+            text="Refresh"
+            onClick={refreshVideos}
+          />
+        }
+        customElement={
+          Object.entries(processes).some(
+            ([, { status }]) => status === 'in_progress'
+          ) && (
+            <Button
+              icon="tabler:download"
+              variant="no-bg"
+              className="p-5"
+              onClick={() => {
+                setIsDownloadProcessModalOpen(true)
+              }}
+            >
+              (
+              {
+                Object.entries(processes).filter(
+                  ([, { status }]) => status === 'in_progress'
+                ).length
+              }
+              )
+            </Button>
+          )
+        }
+      />
+      <SearchInput
+        searchQuery={query}
+        setSearchQuery={setQuery}
+        stuffToSearch="videos"
+      />
+      <DownloadProcessModal
+        isOpen={isDownloadProcessModalOpen}
+        onClose={() => {
+          setIsDownloadProcessModalOpen(false)
+          refreshVideos()
+        }}
+        processes={processes}
+      />
+    </>
+  )
+}
+
+export default Header
