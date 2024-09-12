e75c3b70c1187d52a0c7f4d770e50d52d7311a7f
melvinchia3636 <melvinchia623600@gmail.com>
2024-09-12 13:38:21 +0800

    24w37

--- src/modules/Airports/pages/Airport/index.tsx
+++ src/modules/Airports/pages/Airport/index.tsx
@@ -75,7 +75,7 @@ function Airport(): React.ReactElement {
               </div>
               <Scrollbar>
                 <div className="flex gap-12">
-                  <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAP_API_KEY}>
+                  <APIProvider apiKey={import.meta.env.VITE_GOOGLE_API_KEY}>
                     <Map
                       style={{ width: '100%', height: '24rem' }}
                       defaultCenter={{
