ed5bb197cf3f24f88b92f05c3740ff58c7bcb752
melvinchia3636 <melvinchia623600@gmail.com>
2024-09-13 10:56:11 +0800

    24w37
    Former-commit-id: 316770ea2b616356f4d5c71964b9c6f8d4b0eac5 [formerly 7ebdaddc87849e15648ed25a32276c7b09271328] [formerly 5d257edf662f29913a47c780b12e01fe351a8da6 [formerly a5766847404be542339d8549cd3110a468be9d4f]]
    Former-commit-id: a7d4921f145cf1f3baeb8ff5b053e321eea73ff9 [formerly a6ded901edd2bd348eefa885569055e59057f7f1]
    Former-commit-id: 1ab687f713a81e7fc0f786bd0c867fb095a34656

--- src/modules/PomodoroTimer/components/Timer.tsx
+++ src/modules/PomodoroTimer/components/Timer.tsx
@@ -1,9 +1,60 @@
 import { Icon } from '@iconify/react'
-import React, { useState } from 'react'
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
 import Button from '@components/ButtonsAndInputs/Button'
 
 export default function Timer(): React.ReactElement {
-  const [started, setStarted] = useState(false)
+  // TODO: Implement UI to change time distribution
+  const [timeDistribution] = useState([1500, 300, 900])
+  const [currentRoundBig, setCurrentRoundBig] = useState(1)
+  const [currentRoundSmall, setCurrentRoundSmall] = useState(0)
+  const [currentSection, setCurrentSection] = useState(0)
+  const [timeLeft, setTimeLeft] = useState(timeDistribution[currentSection])
+  const [isRunning, setIsRunning] = useState(false)
+
+  useEffect(() => {
+    if (isRunning) {
+      if (timeLeft > 0) {
+        const timer = setTimeout(() => {
+          setTimeLeft(timeLeft - 1)
+        }, 1)
+
+        return () => {
+          clearTimeout(timer)
+        }
+      }
+      setCurrentRoundSmall(currentRoundSmall + 1)
+      switch (currentSection) {
+        case 0: {
+          setCurrentSection(1)
+          setTimeLeft(timeDistribution[1])
+
+          if ((currentRoundSmall + 1) % 9 === 0) {
+            setCurrentSection(2)
+            setTimeLeft(timeDistribution[2])
+          }
+          break
+        }
+        case 1: {
+          setCurrentSection(0)
+          setTimeLeft(timeDistribution[0])
+          setCurrentRoundBig(currentRoundBig + 1)
+          break
+        }
+        case 2: {
+          if (currentRoundSmall % 9 === 0) {
+            setCurrentRoundSmall(0)
+          }
+          setCurrentSection(0)
+          setTimeLeft(timeDistribution[0])
+          break
+        }
+        default:
+          break
+      }
+    }
+    return () => {}
+  }, [timeLeft, isRunning, currentSection])
 
   return (
     <div className="flex-center flex min-h-0 w-full flex-1 flex-col gap-12">
@@ -22,25 +73,27 @@ export default function Timer(): React.ReactElement {
           className="flex-center radial-progress flex text-custom-500"
           style={{
             // @ts-expect-error - Cannot fix lah this one ;-;
-            '--value': '70',
+            '--value': `${(timeLeft / timeDistribution[currentSection]) * 100}`,
             '--size': '28rem',
             '--thickness': '20px'
           }}
           role="progressbar"
         >
-          <div className="z-[9999] mt-12 flex flex-col items-center gap-4 ">
-            <span className="text-7xl font-medium tracking-widest">02:33</span>
-            <span className="text-lg font-medium uppercase tracking-widest text-custom-500">
-              short break
+          <div className="z-[9999] mt-12 flex flex-col items-center gap-4 text-bg-800 dark:text-bg-100 ">
+            <span className="text-7xl font-medium tracking-widest">
+              {moment.utc(timeLeft * 1000).format('mm:ss')}
             </span>
-            {started ? (
+            <span className="text-lg font-medium uppercase tracking-widest text-custom-500">
+              {['Pomodoro', 'Short break', 'Long break'][currentSection]}
+            </span>
+            {isRunning ? (
               <span className="text-lg font-medium tracking-widest text-bg-100">
-                #8
+                #{currentRoundBig}
               </span>
             ) : (
               <button
                 onClick={() => {
-                  setStarted(true)
+                  setIsRunning(true)
                 }}
                 className="rounded-lg p-4 text-bg-800 hover:bg-bg-50 dark:bg-bg-900 dark:text-bg-100"
               >
@@ -50,14 +103,14 @@ export default function Timer(): React.ReactElement {
           </div>
         </div>
       </div>
-      {started && (
+      {isRunning && (
         <div className="flex items-center gap-6">
           <Button onClick={() => {}} icon="tabler:pause">
             pause session
           </Button>
           <Button
             onClick={() => {
-              setStarted(false)
+              setIsRunning(false)
             }}
             icon="tabler:square"
             variant="secondary"
