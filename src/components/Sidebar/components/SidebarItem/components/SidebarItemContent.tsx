405571e2726941a1ffab183d979818432c1500b2
melvinchia3636 <melvinchia623600@gmail.com>
2024-09-24 11:02:36 +0800

    24w38
    Former-commit-id: 847dd27672a445156cdbc0bd142d42800d10ad37 [formerly 2136d7ad735e0a4120b0198337cd1cc549afab1d] [formerly 9dea44dcfb43d1e6f128144fcc01116550682a5a [formerly f15416bd215f607419f9f6db860c836009ed971f]]
    Former-commit-id: f5f91a105f0d74d5e6c9e005796937c0f69748ed [formerly 7ff50d71397d2299bbc27573cb9f423a06e399fb]
    Former-commit-id: 19fe835cdbe2e2a43c47b846d6b9ecdc78349269

---
+++ src/components/Sidebar/components/SidebarItem/components/SidebarItemContent.tsx
@@ -0,0 +1,70 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+import { t } from 'i18next'
+import React from 'react'
+import { toCamelCase } from '@utils/strings'
+
+function SidebarItemContent({
+  name,
+  sidebarExpanded,
+  isMainSidebarItem,
+  hasAI,
+  number,
+  isMenuOpen,
+  hamburgerMenuItems,
+  active,
+  onCancelButtonClick,
+  needTranslate = false
+}: {
+  name: string
+  sidebarExpanded: boolean
+  isMainSidebarItem: boolean
+  hasAI: boolean
+  number?: number
+  isMenuOpen: boolean
+  hamburgerMenuItems?: React.ReactElement
+  active: boolean
+  onCancelButtonClick?: () => void
+  needTranslate?: boolean
+}): React.ReactElement {
+  return (
+    <span className="flex-between flex w-full min-w-0 gap-4">
+      {sidebarExpanded &&
+        (isMainSidebarItem ? (
+          <span className="flex-between flex w-full gap-2 truncate">
+            <span className="min-w-0 max-w-48 truncate">
+              {t(`modules.${toCamelCase(name)}`)}
+            </span>
+            {hasAI && (
+              <Icon icon="mage:stars-c" className="size-4 text-custom-500" />
+            )}
+          </span>
+        ) : (
+          <span className="block w-full min-w-0 truncate">
+            {needTranslate
+              ? t(
+                  `sidebar.${toCamelCase(
+                    location.pathname.split('/').slice(1)[0].replace(/-/g, ' ')
+                  )}.${toCamelCase(name)}`
+                )
+              : name}
+          </span>
+        ))}
+      {number !== undefined && (
+        <span
+          className={`pr-2 text-sm ${
+            isMenuOpen
+              ? 'hidden'
+              : hamburgerMenuItems !== undefined ||
+                (active && onCancelButtonClick !== undefined)
+              ? 'group-hover:hidden'
+              : 'block'
+          }`}
+        >
+          {number.toLocaleString()}
+        </span>
+      )}
+    </span>
+  )
+}
+
+export default SidebarItemContent
--- src/modules/Calendar/components/Sidebar/components/CategoryList/components/CategoryListItem.tsx
+++ src/modules/Calendar/components/Sidebar/components/CategoryList/components/CategoryListItem.tsx
@@ -1,9 +1,8 @@
-import { Icon } from '@iconify/react'
-import React, { useState } from 'react'
+import React from 'react'
 import { useSearchParams } from 'react-router-dom'
-import HamburgerMenu from '@components/ButtonsAndInputs/HamburgerMenu'
 import MenuItem from '@components/ButtonsAndInputs/HamburgerMenu/MenuItem'
 import { type ICalendarCategory } from '@interfaces/calendar_interfaces'
+import SidebarItem from '@components/Sidebar/components/SidebarItem'
 
 function CategoryListItem({
   item,
@@ -19,92 +18,51 @@ function CategoryListItem({
   >
 }): React.ReactElement {
   const [searchParams, setSearchParams] = useSearchParams()
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <li
-      className={`relative flex min-w-0 items-center gap-6 px-4 font-medium transition-all ${
-        searchParams.get('category') === item.id
-          ? "text-bg-800 after:absolute after:right-0 after:top-1/2 after:h-8 after:w-1 after:-translate-y-1/2 after:rounded-full after:bg-custom-500 after:content-[''] dark:text-bg-50"
-          : 'text-bg-500 dark:text-bg-500'
-      }`}
-    >
-      <div
-        tabIndex={0}
-        role="button"
-        onClick={() => {
-          setSearchParams({
-            ...Object.fromEntries(searchParams.entries()),
-            category: item.id
-          })
-        }}
-        className={`group flex w-full items-center gap-6 whitespace-nowrap rounded-lg p-4 text-left ${
-          searchParams.get('category') === item.id
-            ? 'bg-bg-200/50 dark:bg-bg-800'
-            : 'hover:bg-bg-100/50 dark:hover:bg-bg-800/50'
-        }`}
-      >
-        <span
-          className="block h-8 w-1 shrink-0 rounded-full"
-          style={{
-            backgroundColor: item.color
-          }}
-        />
-        <Icon icon={item.icon} className="size-6 shrink-0" />
-        <div className="w-full min-w-0 truncate">{item.name}</div>
-        <span className={!isMenuOpen ? 'text-sm group-hover:hidden' : 'hidden'}>
-          {item.amount}
-        </span>
-        {searchParams.get('category') === item.id ? (
-          <button
+    <SidebarItem
+      active={searchParams.get('category') === item.id}
+      color={item.color}
+      icon={item.icon}
+      name={item.name}
+      needTranslate={false}
+      number={item.amount}
+      onClick={() => {
+        setSearchParams({
+          ...Object.fromEntries(searchParams.entries()),
+          category: item.id
+        })
+      }}
+      onCancelButtonClick={() => {
+        setSearchParams(searchParams => {
+          searchParams.delete('category')
+          return searchParams
+        })
+      }}
+      hamburgerMenuItems={
+        <>
+          <MenuItem
+            icon="tabler:edit"
             onClick={e => {
               e.stopPropagation()
-              setSearchParams(searchParams => {
-                searchParams.delete('category')
-                return searchParams
-              })
+              setSelectedData(item)
+              setModifyModalOpenType('update')
             }}
-            className="hidden overscroll-contain group-hover:block"
-          >
-            <Icon icon="tabler:x" className="size-5" />
-          </button>
-        ) : (
-          <HamburgerMenu
-            smallerPadding
-            onButtonClick={e => {
+            text="Edit"
+          />
+          <MenuItem
+            isRed
+            icon="tabler:trash"
+            onClick={e => {
               e.stopPropagation()
-              setIsMenuOpen(true)
+              setSelectedData(item)
+              // setDeleteConfirmationModalOpen(true)
             }}
-            className={`relative overscroll-contain ${
-              !isMenuOpen ? 'hidden group-hover:block' : ''
-            }`}
-            onClose={() => {
-              setIsMenuOpen(false)
-            }}
-          >
-            <MenuItem
-              icon="tabler:edit"
-              onClick={e => {
-                e.stopPropagation()
-                setSelectedData(item)
-                setModifyModalOpenType('update')
-              }}
-              text="Edit"
-            />
-            <MenuItem
-              isRed
-              icon="tabler:trash"
-              onClick={e => {
-                e.stopPropagation()
-                setSelectedData(item)
-                // setDeleteConfirmationModalOpen(true)
-              }}
-              text="Delete"
-            />
-          </HamburgerMenu>
-        )}
-      </div>
-    </li>
+            text="Delete"
+          />
+        </>
+      }
+    />
   )
 }
 
