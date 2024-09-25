7971b50816a4fa153859dc9ceb0e162830e77f24
melvinchia3636 <melvinchia623600@gmail.com>
2024-09-25 23:25:39 +0800

    24w39

---
+++ src/utils/date.ts
@@ -0,0 +1,9 @@
+import type { Moment } from 'moment'
+
+export const getDatesBetween = (start: Moment, end: Moment): Moment[] => {
+  if (!start.isValid() || !end.isValid() || start.isAfter(end, 'day')) return []
+
+  if (start.isSame(end, 'day')) return [start]
+
+  return [start].concat(getDatesBetween(start.clone().add(1, 'day'), end))
+}
