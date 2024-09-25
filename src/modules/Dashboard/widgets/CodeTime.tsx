4c6c052410f10fa9b5d79810d5cc0e442bc23a3c
melvinchia3636 <melvinchia623600@gmail.com>
2024-09-25 23:25:39 +0800

    24w39
    Former-commit-id: d0f4387b31397e4971009197b5815c25965f47ee [formerly 4783bc0d9f9ffd45ad5fc3e6b86b08c0cb1a97ac] [formerly 6ea2c81084d50f9e52eed9043c8595f1df094fd3 [formerly 5835995f368dd428149131b4154467517760850b]]
    Former-commit-id: 4fcccc6fedae7f270ecf4365e5bdafd5ab37d6fd [formerly 4ecfd36171f003229a62e55f4ccbb7bd7b50b331]
    Former-commit-id: 03012b5907a7e9a29cc42d68fa87696e06dc5c9f

--- src/modules/Dashboard/widgets/CodeTime.tsx
+++ src/modules/Dashboard/widgets/CodeTime.tsx
@@ -2,12 +2,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Icon } from '@iconify/react'
 import { type ChartOptions, type ScriptableContext } from 'chart.js'
+import moment from 'moment'
 import React, { useEffect, useState } from 'react'
-import { Line } from 'react-chartjs-2'
+import { Bar, Line } from 'react-chartjs-2'
 import { useTranslation } from 'react-i18next'
 import LoadingScreen from '@components/Screens/LoadingScreen'
 import useFetch from '@hooks/useFetch'
 import useThemeColorHex from '@hooks/useThemeColorHex'
+import { getDatesBetween } from '@utils/date'
 
 function msToTime(ms: number): string {
   const seconds = (ms / 1000).toFixed(1)
@@ -91,6 +93,7 @@ export default function CodeTime(): React.ReactElement {
   const [chartData, setChartData] = useState<any>(null)
   const { t } = useTranslation()
   const { theme } = useThemeColorHex()
+  const [view, setView] = useState<'bar' | 'line'>('bar')
 
   useEffect(() => {
     if (typeof data !== 'string') {
@@ -101,7 +104,13 @@ export default function CodeTime(): React.ReactElement {
         datasets: [
           {
             label: 'Code time',
-            data: data.map(({ duration }) => duration / 3600000),
+            data: getDatesBetween(
+              moment(data[0].date),
+              moment(data[data.length - 1].date)
+            ).map(date => {
+              const day = data.find(d => d.date === date.format('YYYY-MM-DD'))
+              return day ? day.duration / 3600000 : 0
+            }),
             backgroundColor: (context: ScriptableContext<'line'>) => {
               const ctx = context.chart.ctx
               const gradient = ctx.createLinearGradient(0, 0, 0, 250)
@@ -112,7 +121,6 @@ export default function CodeTime(): React.ReactElement {
             fill: 'origin',
             borderColor: theme,
             borderWidth: 1,
-            tension: 0.4,
             pointBorderColor: 'rgba(0, 0, 0, 0)',
             pointBackgroundColor: 'rgba(0, 0, 0, 0)',
             pointHoverBackgroundColor: theme + '80',
@@ -129,14 +137,48 @@ export default function CodeTime(): React.ReactElement {
 
   return (
     <div className="flex size-full flex-col gap-4 rounded-lg bg-bg-50 p-6 shadow-custom dark:bg-bg-900">
-      <h1 className="mb-2 flex items-center gap-2 text-xl font-semibold">
-        <Icon icon="tabler:chart-line" className="text-2xl" />
-        <span className="ml-2">{t('dashboard.widgets.codeTime.title')}</span>
-      </h1>
+      <div className="flex-between flex">
+        <h1 className="mb-2 flex items-center gap-2 text-xl font-semibold">
+          <Icon icon="tabler:chart-line" className="text-2xl" />
+          <span className="ml-2">{t('dashboard.widgets.codeTime.title')}</span>
+        </h1>
+        <div className="flex items-center gap-2 rounded-md bg-bg-50 p-2 shadow-custom dark:bg-bg-800/50">
+          {['bar', 'line'].map(viewType => (
+            <button
+              key={viewType}
+              onClick={() => {
+                setView(viewType as 'bar' | 'line')
+              }}
+              className={`flex items-center gap-2 rounded-md p-2 px-4 transition-all ${
+                viewType === view
+                  ? 'bg-bg-200/50 dark:bg-bg-700/50'
+                  : 'text-bg-500 hover:text-bg-800 dark:hover:text-bg-50'
+              }`}
+            >
+              <Icon
+                icon={
+                  viewType === 'bar'
+                    ? 'tabler:chart-bar'
+                    : viewType === 'line'
+                    ? 'tabler:chart-line'
+                    : ''
+                }
+                className="size-6"
+              />
+              {viewType[0].toUpperCase() + viewType.slice(1)}
+            </button>
+          ))}
+        </div>
+      </div>
       <div className="flex-center flex size-full min-h-0 flex-1">
         {chartData ? (
-          /* @ts-expect-error - lazy to fix =) */
-          <Line data={chartData} options={options2} />
+          view === 'bar' ? (
+            /* @ts-expect-error - lazy to fix =) */
+            <Bar data={chartData} options={options2} />
+          ) : (
+            /* @ts-expect-error - lazy to fix =) */
+            <Line data={chartData} options={options2} />
+          )
         ) : (
           <LoadingScreen />
         )}
