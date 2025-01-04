6bc141d0e338f1425edf6772f924da4a4d9f5bcd
melvinchia3636 <melvinchia623600@gmail.com>
2025-01-04 17:13:34 +0800

    25w01

--- tools/createModule/functions/getModuleDescInOtherLangs.ts
+++ tools/createModule/functions/getModuleDescInOtherLangs.ts
@@ -1,22 +1,16 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import ora from 'ora'
 import prompts from 'prompts'
+import { errorAndProceed } from '../utils/errorAndProceed'
 
-async function getModuleDescInOtherLangs({
-  login,
-  moduleDesc
-}: {
-  login: any
-  moduleDesc: string
-}): Promise<{
-  moduleDescZHCN: string
-  moduleDescZHTW: string
-  moduleDescMY: string
-} | null> {
+async function getTranslationSuggestions(
+  login: any,
+  moduleName: string
+): Promise<any | null> {
   const spinner = ora('Fetching translation suggestions...').start()
 
   const translationSuggestions = await fetch(
-    `${process.env.VITE_API_HOST}/locales/ai-generate`,
+    `${process.env.VITE_API_HOST}/locales/ai-generate/module-description`,
     {
       method: 'POST',
       headers: {
@@ -24,7 +18,7 @@ async function getModuleDescInOtherLangs({
         Authorization: `Bearer ${login.token}`
       },
       body: JSON.stringify({
-        key: moduleDesc
+        name: moduleName
       })
     }
   )
@@ -37,60 +31,187 @@ async function getModuleDescInOtherLangs({
     return null
   }
 
-  const { ms, 'zh-CN': zhCN, 'zh-TW': zhTW } = translationSuggestions.data
+  return translationSuggestions
+}
 
-  const moduleDescZHCN = await prompts({
-    type: 'text',
-    name: 'moduleDesc',
-    message: 'Enter the module description (in Simplified Chinese)',
-    initial: zhCN,
-    validate: value => {
-      if (!value) {
-        return 'Module description is required'
-      }
-      return true
-    }
-  })
+async function getModuleDescInOtherLangs({
+  login,
+  moduleName
+}: {
+  login: any
+  moduleName: string
+}): Promise<{
+  moduleDescEN: string
+  moduleDescZHCN: string
+  moduleDescZHTW: string
+  moduleDescMY: string
+} | null> {
+  const translationSuggestions = await getTranslationSuggestions(
+    login,
+    moduleName
+  )
 
-  if (!moduleDescZHCN.moduleDesc) {
+  if (!translationSuggestions) {
     return null
   }
 
-  const moduleDescZHTW = await prompts({
-    type: 'text',
-    name: 'moduleDesc',
-    message: 'Enter the module description (in Traditional Chinese)',
-    initial: zhTW,
-    validate: value => {
-      if (!value) {
-        return 'Module description is required'
-      }
-      return true
-    }
-  })
+  let { en, ms, 'zh-CN': zhCN, 'zh-TW': zhTW } = translationSuggestions.data
 
-  if (!moduleDescZHTW.moduleDesc) {
-    return null
+  let moduleDescEN
+
+  while (true) {
+    moduleDescEN = await prompts({
+      type: 'text',
+      name: 'moduleDesc',
+      message: 'Module description (in English)',
+      initial: `${en} (Type "r" and press enter to get a new suggestion)`,
+      validate: value => {
+        if (!value) {
+          return 'Module description is required'
+        }
+        return true
+      }
+    })
+
+    if (!moduleDescEN.moduleDesc) {
+      await errorAndProceed('Module description is required')
+      continue
+    }
+
+    if (moduleDescEN.moduleDesc === 'r') {
+      const translationSuggestions = await getTranslationSuggestions(
+        login,
+        moduleName
+      )
+
+      if (!translationSuggestions) {
+        return null
+      }
+
+      en = translationSuggestions.data.en
+      continue
+    }
+
+    break
   }
 
-  const moduleDescMY = await prompts({
-    type: 'text',
-    name: 'moduleDesc',
-    message: 'Enter the module description (in Malay)',
-    initial: ms,
-    validate: value => {
-      if (!value) {
-        return 'Module description is required'
-      }
-      return true
-    }
-  })
+  let moduleDescZHCN
 
-  if (!moduleDescMY.moduleDesc) {
-    return null
+  while (true) {
+    moduleDescZHCN = await prompts(
+      {
+        type: 'text',
+        name: 'moduleDesc',
+        message: 'Module description (in Simplified Chinese)',
+        initial: `${zhCN} (Type "r" and press enter to get a new suggestion)`,
+        validate: value => {
+          if (!value) {
+            return 'Module description is required'
+          }
+          return true
+        }
+      },
+      { onCancel: () => process.exit(0) }
+    )
+
+    if (!moduleDescZHCN.moduleDesc) {
+      await errorAndProceed('Module description is required')
+      continue
+    }
+
+    if (moduleDescZHCN.moduleDesc === 'r') {
+      const translationSuggestions = await getTranslationSuggestions(
+        login,
+        moduleName
+      )
+
+      if (!translationSuggestions) {
+        return null
+      }
+
+      zhCN = translationSuggestions.data['zh-CN']
+      continue
+    }
+
+    break
+  }
+
+  let moduleDescZHTW
+  while (true) {
+    moduleDescZHTW = await prompts({
+      type: 'text',
+      name: 'moduleDesc',
+      message: 'Module description (in Traditional Chinese)',
+      initial: `${zhTW} (Type "r" and press enter to get a new suggestion)`,
+      validate: value => {
+        if (!value) {
+          return 'Module description is required'
+        }
+        return true
+      }
+    })
+
+    if (!moduleDescZHTW.moduleDesc) {
+      await errorAndProceed('Module description is required')
+      continue
+    }
+
+    if (moduleDescZHTW.moduleDesc === 'r') {
+      const translationSuggestions = await getTranslationSuggestions(
+        login,
+        moduleName
+      )
+
+      if (!translationSuggestions) {
+        return null
+      }
+
+      zhTW = translationSuggestions.data['zh-TW']
+      continue
+    }
+
+    break
+  }
+
+  let moduleDescMY
+  while (true) {
+    moduleDescMY = await prompts({
+      type: 'text',
+      name: 'moduleDesc',
+      message: 'Module description (in Malay)',
+      initial: `${ms} (Type "r" and press enter to get a new suggestion)`,
+      validate: value => {
+        if (!value) {
+          return 'Module description is required'
+        }
+        return true
+      }
+    })
+
+    if (!moduleDescMY.moduleDesc) {
+      await errorAndProceed('Module description is required')
+      continue
+    }
+
+    if (moduleDescMY.moduleDesc === 'r') {
+      const translationSuggestions = await getTranslationSuggestions(
+        login,
+        moduleName
+      )
+
+      if (!translationSuggestions) {
+        return null
+      }
+
+      ms = translationSuggestions.data.ms
+      continue
+    }
+
+    break
   }
 
   return {
+    moduleDescEN: moduleDescEN.moduleDesc,
     moduleDescZHCN: moduleDescZHCN.moduleDesc,
     moduleDescZHTW: moduleDescZHTW.moduleDesc,
     moduleDescMY: moduleDescMY.moduleDesc
--- tools/createModule/functions/loginUser.ts
+++ tools/createModule/functions/loginUser.ts
@@ -4,33 +4,49 @@ import ora from 'ora'
 import prompts from 'prompts'
 
 async function loginUser(): Promise<[boolean, any]> {
-  const username = await prompts({
-    type: 'text',
-    name: 'username',
-    message: 'Enter your username',
-    validate: value => {
-      if (!value) {
-        return 'Username is required'
+  const username = await prompts(
+    {
+      type: 'text',
+      name: 'username',
+      message: 'Username',
+      validate: value => {
+        if (!value) {
+          return 'Username is required'
+        }
+        return true
       }
-      return true
+    },
+    {
+      onCancel: () => {
+        console.log(chalk.red('✖ Login cancelled'))
+        process.exit(0)
+      }
     }
-  })
+  )
 
   if (!username.username) {
     return [false, null]
   }
 
-  const password = await prompts({
-    type: 'password',
-    name: 'password',
-    message: 'Enter your password',
-    validate: value => {
-      if (!value) {
-        return 'Password is required'
+  const password = await prompts(
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Password',
+      validate: value => {
+        if (!value) {
+          return 'Password is required'
+        }
+        return true
       }
-      return true
+    },
+    {
+      onCancel: () => {
+        console.log(chalk.red('✖ Login cancelled'))
+        process.exit(0)
+      }
     }
-  })
+  )
 
   if (!password.password) {
     return [false, null]
@@ -52,12 +68,18 @@ async function loginUser(): Promise<[boolean, any]> {
     .finally(() => spinner.stop())
 
   if (login.state === 'error') {
-    console.log(login)
     console.error('Invalid credentials')
     return [false, null]
   }
 
   console.log(chalk.green('✔ Login successful'))
+  await new Promise(resolve => setTimeout(resolve, 1000))
+  process.stdout.moveCursor(0, -1)
+  process.stdout.clearLine(1)
+  process.stdout.moveCursor(0, -1)
+  process.stdout.clearLine(1)
+  process.stdout.moveCursor(0, -1)
+  process.stdout.clearLine(1)
 
   return [true, login]
 }
