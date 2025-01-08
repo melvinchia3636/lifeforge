706b7cb8337dc40eef5c53ed7fce7d4c0d5468b5
melvinchia3636 <melvinchia623600@gmail.com>
2025-01-08 22:14:11 +0800

    25w02

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,20 @@ export default defineConfig({
     }
   },
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        }
+      }
+    }
   },
   optimizeDeps: {
     esbuildOptions: {
