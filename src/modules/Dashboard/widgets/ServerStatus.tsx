a36fbe771319f4460858aed2f8ec664084cac3f0
melvinchia3636 <melvinchia623600@gmail.com>
2025-01-27 10:21:06 +0800

    25w05
    Former-commit-id: 1b5998cf137171f0e1b563a9b944e5f2d77d1091 [formerly 6b55b65a92f722f0bdc95a67eb83096d94e34657] [formerly b8bb8b464092002e569ebcc47b2861ed2ed3708e [formerly 9b8066cdd6c55ba7ddaeed2ae53e3fb86d4b8fb7]]
    Former-commit-id: 72541d424113026566cbff99e40e0557a05f800f [formerly 23fd4639b327e6cf4243f93d2f1de44fb6edfd1e]
    Former-commit-id: 5e36b5b5118466c25be0bd0a5acac8ff04f8b658

--- src/modules/Dashboard/widgets/ServerStatus.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import DashboardItem from '@components/utilities/DashboardItem'
-
-export default function ServerStatus(): React.ReactElement {
-  return (
-    <DashboardItem icon="tabler:server" title="Server Status"></DashboardItem>
-  )
-}
