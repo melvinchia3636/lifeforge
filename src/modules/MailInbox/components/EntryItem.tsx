bba6234f79c658bdf8e261506313b5874502fca8
melvinchia3636 <melvinchia623600@gmail.com>
2025-02-03 11:59:35 +0800

    25w06
    Former-commit-id: e38fcf4c7cae9dce9514a1011a5ad468416d7b46 [formerly e7598905b8cd52b1433def96dc13084c071dcd30] [formerly 763f502a5cb9553b0597c8de281d0cea84a99c60 [formerly e0481aeeec6bc5cce985daa82962d7701a416b30]]
    Former-commit-id: 11942b8fb9f45ac0fa703706c54d8a3ff600ce07 [formerly 8e972463820bd0d8673a062779c4b0cb8d48e5a6]
    Former-commit-id: 97919fed9f6e4d75c594eef85389c47df4200b81

--- src/modules/MailInbox/components/EntryItem.tsx
+++ src/modules/MailInbox/components/EntryItem.tsx
@@ -1,17 +1,27 @@
 import { Icon } from '@iconify/react/dist/iconify.js'
 import moment from 'moment'
 import React from 'react'
-import { Button } from '@components/buttons'
+import { Button, Checkbox } from '@components/buttons'
 import HamburgerMenu from '@components/buttons/HamburgerMenu'
 import MenuItem from '@components/buttons/HamburgerMenu/components/MenuItem'
 import { type IMailInboxEntry } from '@interfaces/mail_inbox_interfaces'
 
 function EntryItem({
   mail,
-  onView
+  onView,
+  onDelete,
+  isSelected,
+  hasSelected,
+  onShiftSelect,
+  onSelect
 }: {
   mail: IMailInboxEntry
   onView: (mailId: string) => void
+  onDelete: (mailId: string) => void
+  isSelected: boolean
+  hasSelected: boolean
+  onShiftSelect: (mailId: string) => void
+  onSelect: (mailId: string) => void
 }): React.ReactElement {
   return (
     <div
@@ -19,47 +29,69 @@ function EntryItem({
       className="flex-between cursor-pointer flex w-full gap-12 border-b border-bg-200 p-4 text-left transition-all hover:bg-bg-200/50 dark:border-bg-800/50 dark:hover:bg-bg-800/50"
       role="button"
       tabIndex={0}
-      onClick={() => onView(mail.id)}
+      onClick={e => {
+        if (hasSelected) {
+          if (e.shiftKey) {
+            onShiftSelect(mail.id)
+          } else {
+            onSelect(mail.id)
+          }
+        } else {
+          onView(mail.id)
+        }
+      }}
       onKeyDown={e => {
         if (e.key === 'Enter') {
           onView(mail.id)
         }
       }}
     >
-      <div className="w-full min-w-0">
-        <span className="block text-sm text-bg-500">
-          {(() => {
-            if (mail.from.name) {
-              return mail.from.name
+      <div className="flex w-full min-w-0 items-center gap-6">
+        <Checkbox
+          checked={isSelected}
+          onChange={e => {
+            if (e.shiftKey) {
+              onShiftSelect(mail.id)
+            } else {
+              onSelect(mail.id)
             }
+          }}
+        />
+        <div className="w-full min-w-0">
+          <span className="block text-sm text-bg-500">
+            {(() => {
+              if (mail.from.name) {
+                return mail.from.name
+              }
 
-            if (mail.from.address) {
-              return mail.from.address
-            }
+              if (mail.from.address) {
+                return mail.from.address
+              }
 
-            return 'No sender'
-          })()}
-        </span>
-        <div
-          className={`flex w-full min-w-0 items-center gap-2 text-lg ${
-            mail.seen ? 'font-medium' : 'font-bold'
-          }`}
-        >
-          <span className="min-w-0 truncate">
-            {mail.subject === '' ? 'No subject' : mail.subject}
+              return 'No sender'
+            })()}
           </span>
-          {!mail.seen && (
-            <>
-              <span className="rounded-full bg-custom-500/30 px-3 py-0.5 text-xs font-semibold text-custom-500">
-                New
-              </span>
-            </>
-          )}
-        </div>
+          <div
+            className={`flex w-full min-w-0 items-center gap-2 text-lg ${
+              mail.seen ? 'font-medium' : 'font-bold'
+            }`}
+          >
+            <span className="min-w-0 truncate">
+              {mail.subject === '' ? 'No subject' : mail.subject}
+            </span>
+            {!mail.seen && (
+              <>
+                <span className="rounded-full bg-custom-500/30 px-3 py-0.5 text-xs font-semibold text-custom-500">
+                  New
+                </span>
+              </>
+            )}
+          </div>
 
-        <p className="mt-1 truncate text-base font-light text-bg-500">
-          {mail.text.trim() || 'No content'}
-        </p>
+          <p className="mt-1 truncate text-base font-light text-bg-500">
+            {mail.text.trim() || 'No content'}
+          </p>
+        </div>
       </div>
       <div className="flex shrink-0 items-center gap-2">
         {mail.attachments?.length > 0 && (
@@ -82,6 +114,15 @@ function EntryItem({
             text="Archive"
             onClick={e => {
               e.stopPropagation()
+            }}
+          />
+          <MenuItem
+            isRed
+            icon="tabler:trash"
+            text="Delete"
+            onClick={e => {
+              e.stopPropagation()
+              onDelete(mail.id)
             }}
           />
         </HamburgerMenu>
