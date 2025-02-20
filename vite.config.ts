a8493e022fdc724ab2562742a711925b41d2ee61
melvinchia3636 <melvinchia623600@gmail.com>
2025-02-21 07:28:09 +0800

    25w08

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
+import mkcert from 'vite-plugin-mkcert'
 
 const ReactCompilerConfig = {
   sources: filename => {
@@ -17,7 +18,8 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]]
       }
     }),
-    tailwindcss()
+    tailwindcss(),
+    mkcert()
   ],
   resolve: {
     alias: {
