c499ac9cf6e8eddcbe51e8ad69d91e63af89fc29
melvinchia3636 <melvinchia623600@gmail.com>
2025-03-14 14:57:37 +0800

    25w11
    Former-commit-id: cfe1e793b5985b4f8131b89940e1083e49f3d96e [formerly 730c7f747d2cf9c1a3c0fff7e2e4b1f7e8cac106] [formerly 013aae7713d53a87c5fc9a16fef834660690117e [formerly c77f662ea1ddf8a24b368e55831b22db05033dd0]]
    Former-commit-id: 912feb30599ee0e80cffda9fbed8c9deabf8f79a [formerly d2c5dca0b795ca941ee594eec3a71f307b70fc9f]
    Former-commit-id: a1d6f58fb1f476f4d883c9318ff58066d3b3533b

--- src/modules/Calendar/components/Sidebar/components/MiniCalendar/index.tsx
+++ src/modules/Calendar/components/Sidebar/components/MiniCalendar/index.tsx
@@ -1,24 +1,41 @@
 import moment from 'moment'
 import { useState } from 'react'
 
+import { QueryWrapper } from '@lifeforge/ui'
+
 import { type Loadable } from '@interfaces/common'
+
+import useAPIQuery from '@hooks/useAPIQuery'
 
 import {
   type ICalendarCategory,
-  type ICalendarEvent
+  ICalendarEvent
 } from '../../../../interfaces/calendar_interfaces'
 import MiniCalendarContent from './components/MiniCalendarContent'
 import MiniCalendarHeader from './components/MiniCalendarHeader'
 
 function MiniCalendar({
-  events,
   categories
 }: {
-  events: ICalendarEvent[]
   categories: Loadable<ICalendarCategory[]>
 }) {
   const [currentMonth, setCurrentMonth] = useState(moment().month())
   const [currentYear, setCurrentYear] = useState(moment().year())
+  const startDate = moment()
+    .year(currentYear)
+    .month(currentMonth)
+    .startOf('month')
+    .format('YYYY-MM-DD')
+  const endDate = moment()
+    .year(currentYear)
+    .month(currentMonth)
+    .endOf('month')
+    .format('YYYY-MM-DD')
+
+  const eventsQuery = useAPIQuery<ICalendarEvent[]>(
+    `calendar/events?start=${startDate}&end=${endDate}`,
+    ['calendar', 'events', currentYear, currentMonth]
+  )
 
   return (
     <section className="flex w-full flex-col gap-4 p-8 pt-6">
@@ -29,12 +46,16 @@ function MiniCalendar({
           setCurrentMonth={setCurrentMonth}
           setCurrentYear={setCurrentYear}
         />
-        <MiniCalendarContent
-          categories={categories}
-          currentMonth={currentMonth}
-          currentYear={currentYear}
-          events={events}
-        />
+        <QueryWrapper query={eventsQuery}>
+          {events => (
+            <MiniCalendarContent
+              categories={categories}
+              currentMonth={currentMonth}
+              currentYear={currentYear}
+              events={events}
+            />
+          )}
+        </QueryWrapper>
       </div>
     </section>
   )
