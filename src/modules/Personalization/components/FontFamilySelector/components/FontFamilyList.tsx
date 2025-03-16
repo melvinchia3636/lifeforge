5d71ea154c1b19d2afcb0341a405c5c5a413a061
melvinchia3636 <melvinchia623600@gmail.com>
2025-03-16 16:15:29 +0800

    25w12
    Former-commit-id: c8495259c3fb77d1748e11639a4d4ff72301d067 [formerly 61212cad8b58a5d525aad85404a9fd5b3b4fd10d] [formerly 66285067ac45af349971ff9c4c5cb4769ba61974 [formerly a287cd31e730a1b367b26ba461ec567ab02fcb96]]
    Former-commit-id: c16901e82ac5fe19d26ad5904ef03a4ee06fbf26 [formerly f18f978ff03c5d6309d10caba68b2147f8ce9041]
    Former-commit-id: d8c59e476569c94d49c50028fa031acc4b1f4a53

---
+++ src/modules/Personalization/components/FontFamilySelector/components/FontFamilyList.tsx
@@ -0,0 +1,102 @@
+import { Listbox, ListboxButton, ListboxOptions } from '@headlessui/react'
+import { Icon } from '@iconify/react'
+import clsx from 'clsx'
+import { useTranslation } from 'react-i18next'
+
+import { Tooltip } from '@lifeforge/ui'
+
+import FontFamilyItem from './FontFamilyItem'
+
+interface FontFamilyListProps {
+  enabled: string | boolean
+  componentBgWithHover: string
+  fontFamily: string
+  setFontFamily: (font: string) => void
+  allFonts: any[]
+}
+
+function FontFamilyList({
+  enabled,
+  componentBgWithHover,
+  fontFamily,
+  setFontFamily,
+  allFonts
+}: FontFamilyListProps) {
+  const { t } = useTranslation('modules.personalization')
+
+  if (enabled === 'loading') {
+    return <Icon className="size-6 text-bg-500" icon="svg-spinners:180-ring" />
+  }
+
+  if (enabled) {
+    return (
+      <Listbox
+        value={fontFamily}
+        onChange={font => {
+          setFontFamily(font)
+        }}
+      >
+        <div className="relative mt-1 w-full md:w-64">
+          <ListboxButton
+            className={clsx(
+              'shadow-custom outline-hidden focus:outline-hidden flex w-full items-center gap-2 rounded-lg py-4 pl-4 pr-10 text-left transition-all',
+              componentBgWithHover
+            )}
+          >
+            <span
+              className="-mt-px block truncate"
+              style={{
+                fontFamily
+              }}
+            >
+              {fontFamily || (
+                <span className="text-bg-500">
+                  {t('fontFamily.pleaseSelect')}
+                </span>
+              )}
+            </span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <Icon className="text-bg-500 size-5" icon="tabler:chevron-down" />
+            </span>
+          </ListboxButton>
+          <ListboxOptions
+            transition
+            anchor="bottom end"
+            className="divide-bg-200 bg-bg-100 text-bg-800 dark:divide-bg-800 dark:border-bg-700 dark:bg-bg-900 dark:text-bg-50 focus:outline-hidden data-closed:scale-95 data-closed:opacity-0 h-72 w-80 divide-y rounded-md py-1 text-base shadow-lg transition duration-100 ease-out [--anchor-gap:8px]"
+          >
+            {allFonts.map(({ family }) => (
+              <FontFamilyItem key={family} family={family} />
+            ))}
+          </ListboxOptions>
+        </div>
+      </Listbox>
+    )
+  }
+
+  return (
+    <p className="text-bg-500 flex items-center gap-2">
+      {t('fontFamily.disabled.title')}
+      <Tooltip
+        icon="tabler:info-circle"
+        id="fontFamilyDisabled"
+        tooltipProps={{
+          clickable: true
+        }}
+      >
+        <p className="text-bg-500 max-w-84">
+          {t('fontFamily.disabled.tooltip')}{' '}
+          <a
+            className="font-medium underline text-custom-500 decoration-custom-500 decoration-2"
+            href="https://docs.lifeforge.melvinchia.dev/user-guide/personalization#font-family"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Customization Guide
+          </a>
+        </p>
+      </Tooltip>
+    </p>
+  )
+}
+
+export default FontFamilyList
