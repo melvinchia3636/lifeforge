80adc5a7110b1454ff448780e2c3b096597c04c4
melvinchia3636 <melvinchia623600@gmail.com>
2025-03-18 11:39:05 +0800

    25w12

--- src/modules/Notes/Subject/components/Directory/components/EntryItem/components/EntryName.tsx
+++
@@ -1,9 +0,0 @@
-function EntryName({ name }: { name: string }) {
-  return (
-    <div className="pointer-events-none z-50 w-80 truncate text-lg font-medium">
-      {name}
-    </div>
-  )
-}
-
-export default EntryName
