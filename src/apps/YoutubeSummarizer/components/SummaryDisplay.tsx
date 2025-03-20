cf2ec976ef75ea1758830a98c596e1e0cdaa1bf2
melvinchia3636 <melvinchia623600@gmail.com>
2025-03-20 09:38:07 +0800

    25w12
    Former-commit-id: aad04b1ecaad3e2f64d3d4056257749a7962d962 [formerly 2c37588ed72636714c8691fa04272027ba8010cb] [formerly 84fda99bc1ad1f608c4f934f9058b3a8d574ed89 [formerly fffdc29befba93e1d64498a90dee02a8580a1370]]
    Former-commit-id: a379197eaa594ee2888cb43c77996e82fb6a091f [formerly 2636e870da7852b24b1037264959b01e9a905b8b]
    Former-commit-id: bf19081480f830d41b6539f0a149a06784258b4d

---
+++ src/apps/YoutubeSummarizer/components/SummaryDisplay.tsx
@@ -0,0 +1,26 @@
+import Markdown from 'react-markdown'
+
+import { DashboardItem } from '@lifeforge/ui'
+
+interface SummaryDisplayProps {
+  summary: string | null
+}
+
+function SummaryDisplay({ summary }: SummaryDisplayProps) {
+  if (!summary) return null
+
+  return (
+    <DashboardItem
+      className="mt-6 h-min"
+      icon="tabler:file-text"
+      namespace="apps.youtubeSummarizer"
+      title="Video Summary"
+    >
+      <div className="text-bg-500 prose max-w-full!">
+        <Markdown>{summary}</Markdown>
+      </div>
+    </DashboardItem>
+  )
+}
+
+export default SummaryDisplay
