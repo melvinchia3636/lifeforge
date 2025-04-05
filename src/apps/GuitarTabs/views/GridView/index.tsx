d18d7947fc0b01b3416b0395a7c4f09217034f3c
melvinchia3636 <melvinchia623600@gmail.com>
2025-04-05 11:48:58 +0800

    25w14

--- src/apps/GuitarTabs/views/GridView/index.tsx
+++ src/apps/GuitarTabs/views/GridView/index.tsx
@@ -15,7 +15,7 @@ function GridView({
   queryKey: unknown[]
 }) {
   return (
-    <div className="mb-6 grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] gap-4">
+    <div className="mb-6 grid grid-cols-[repeat(auto-fill,minmax(12rem,1fr))] gap-4 sm:grid-cols-[repeat(auto-fill,minmax(16rem,1fr))]">
       {entries.map(entry => (
         <EntryItem
           key={entry.id}
