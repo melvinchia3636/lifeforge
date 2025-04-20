8729c5257355601743b0d20622d5df2578b367b5
Melvin Chia <melvinchia623600@gmail.com>
2025-04-20 20:44:04 +0800

    25w17
    Former-commit-id: ebe2629f0c99d7e42fab6e0dc6513f29b3e07c5c [formerly eb0bd829cb9eced2a60a271bfdc8f07c418bba23] [formerly 4d292dc124caf0911ef5e9bb68d99d9149bad605 [formerly 87157451e9a78a5eb9593a8207c23d3fd0bb6b2e]]
    Former-commit-id: daa5e78f9479f64961049c5c7766584a33a4bd04 [formerly 9f3af0fa6db8b1ad011f4e47fad93d5eaa5c52a9]
    Former-commit-id: 64b90cd791405818b47664a4bafe46aca3e49b96

---
+++ src/apps/GuitarTabs/components/Sidebar/components/SidebarAuthorItem.tsx
@@ -0,0 +1,44 @@
+import { memo, useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { SidebarItem } from '@lifeforge/ui'
+
+function SidebarAuthorItem({
+  author,
+  count,
+  isActive,
+  onCancel,
+  onSelect
+}: {
+  author: string | null
+  count: number
+  isActive: boolean
+  onCancel: () => void
+  onSelect: (author: string | null) => void
+}) {
+  const { t } = useTranslation('apps.guitarTabs')
+
+  const handleClick = useCallback(() => {
+    onSelect(author)
+  }, [])
+
+  return (
+    <SidebarItem
+      active={isActive}
+      autoActive={false}
+      icon="tabler:user"
+      name={author || t('unknownAuthor')}
+      number={count}
+      onCancelButtonClick={onCancel}
+      onClick={handleClick}
+    />
+  )
+}
+
+export default memo(SidebarAuthorItem, (prevProps, nextProps) => {
+  return (
+    prevProps.author === nextProps.author &&
+    prevProps.count === nextProps.count &&
+    prevProps.isActive === nextProps.isActive
+  )
+})
