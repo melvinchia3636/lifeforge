70775f615eab481d50dde46efe24dbcdf07d86b5
Melvin Chia <melvinchia623600@gmail.com>
2025-05-18 00:45:39 +0800

    25w20
    Former-commit-id: 0dba47a62581de303a4a3d139b26bc2838b69be6 [formerly 12aef1108cb3a29a92aa4f0fbb4d022b0cc7b02a] [formerly 3ffae5127943227b516f5ca5f223634734dfb2fe [formerly d5dfb9747aa9eec1112c526c6b29f594f4e91f35]]
    Former-commit-id: 23b92a85c5a9965745215bf203259db2751769e8 [formerly 109fcc0f8e56d75adf91d03b636de5d885fd0295]
    Former-commit-id: 72b523e0498a7b1676c814b86338013bba79864d

--- src/apps/Calendar/index.tsx
+++ src/apps/Calendar/index.tsx
@@ -95,7 +95,7 @@ function CalendarModule() {
           <MenuItems
             transition
             anchor="bottom end"
-            className="bg-bg-100 dark:bg-bg-800 w-48 overflow-hidden overscroll-contain rounded-md shadow-lg outline-hidden transition duration-100 ease-out [--anchor-gap:8px] focus:outline-hidden data-closed:scale-95 data-closed:opacity-0"
+            className="bg-bg-100 dark:bg-bg-800 overflow-hidden overscroll-contain rounded-md shadow-lg outline-hidden transition duration-100 ease-out [--anchor-gap:8px] focus:outline-hidden data-closed:scale-95 data-closed:opacity-0"
           >
             <MenuItem
               icon="tabler:photo"
