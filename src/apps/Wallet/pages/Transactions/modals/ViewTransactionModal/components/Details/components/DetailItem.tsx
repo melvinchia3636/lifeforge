439c861ccc4bd0550d6bb282ce96d1cf0501a2e7
Melvin Chia <melvinchia623600@gmail.com>
2025-07-04 23:00:27 +0800

    25w27

---
+++ src/apps/Wallet/pages/Transactions/modals/ViewTransactionModal/components/Details/components/DetailItem.tsx
@@ -0,0 +1,38 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
+import clsx from 'clsx'
+import { useTranslation } from 'react-i18next'
+
+import useComponentBg from '@hooks/useComponentBg'
+
+function DetailItem({
+  icon,
+  name,
+  vertical = false,
+  children
+}: {
+  icon: string
+  name: string
+  vertical?: boolean
+  children: React.ReactElement
+}) {
+  const { t } = useTranslation('apps.wallet')
+  const { componentBgLighter } = useComponentBg()
+
+  return (
+    <div
+      className={clsx(
+        'rounded-lg p-4',
+        componentBgLighter,
+        vertical ? 'space-y-4' : 'flex-between'
+      )}
+    >
+      <div className="text-bg-500 flex items-center gap-3">
+        <Icon className="size-6" icon={icon} />
+        <h3 className="text-lg font-medium">{t(`inputs.${name}`)}</h3>
+      </div>
+      {children}
+    </div>
+  )
+}
+
+export default DetailItem
