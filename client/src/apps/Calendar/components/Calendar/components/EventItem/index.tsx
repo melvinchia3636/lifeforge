f91160d78e21520f92c8ee510f179259e6c31f8c
Melvin Chia <melvinchia623600@gmail.com>
2025-07-17 16:22:37 +0800

    25w29
    Former-commit-id: 34a10295249ce0afc8b392a73a9a932947f1d014 [formerly 7b59b20157dbfe7ddd2b9fe73589ef00a6720719] [formerly 31b8c6616e29ecbd359dc8325af54216476441d9 [formerly bc5f3c2b4b2f6a38ab2fe5c4d8e06a30afcde22a]]
    Former-commit-id: ba7ffec9b69f54964598a12c6d8b9f133cb4339e [formerly 1c7b06e1477331a33f2dcc4edddebb3b4315068d]
    Former-commit-id: b44438d44b4aaf32e3b5fbe5aed41e5ed9bd627f

---
+++ client/src/apps/Calendar/components/Calendar/components/EventItem/index.tsx
@@ -0,0 +1,45 @@
+import { memo, useMemo } from "react";
+
+import { INTERNAL_CATEGORIES } from "@apps/Calendar/constants/internalCategories";
+
+import useAPIQuery from "@hooks/useAPIQuery.ts";
+
+import {
+  type ICalendarCategory,
+  type ICalendarEvent,
+} from "../../../../interfaces/calendar_interfaces.js";
+import EventItemButton from "./components/EventItemButton.js";
+import EventItemTooltip from "./components/EventItemTooltip.js";
+
+function EventItem({ event }: { event: ICalendarEvent }) {
+  const categoriesQuery = useAPIQuery<ICalendarCategory[]>(
+    "calendar/categories",
+    ["calendar", "categories"]
+  );
+  const category = useMemo<ICalendarCategory | undefined>(() => {
+    if (event.category.startsWith("_")) {
+      return (INTERNAL_CATEGORIES[
+        event.category as keyof typeof INTERNAL_CATEGORIES
+      ] ?? {}) as ICalendarCategory;
+    }
+
+    return categoriesQuery.data?.find(
+      (category) => category.id === event.category
+    );
+  }, [categoriesQuery, event.category]);
+
+  return (
+    <>
+      <EventItemButton
+        color={category?.color ?? ""}
+        icon={category?.icon ?? ""}
+        id={event.id}
+        isStrikethrough={event.is_strikethrough}
+        title={event.title}
+      />
+      <EventItemTooltip category={category} event={event} />
+    </>
+  );
+}
+
+export default memo(EventItem);
---
+++ client/src/core/routes/components/ChildRoutesRenderer.tsx
@@ -0,0 +1,43 @@
+import { Suspense } from "react";
+import { Route } from "react-router";
+
+import { LoadingScreen, ModalManager } from "@lifeforge/ui";
+
+import { ModuleConfig } from "../interfaces/routes_interfaces";
+import APIKeyStatusProvider from "../providers/APIKeyStatusProvider";
+
+function ChildRoutesRenderer({
+  routes,
+  isNested = false,
+  APIKeys = [],
+  t,
+}: {
+  routes: ModuleConfig["routes"];
+  t: any;
+  isNested?: boolean;
+  APIKeys?: string[];
+}) {
+  return Object.entries(routes).map(([path, component]) => {
+    const Comp = component;
+
+    return (
+      <Route
+        key={`path-${path}`}
+        element={
+          <APIKeyStatusProvider APIKeys={APIKeys}>
+            <Suspense
+              key={`path-${path}`}
+              fallback={<LoadingScreen customMessage={t("loadingModule")} />}
+            >
+              <Comp />
+            </Suspense>
+            <ModalManager />
+          </APIKeyStatusProvider>
+        }
+        path={(!isNested ? "/" : "") + path}
+      />
+    );
+  });
+}
+
+export default ChildRoutesRenderer;
