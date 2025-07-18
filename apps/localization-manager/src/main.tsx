96a09dcd7892544c962186f007ce55a329730763
Melvin Chia <melvinchia623600@gmail.com>
2025-07-18 14:12:25 +0800

    25w29
    Former-commit-id: bdf82f227358a0008c6c31bd7720ed3fdfef7187 [formerly efe4ce0578216d46ccb9d1cb9cc90fb41abc60b8] [formerly b527b3181e8a2e383e56f3d020f51045b0b5ea84 [formerly 0febed1bb9af5358b046d0d7d597f310e2714e07]]
    Former-commit-id: e68094869d611fb1f6f1b66abcc3a8b3fcdcf5ac [formerly b9e02669dd42b2b0fbddfed13356a2773c33c502]
    Former-commit-id: 9c7458a944b8872d4e3170676ceb838723402092

--- apps/localization-manager/src/main.tsx
+++ apps/localization-manager/src/main.tsx
@@ -1,20 +1,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
 import { APIEndpointProvider, PersonalizationProvider } from 'shared/lib'
 
 import './index.css'
 
 import App from './App.tsx'
-import './i18n'
+import i18n from './i18n'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <APIEndpointProvider endpoint={import.meta.env.VITE_API_HOST}>
     <QueryClientProvider client={queryClient}>
-      <PersonalizationProvider>
-        <App />
-      </PersonalizationProvider>
+      <I18nextProvider i18n={i18n}>
+        <PersonalizationProvider>
+          <App />
+        </PersonalizationProvider>
+      </I18nextProvider>
     </QueryClientProvider>
   </APIEndpointProvider>
 )
