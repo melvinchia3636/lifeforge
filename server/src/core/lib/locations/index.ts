a6184e182e9181b7209a8c619bd6d3a79bf4e3ea
melvinchia3636 <melvinchia623600@gmail.com>
2025-07-20 07:24:18 +0800

    25w29

--- server/src/core/lib/locations/index.ts
+++ server/src/core/lib/locations/index.ts
@@ -8,6 +8,8 @@ import express from 'express'
 
 import { LocationsControllersSchemas } from 'shared/types/controllers'
 
+import * as LocationsService from './services/locations.service'
+
 const router = express.Router()
 
 const getLocation = forgeController
@@ -21,42 +23,7 @@ const getLocation = forgeController
       throw new ClientError('API key not found')
     }
 
-    const response = await fetch(
-      `https://places.googleapis.com/v1/places:searchText`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Goog-FieldMask':
-            'places.displayName,places.location,places.formattedAddress',
-          'X-Goog-Api-Key': key
-        },
-        body: JSON.stringify({
-          textQuery: q
-        })
-      }
-    ).then(res => res.json())
-
-    const places: {
-      displayName: {
-        languageCode: string
-        text: string
-      }
-      formattedAddress: string
-      location: {
-        latitude: number
-        longitude: number
-      }
-    }[] = response.places ?? []
-
-    return places.map(place => ({
-      name: place.displayName.text,
-      formattedAddress: place.formattedAddress,
-      location: {
-        latitude: place.location.latitude,
-        longitude: place.location.longitude
-      }
-    }))
+    return LocationsService.searchLocations(q, key)
   })
 
 const checkIsEnabled = forgeController
