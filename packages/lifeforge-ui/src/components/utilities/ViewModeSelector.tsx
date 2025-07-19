0976f508d87a9d43cc18d9c39b58091be19ca7da
Melvin Chia <melvinchia623600@gmail.com>
2025-07-19 18:58:55 +0800

    25w29

--- packages/lifeforge-ui/src/components/utilities/ViewModeSelector.tsx
+++ packages/lifeforge-ui/src/components/utilities/ViewModeSelector.tsx
@@ -15,7 +15,7 @@ function ViewModeSelector<T extends string>({
   return (
     <div
       className={clsx(
-        'shadow-custom component-bg mt-4 flex items-center gap-2 rounded-md p-2',
+        'shadow-custom component-bg flex items-center gap-2 rounded-md p-2',
         className
       )}
     >
