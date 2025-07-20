5ecce8eedea280e21f82a4c53a5a3787d578d58d
Melvin Chia <melvinchia623600@gmail.com>
2025-07-20 17:37:02 +0800

    25w29
    Former-commit-id: 822bfe2ec80da17600c6aea3712456a56957a6a6 [formerly dbf14903b3b8a1f6ea405f576c349ab540b49d10] [formerly 79a665b2984fb0be722d710d482348d3e3bdb704 [formerly 82f4612422f303273468e378a1a3127a20b2a156]]
    Former-commit-id: 3538579d6e4302e29f704cda63eba5d86e624145 [formerly b9c089a7d1065d108bc8b1a6b38034d622730614]
    Former-commit-id: 03cf36bbcd81149b42db12937b80776e94212f45

---
+++ client/src/apps/Wallet/pages/Dashboard/components/TransactionsCard/components/TransactionParticular.tsx
@@ -0,0 +1,33 @@
+import { useWalletData } from '@apps/Wallet/hooks/useWalletData'
+import { IWalletTransaction } from '@apps/Wallet/pages/Transactions'
+
+function TransactionParticular({
+  transaction
+}: {
+  transaction: IWalletTransaction
+}) {
+  const { assetsQuery } = useWalletData()
+
+  const assets = assetsQuery.data ?? []
+
+  return (
+    <>
+      {transaction.type === 'transfer' ? (
+        `Transfer from ${
+          assets.find(asset => asset.id === transaction.from)?.name
+        } to ${assets.find(asset => asset.id === transaction.to)?.name}`
+      ) : (
+        <>
+          {transaction.particulars}{' '}
+          {transaction.location_name && (
+            <>
+              <span className="text-bg-500">@</span> {transaction.location_name}
+            </>
+          )}
+        </>
+      )}
+    </>
+  )
+}
+
+export default TransactionParticular
