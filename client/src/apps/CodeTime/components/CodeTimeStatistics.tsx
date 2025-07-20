367bb04989578736d2985a3f82844c36c58b66c5
Melvin Chia <melvinchia623600@gmail.com>
2025-07-20 23:44:40 +0800

    25w30
    Former-commit-id: c253fe13b94f1aef0289f6d2b6b2dd5e41b98db9 [formerly 21b132cd930705b9b14e3d8689a385c7e5223958] [formerly ccc9efdb0581c36a0f016f1efb988f027d1ac53d [formerly dabe74cd133f4ab61d1776291058170c78ba2974]]
    Former-commit-id: 8285c6386bed6a765361e22c0bac9917ad02bc13 [formerly 4c543357013bb5e2c3ffa302d89ed821a6322d30]
    Former-commit-id: 196ca0b3eaedffb779b500be61196dd88d828359

--- client/src/apps/CodeTime/components/CodeTimeStatistics.tsx
+++ client/src/apps/CodeTime/components/CodeTimeStatistics.tsx
@@ -24,51 +24,73 @@ function CodeTimeStatistics() {
       </h1>
       <QueryWrapper query={statsQuery}>
         {stats => (
-          <div className="grid grid-cols-[repeat(auto-fit,minmax(14rem,1fr))] gap-3">
-            {Object.entries(stats).map(([key, value], index) => (
-              <div
-                key={key}
-                className="flex-between shadow-custom component-bg flex w-full flex-col gap-2 rounded-lg p-3 sm:items-start sm:p-6"
-              >
-                <div className="flex w-full flex-row items-center gap-2 sm:flex-col sm:items-start">
-                  <div className="shadow-custom component-bg-lighter flex rounded-lg p-2 sm:p-4">
-                    <Icon
-                      className={clsx(
-                        'text-2xl sm:text-3xl',
-                        index === 3
-                          ? 'text-orange-300'
-                          : 'text-bg-500 dark:text-bg-50'
-                      )}
-                      icon={
-                        {
-                          'Most time spent': 'tabler:coffee',
-                          'Total time spent': 'tabler:clock',
-                          'Average time spent': 'tabler:wave-saw-tool',
-                          'Longest streak': 'tabler:flame',
-                          'Current streak': 'tabler:flame'
-                        }[key]!
-                      }
-                    />
-                  </div>
-                  <div className="text-bg-500 text-lg whitespace-nowrap">
-                    {t(`statisticType.${_.camelCase(key)}`)}
-                  </div>
+          <>
+            <div className="flex-between component-bg shadow-custom w-full gap-6 rounded-lg p-6">
+              <div className="flex items-center gap-4">
+                <div className="shadow-custom component-bg-lighter flex rounded-lg p-2 sm:p-4">
+                  <Icon
+                    className="text-bg-500 dark:text-bg-50 text-2xl sm:text-3xl"
+                    icon="tabler:calendar"
+                  />
                 </div>
-                <div className="mt-2 text-4xl font-semibold whitespace-nowrap">
-                  {index < 3 ? (
-                    <HoursAndMinutesFromSeconds seconds={value} />
-                  ) : (
-                    <>
-                      {value}
-                      <span className="text-bg-500 pl-1 text-3xl font-normal">
-                        {t('units.days')}
-                      </span>
-                    </>
-                  )}
+                <div className="text-bg-500 text-xl font-medium whitespace-nowrap">
+                  {t('statisticType.timeSpentToday')}
                 </div>
               </div>
-            ))}
-          </div>
+              <div className="text-4xl font-semibold">
+                <HoursAndMinutesFromSeconds
+                  seconds={stats['Time spent today']}
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(14rem,1fr))] gap-3">
+              {Object.entries(stats)
+                .slice(0, -1)
+                .map(([key, value], index) => (
+                  <div
+                    key={key}
+                    className="flex-between shadow-custom component-bg flex w-full flex-col gap-2 rounded-lg p-3 sm:items-start sm:p-6"
+                  >
+                    <div className="flex w-full flex-row items-center gap-2 sm:flex-col sm:items-start">
+                      <div className="shadow-custom component-bg-lighter flex rounded-lg p-2 sm:p-4">
+                        <Icon
+                          className={clsx(
+                            'text-2xl sm:text-3xl',
+                            index === 3
+                              ? 'text-orange-300'
+                              : 'text-bg-500 dark:text-bg-50'
+                          )}
+                          icon={
+                            {
+                              'Most time spent': 'tabler:coffee',
+                              'Total time spent': 'tabler:clock',
+                              'Average time spent': 'tabler:wave-saw-tool',
+                              'Longest streak': 'tabler:flame',
+                              'Current streak': 'tabler:flame'
+                            }[key]!
+                          }
+                        />
+                      </div>
+                      <div className="text-bg-500 text-lg whitespace-nowrap">
+                        {t(`statisticType.${_.camelCase(key)}`)}
+                      </div>
+                    </div>
+                    <div className="mt-2 text-4xl font-semibold whitespace-nowrap">
+                      {index < 3 ? (
+                        <HoursAndMinutesFromSeconds seconds={value} />
+                      ) : (
+                        <>
+                          {value}
+                          <span className="text-bg-500 pl-1 text-3xl font-normal">
+                            {t('units.days')}
+                          </span>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </>
         )}
       </QueryWrapper>
     </div>
