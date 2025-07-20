51d9f3d7b7eac3199568151bc71d1ed5a546bdf9
melvinchia3636 <melvinchia623600@gmail.com>
2025-07-20 17:51:00 +0800

    25w30

--- server/src/apps/wallet/controllers/assets.controller.ts
+++ server/src/apps/wallet/controllers/assets.controller.ts
@@ -23,6 +23,18 @@ const createAsset = forgeController
   .statusCode(201)
   .callback(async ({ pb, body }) => await AssetsService.createAsset(pb, body))
 
+const getAssetAccumulatedBalance = forgeController
+  .route('GET /balance/:id')
+  .description('Get accumulated balance for a wallet asset')
+  .schema(WalletControllersSchemas.Assets.getAssetAccumulatedBalance)
+  .existenceCheck('params', {
+    id: 'wallet__assets'
+  })
+  .callback(
+    async ({ pb, params: { id } }) =>
+      await AssetsService.getAssetAccumulatedBalance(pb, id)
+  )
+
 const updateAsset = forgeController
   .route('PATCH /:id')
   .description('Update an existing wallet asset')
@@ -49,6 +61,7 @@ const deleteAsset = forgeController
 
 bulkRegisterControllers(walletAssetsRouter, [
   getAllAssets,
+  getAssetAccumulatedBalance,
   createAsset,
   updateAsset,
   deleteAsset
--- server/src/apps/wallet/services/assets.service.ts
+++ server/src/apps/wallet/services/assets.service.ts
@@ -1,3 +1,5 @@
+import Moment from 'moment'
+import MomentRange from 'moment-range'
 import PocketBase from 'pocketbase'
 
 import { ISchemaWithPB } from 'shared/types/collections'
@@ -11,6 +13,98 @@ export const getAllAssets = (
     .getFullList<ISchemaWithPB<WalletCollectionsSchemas.IAssetAggregated>>({
       sort: 'name'
     })
+
+// @ts-expect-error Hmmmmmm
+const moment = MomentRange.extendMoment(Moment)
+
+export const getAssetAccumulatedBalance = async (
+  pb: PocketBase,
+  assetId: string
+): Promise<Record<string, number>> => {
+  const { starting_balance } = await pb
+    .collection('wallet__assets')
+    .getOne<
+      Pick<ISchemaWithPB<WalletCollectionsSchemas.IAsset>, 'starting_balance'>
+    >(assetId, {
+      fields: 'starting_balance'
+    })
+
+  const allIncomeExpensesTransactions = await pb
+    .collection('wallet__transactions')
+    .getFullList<
+      Pick<
+        ISchemaWithPB<WalletCollectionsSchemas.ITransaction>,
+        'amount' | 'date' | 'type'
+      >
+    >({
+      filter: `asset = '${assetId}'`,
+      sort: '-date',
+      fields: 'amount,date,type'
+    })
+
+  const allTransferTransactions = await pb
+    .collection('wallet__transactions_transfer')
+    .getFullList<
+      Pick<
+        ISchemaWithPB<WalletCollectionsSchemas.ITransactionsTransfer>,
+        'from' | 'to'
+      > & {
+        expand: {
+          base_transaction: Pick<
+            ISchemaWithPB<WalletCollectionsSchemas.ITransaction>,
+            'amount' | 'date'
+          >
+        }
+      }
+    >({
+      filter: `from = '${assetId}' || to = '${assetId}'`,
+      sort: '-base_transaction.date',
+      expand: 'base_transaction',
+      fields: 'base_transaction.amount,base_transaction.date,from,to'
+    })
+
+  let currentBalance = starting_balance
+
+  const accumulatedBalance: Record<string, number> = {}
+
+  const allDateInBetween = moment
+    .range(
+      moment(
+        allIncomeExpensesTransactions[allIncomeExpensesTransactions.length - 1]
+          .date
+      ),
+      moment()
+    )
+    .by('day')
+
+  for (const date of allDateInBetween) {
+    const dateStr = date.format('YYYY-MM-DD')
+
+    accumulatedBalance[dateStr] = parseFloat(currentBalance.toFixed(2))
+
+    const transactionsOnDate = [
+      ...allIncomeExpensesTransactions.filter(t =>
+        moment(t.date).isSame(date, 'day')
+      ),
+      ...allTransferTransactions
+        .filter(t => moment(t.expand.base_transaction.date).isSame(date, 'day'))
+        .map(t => ({
+          type: t.from === assetId ? 'expenses' : 'income',
+          amount: t.expand.base_transaction.amount
+        }))
+    ]
+
+    for (const transaction of transactionsOnDate) {
+      if (transaction.type === 'expenses') {
+        currentBalance += transaction.amount
+      } else if (transaction.type === 'income') {
+        currentBalance -= transaction.amount
+      }
+    }
+  }
+
+  return accumulatedBalance
+}
 
 export const createAsset = (
   pb: PocketBase,
