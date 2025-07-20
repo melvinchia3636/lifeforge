9076f360c5040fa24446e801c68ebef9dbde32ba
melvinchia3636 <melvinchia623600@gmail.com>
2025-07-20 13:30:02 +0800

    25w29
    Former-commit-id: c8208d3541c9d4d9c60133efa79bd77115db16e9 [formerly f58315a348bfeb7a41c4c7296ece8a6ac02e5f77] [formerly 6d20f3393459c184d843cbde590d8146ede65aa8 [formerly c593ce1175d2c016320d7ff9815eba4552ff95f8]]
    Former-commit-id: 9cc33fd835b00bb317c34888b3166630722254c8 [formerly e045075471638fc25e4a44d079891c7271bfcb1e]
    Former-commit-id: d07d8476ea497e2bc6cef498a14ba065e407fdeb

--- shared/src/types/collections/blog.schema.ts
+++ shared/src/types/collections/blog.schema.ts
@@ -3,7 +3,7 @@
  * You may regenerate it by running `bun run schema:generate:collection` in the root directory.
  * If you want to add custom schemas, you will find a dedicated space at the end of this file.
  * Generated for module: blog
- * Generated at: 2025-07-20T05:23:51.733Z
+ * Generated at: 2025-07-20T05:29:45.172Z
  * Contains: entry, category
  */
 
