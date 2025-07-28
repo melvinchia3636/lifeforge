21216b2e707e3bd5009a428fce635f8169a9d282
Melvin Chia <melvinchia623600@gmail.com>
2025-07-28 11:01:23 +0800

    25w31

--- server/src/apps/ideaBox/routes/ideas.ts
+++ server/src/apps/ideaBox/routes/ideas.ts
@@ -1,8 +1,8 @@
 import { SchemaWithPB } from '@functions/database/PBService/typescript/pb_service'
+import getMedia from '@functions/external/media'
 import { forgeController, forgeRouter } from '@functions/routes'
 import { ClientError } from '@functions/routes/utils/response'
 import COLLECTION_SCHEMAS, { SCHEMAS } from '@schema'
-import fs from 'fs'
 import multer from 'multer'
 import { z } from 'zod/v4'
 
@@ -157,119 +157,192 @@ const list = forgeController.query
     ] as Array<SchemaWithPB<z.infer<typeof _returnSchema>>>
   })
 
+const createSchema = SCHEMAS.idea_box.entries
+  .omit({
+    created: true,
+    updated: true,
+    type: true
+  })
+  .and(
+    z.union([
+      SCHEMAS.idea_box.entries_text
+        .omit({
+          base_entry: true
+        })
+        .extend({
+          type: z.literal('text')
+        }),
+      SCHEMAS.idea_box.entries_image
+        .omit({
+          base_entry: true,
+          image: true
+        })
+        .extend({
+          type: z.literal('image')
+        }),
+      SCHEMAS.idea_box.entries_link
+        .omit({
+          base_entry: true
+        })
+        .extend({
+          type: z.literal('link')
+        })
+    ])
+  )
+
 const create = forgeController.mutation
   .description('Create a new idea')
   .input({
-    body: SCHEMAS.idea_box.entries
-      .pick({
-        type: true,
-        container: true,
-        tags: true
-      })
-      .extend({
-        imageLink: z.string().optional(),
-        tags: z.string().transform(val => JSON.parse(val))
-      })
+    body: createSchema
+  })
+  .media({
+    image: {
+      optional: true
+    }
   })
   .middlewares(multer().single('image'))
   .existenceCheck('body', {
-    container: 'idea_box__containers'
+    container: 'idea_box__containers',
+    folder: 'idea_box__folders'
   })
   .statusCode(201)
-  .callback(async ({ pb, body: { type, container, imageLink, tags }, req }) => {
-    const { file } = req
+  .callback(async ({ pb, body: rawBody, media: { image } }) => {
+    const body = rawBody as z.infer<typeof createSchema>
 
-    const data = {
-      title: '',
-      content: '',
-      type,
-      container,
-      folder,
-      tags: tags || null,
-      image: null as File | null
+    const baseEntry = await pb.create
+      .collection('idea_box__entries')
+      .data({
+        container: body.container,
+        folder: body.folder,
+        type: body.type,
+        tags: body.tags
+      })
+      .execute()
+
+    if (body.type === 'text') {
+      await pb.create
+        .collection('idea_box__entries_text')
+        .data({
+          base_entry: baseEntry.id,
+          content: body.content
+        })
+        .execute()
+    } else if (body.type === 'image') {
+      if (!image) {
+        throw new ClientError('Image is required for image entries')
+      }
+
+      const imageData = await getMedia('idea', image)
+
+      await pb.create
+        .collection('idea_box__entries_image')
+        .data({
+          base_entry: baseEntry.id,
+          image: imageData
+        })
+        .execute()
+    } else if (body.type === 'link') {
+      await pb.create
+        .collection('idea_box__entries_link')
+        .data({
+          base_entry: baseEntry.id,
+          link: body.link
+        })
+        .execute()
     }
-
-    switch (type) {
-      case 'text':
-      case 'link':
-        data['title'] = title
-        data['content'] = content
-        break
-      case 'image':
-        if (imageLink) {
-          const response = await fetch(imageLink)
-
-          const buffer = await response.arrayBuffer()
-
-          data['image'] = new File([buffer], 'image.jpg', {
-            type: 'image/jpeg'
-          })
-          data['title'] = title
-        } else {
-          if (!file) {
-            throw new ClientError('Image file is required for image type ideas')
-          }
-          data['image'] = new File(
-            [fs.readFileSync(file.path)],
-            file.originalname,
-            {
-              type: file.mimetype
-            }
-          )
-          data['title'] = title
-        }
-        break
-    }
-
-    return pb.create.collection('idea_box__entries').data(data).execute()
   })
+
+const updateSchema = SCHEMAS.idea_box.entries
+  .omit({
+    created: true,
+    updated: true,
+    type: true,
+    folder: true,
+    container: true
+  })
+  .and(
+    z.union([
+      SCHEMAS.idea_box.entries_text
+        .omit({
+          base_entry: true
+        })
+        .extend({
+          type: z.literal('text')
+        }),
+      SCHEMAS.idea_box.entries_image
+        .omit({
+          base_entry: true,
+          image: true
+        })
+        .extend({
+          type: z.literal('image')
+        }),
+      SCHEMAS.idea_box.entries_link
+        .omit({
+          base_entry: true
+        })
+        .extend({
+          type: z.literal('link')
+        })
+    ])
+  )
 
 const update = forgeController.mutation
   .description('Update an idea')
   .input({
-    query: z.object({
-      id: z.string()
-    }),
-    body: z.object({
-      title: z.string().optional(),
-      content: z.string().optional(),
-      type: z.enum(['text', 'link', 'image']),
-      tags: z.array(z.string()).optional()
-    })
+    body: updateSchema
+  })
+  .media({
+    image: {
+      optional: true
+    }
   })
   .existenceCheck('query', {
     id: 'idea_box__entries'
   })
-  .callback(
-    async ({ pb, query: { id }, body: { title, content, type, tags } }) => {
-      let data
+  .callback(async ({ pb, body: rawBody, media: { image } }) => {
+    const body = rawBody as z.infer<typeof createSchema>
 
-      switch (type) {
-        case 'text':
-        case 'link':
-          data = {
-            title,
-            content,
-            type,
-            tags: tags || null
-          }
-          break
-        case 'image':
-          data = {
-            title,
-            type,
-            tags: tags || null
-          }
-          break
+    const baseEntry = await pb.update
+      .collection('idea_box__entries')
+      .data({
+        type: body.type,
+        tags: body.tags
+      })
+      .execute()
+
+    if (body.type === 'text') {
+      await pb.update
+        .collection('idea_box__entries_text')
+        .data({
+          base_entry: baseEntry.id,
+          content: body.content
+        })
+        .execute()
+    } else if (body.type === 'image') {
+      if (!image) {
+        throw new ClientError('Image is required for image entries')
       }
 
-      return pb.update
-        .collection('idea_box__entries')
-        .id(id)
-        .data(data)
+      const imageData = await getMedia('idea', image)
+
+      await pb.update
+        .collection('idea_box__entries_image')
+        .data({
+          base_entry: baseEntry.id,
+          image: imageData
+        })
+        .execute()
+    } else if (body.type === 'link') {
+      await pb.update
+        .collection('idea_box__entries_link')
+        .data({
+          base_entry: baseEntry.id,
+          link: body.link
+        })
         .execute()
     }
-  )
+  })
 
 const remove = forgeController.mutation
   .description('Delete an idea')
