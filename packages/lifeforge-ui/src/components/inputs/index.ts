ae3e7e0cc5d4b146a94044e02d4d7ef7a8bf6032
Melvin Chia <melvinchia623600@gmail.com>
2025-07-31 22:31:23 +0800

    25w31
    Former-commit-id: 346b5b7d78e2a3d31b0587b45488bc59d40d0573 [formerly 1c537850334cf50425839250aa200642898a96e1] [formerly 50f13d233401af922ce7c2a6e9938fbf4a75dcd1 [formerly 580bd470905b380f4fa6a73f9385baa30eab360b]]
    Former-commit-id: ee2b0484e8b38529bdd8a3a00ae54e653f6a97cd [formerly 5f5a5ec806571a255ec88d1c033b9794961dba35]
    Former-commit-id: 06a5fd4de2e21bbf952e16aef31f4148f3f0a2aa

--- packages/lifeforge-ui/src/components/inputs/index.ts
+++ packages/lifeforge-ui/src/components/inputs/index.ts
@@ -18,17 +18,19 @@ export { default as SearchInput } from './SearchInput'
 
 export { default as TagsInput } from './TagsInput'
 
+export { default as SliderInput } from './SliderInput'
+
 export { default as LocationInput } from './LocationInput'
 
 export { default as ListboxInput } from './ListboxInput'
 
-export { default as ComboboxInput } from './ComboboxInput'
-
-export { default as SliderInput } from './SliderInput'
-
 export { default as ListboxOption } from './ListboxInput/components/ListboxOption'
 
+export { default as ListboxOptions } from './ListboxInput/components/ListboxOptions'
+
 export { default as ListboxNullOption } from './ListboxInput/components/ListboxNullOption'
+
+export { default as ComboboxInput } from './ComboboxInput'
 
 export { default as ComboboxOption } from './ComboboxInput/components/ComboboxOption'
 
