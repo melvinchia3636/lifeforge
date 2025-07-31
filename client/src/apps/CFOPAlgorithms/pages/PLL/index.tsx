f56e29e1c62264d5c3bd7cc5339967199b698726
Melvin Chia <melvinchia623600@gmail.com>
2025-08-01 00:00:39 +0800

    25w31
    Former-commit-id: 6b1551124661bbb068a3cab12b9ca3b1e3826943 [formerly 140c842cd03984dfc3a43473b8392ef38d1b4be6] [formerly d8fbd16e99e39672fb316de9ac2d8049c6f9bd58 [formerly 1cc469124d77b312972f7fe6db0c423e8cc8eb0b]]
    Former-commit-id: b642a4533f098c1840eeca49b1baf44f297d99b0 [formerly 86759eabc3bedf7012f7b58788e88fa4e791fbfd]
    Former-commit-id: 745c6ffea0f7f289106479c9017736660809d778

---
+++ client/src/apps/CFOPAlgorithms/pages/PLL/index.tsx
@@ -0,0 +1,42 @@
+import { GoBackButton, ModuleWrapper } from 'lifeforge-ui'
+import { useNavigate } from 'react-router'
+
+import { algsetScrambles } from '../../algorithms/PLL'
+import { DEFAULT_CUBE, applyMoves } from '../../scripts/genCube'
+import AlgEntry from './AlgEntry'
+
+function CFOPPLL() {
+  const navigate = useNavigate()
+
+  return (
+    <ModuleWrapper>
+      <header className="space-y-1">
+        <GoBackButton
+          onClick={() => {
+            navigate('/cfop-algorithms')
+          }}
+        />
+        <div className="flex-between flex">
+          <h1 className="flex items-center gap-4 text-2xl font-semibold sm:text-3xl">
+            <img
+              alt="PLL"
+              className="size-16"
+              src="/assets/cfop/landing-pll.webp"
+            />
+            Permutation of the Last Layer
+          </h1>
+        </div>
+      </header>
+      <ul className="my-8 space-y-4">
+        {algsetScrambles.map((algset, index) => {
+          let cube = DEFAULT_CUBE
+          cube = applyMoves(cube, algset[0])
+
+          return <AlgEntry key={index} cube={cube} index={index} />
+        })}
+      </ul>
+    </ModuleWrapper>
+  )
+}
+
+export default CFOPPLL
