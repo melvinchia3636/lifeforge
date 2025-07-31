fe554d874c1833c420c98ae8283cdbd5c3ed7f09
Melvin Chia <melvinchia623600@gmail.com>
2025-07-31 23:45:01 +0800

    25w31

--- client/src/apps/ChangiAirportFlightStatus/index.tsx
+++ client/src/apps/ChangiAirportFlightStatus/index.tsx
@@ -1,53 +1,83 @@
+import { Listbox, ListboxButton } from "@headlessui/react"
+import { Icon } from "@iconify/react/dist/iconify.js"
+import { useQuery } from "@tanstack/react-query"
+import forgeAPI from "@utils/forgeAPI"
+import clsx from "clsx"
+import { ModuleWrapper, ModuleHeader, SearchInput, Scrollbar, ListboxOptions, ListboxOption } from "lifeforge-ui"
+import { useState } from "react"
+
+const STATUSES = {
+  Departed: ['text-green-500', 'tabler:plane-departure', 'bg-green-500/20'],
+  Boarding: ['text-blue-500', 'tabler:users', 'bg-blue-500/20'],
+  'Gate Closed': ['text-red-500', 'tabler:door-off', 'bg-red-500/20'],
+  'Gate Closing': ['text-orange-500', 'tabler:door', 'bg-orange-500/20'],
+  'Gate Open': ['text-yellow-500', 'tabler:door-enter', 'bg-yellow-500/20'],
+  'New Gate': ['text-lime-500', 'tabler:transfer', 'bg-lime-500/20'],
+  'Re-timed': ['text-fuchsia-500', 'tabler:clock', 'bg-fuchsia-500/20'],
+  Scheduled: ['text-indigo-500', 'tabler:calendar-event', 'bg-indigo-500/20'],
+  'Last Call': ['text-pink-500', 'tabler:bell', 'bg-pink-500/20'],
+  Cancelled: ['text-red-500', 'tabler:ban', 'bg-red-500/20'],
+  Landed: ['text-blue-500', 'tabler:plane-arrival', 'bg-blue-500/20'],
+  Confirmed: ['text-green-500', 'tabler:check', 'bg-green-500/20'],
+  Delayed: ['text-red-500', 'tabler:clock-stop', 'bg-red-500/20']
+}
+
+const SEARCH_TYPE = [
+  ['Departures', 'tabler:plane-departure', 'dep'],
+  ['Arrivals', 'tabler:plane-arrival', 'arr']
+]
 
 function ChangiAirportFlightStatus() {
+  const [type, setType] = useState('dep')
+  const [searchQuery, setSearchQuery] = useState('')
+  
+  const flightsQuery = useQuery(forgeAPI.changiAirportFlightStatus.)
+
   return (
-    <ModuleWrapper
+    <ModuleWrapper>
       <ModuleHeader icon="tabler:plane" title="Changi Flight Status" />
       <div className="mt-6 flex items-center gap-2">
         <Listbox
           as="div"
           className="relative"
-          value={searchParams.get('type') ?? 'dep'}
+          value={type}
           onChange={value => {
-            setSearchParams({ type: value })
+            setType(value)
           }}
         >
           <ListboxButton
-            className={clsx(
-              'flex-between shadow-custom flex w-48 gap-2 rounded-md p-4',
-              componentBgWithHover
-            )}
+            className=
+              'flex-between shadow-custom flex w-48 gap-2 rounded-md p-4 component-bg-with-hover'
           >
             <div className="flex items-center gap-2">
               <Icon
                 className="size-6"
                 icon={
                   SEARCH_TYPE.find(
-                    ([, , value]) => value === searchParams.get('type')
+                    ([, , t]) => t === type
                   )?.[1] || 'tabler:plane-departure'
                 }
               />
               <span className="font-medium whitespace-nowrap">
                 {SEARCH_TYPE.find(
-                  ([, , value]) => value === searchParams.get('type')
+                  ([, , t]) => t === type
                 )?.[0] || 'Departure'}
               </span>
             </div>
             <Icon className="text-bg-500 size-5" icon="tabler:chevron-down" />
           </ListboxButton>
-          <ListboxOrComboboxOptions>
+          <ListboxOptions>
             {SEARCH_TYPE.map(([name, icon, value]) => (
-              <ListboxOrComboboxOption
+              <ListboxOption
                 key={value}
                 icon={icon}
                 text={name}
                 value={value}
               />
             ))}
-          </ListboxOrComboboxOptions>
+          </ListboxOptions>
         </Listbox>
         <SearchInput
-          hasTopMargin={false}
           namespace="modules.changiAirport"
           searchQuery={searchQuery}
           setSearchQuery={setSearchQuery}
