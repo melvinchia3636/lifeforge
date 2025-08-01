d50123050d474cb44429be29db1198302fb44232
Melvin Chia <melvinchia623600@gmail.com>
2025-08-01 16:39:51 +0800

    25w31
    Former-commit-id: 725e30b4cafd27369cac9f15c10f4e25575bc5ce [formerly fb6400e0e196fbe8d6c378cbfb3cf00d46f96a09] [formerly 3cfb9bf4683aa1ba5231676bd4fe533716b8c405 [formerly f801afec6c5ee3e66a5bf8d7a0601da6421c442a]]
    Former-commit-id: df26d7cd1f74a54be1210b5ad5e2fd3bb7bb1aab [formerly ff75c5ac77c2fdffe636008ac0f1ed6015e315c0]
    Former-commit-id: ffcc5e99cd976178ea8d751b265b8b28c7aae609

--- apps/mrt-map-builder/src/utils/forgeAPI.tsx
+++ apps/mrt-map-builder/src/utils/forgeAPI.tsx
@@ -1,6 +1,6 @@
 import { createForgeAPIClient } from 'lifeforge-api'
 
-import { type AppRoutes } from '../../../../server/src/core/appRoutes.type'
+import { type AppRoutes } from '../../../../server/src/core/routes/routes.type'
 
 const forgeAPI = createForgeAPIClient<AppRoutes>(
   import.meta.env.VITE_API_HOST || 'https://localhost:3000'
