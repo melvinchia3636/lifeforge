42fb6b198edaebe268fe596b5d2791aae26904f9
melvinchia3636 <melvinchia623600@gmail.com>
2025-08-04 12:19:57 +0800

    25w32
    Former-commit-id: 81ea018aaccb76f784242fe455901d927e6ca676 [formerly 2ec7211583e596547209f86d33407a28104e1131] [formerly 504c8df281d89703a63557c5dbd59dfe2a0ac1c7 [formerly 2f18892fe534c4426d4b0c7bd6ad8babfa8fe75f]]
    Former-commit-id: 3e563d5259ab392ab30484c9b724e3177003d777 [formerly eb826ed5a3450d8d540034f1211776f5274ca584]
    Former-commit-id: dbae3ec2586406392de6d88517490daa99846bcb

---
+++ client/src/apps/Music/components/Bottombar/components/DurationSlider.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react-compiler/react-compiler */
+import dayjs from 'dayjs'
+
+import { useMusicContext } from '@apps/Music/providers/MusicProvider'
+
+function DurationSlider() {
+  const { audio, currentDuration, setCurrentDuration, currentMusic } =
+    useMusicContext()
+
+  if (currentMusic === null) {
+    return <></>
+  }
+
+  return (
+    <div className="flex w-full items-center gap-2 text-sm">
+      <span className="text-bg-500 -mt-0.5">
+        {dayjs
+          .duration(+currentDuration, 'seconds')
+          .format(+currentDuration >= 3600 ? 'H:mm:ss' : 'm:ss')}
+      </span>
+      <input
+        className="main bg-bg-200 dark:bg-bg-700 h-1 w-full cursor-pointer overflow-hidden rounded-full"
+        max={currentMusic.duration}
+        style={{
+          backgroundSize: `${
+            (+currentDuration / +currentMusic.duration) * 100
+          }% 100%`
+        }}
+        type="range"
+        value={currentDuration}
+        onChange={e => {
+          audio.currentTime = +e.target.value
+          setCurrentDuration(+e.target.value)
+        }}
+      ></input>
+      <span className="text-bg-500 -mt-0.5">
+        {dayjs
+          .duration(+currentMusic.duration, 'seconds')
+          .format(+currentMusic.duration >= 3600 ? 'H:mm:ss' : 'm:ss')}
+      </span>
+    </div>
+  )
+}
+
+export default DurationSlider
---
+++ client/src/apps/Music/components/MusicList/index.tsx
@@ -0,0 +1,67 @@
+import { QueryWrapper } from 'lifeforge-ui'
+import { useCallback } from 'react'
+import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer'
+import List from 'react-virtualized/dist/commonjs/List'
+
+import { useMusicContext } from '@apps/Music/providers/MusicProvider'
+
+import MusicListItem from './components/MusicListItem'
+
+const AS = AutoSizer as any
+
+const L = List as any
+
+function MusicList({ debouncedSearchQuery }: { debouncedSearchQuery: string }) {
+  const { musicsQuery } = useMusicContext()
+
+  const musics = musicsQuery.data ?? []
+
+  const rowRenderer = useCallback(
+    ({
+      index,
+      key,
+      style
+    }: {
+      index: number
+      key: string
+      style: React.CSSProperties
+    }) => {
+      const music = musics.filter(music =>
+        music.name.toLowerCase().includes(debouncedSearchQuery.toLowerCase())
+      )[index]
+
+      return (
+        <div key={key} style={style}>
+          <MusicListItem music={music} />
+        </div>
+      )
+    },
+    [debouncedSearchQuery, musics]
+  )
+
+  return (
+    <QueryWrapper query={musicsQuery}>
+      {musics => (
+        <AS>
+          {({ height, width }: { height: number; width: number }) => (
+            <L
+              height={height}
+              rowCount={
+                musics.filter(music =>
+                  music.name
+                    .toLowerCase()
+                    .includes(debouncedSearchQuery.toLowerCase())
+                ).length
+              }
+              rowHeight={60}
+              rowRenderer={rowRenderer}
+              width={width}
+            />
+          )}
+        </AS>
+      )}
+    </QueryWrapper>
+  )
+}
+
+export default MusicList
