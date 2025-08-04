60af8a098e6541e1a4e1a241698a7b984402cabd
Melvin Chia <melvinchia623600@gmail.com>
2025-08-04 12:16:57 +0800

    25w32
    Former-commit-id: 34542e06b3d8bf6e04921ffa14e0038c9cfd35ec [formerly 3e4da32da14e52fc82540cae0ca3876fe88c63eb] [formerly af16e5a5b1c0b600f49ef997670f3147517df0ca [formerly 3a3ae4a7ecf4290cf44b78c4453847519d7f2fa9]]
    Former-commit-id: 632642dd00cd9777b810490f2f8716efdce3be53 [formerly a29cf76428198cd7732fb2f72c5292304fda7488]
    Former-commit-id: a5b4d5758d7321365a6f4dce5e2963ae4e2e4821

--- server/src/apps/music/routes/entries.ts
+++ server/src/apps/music/routes/entries.ts
@@ -1,48 +1,65 @@
-import { forgeController } from '@functions/routes'
+import { forgeController, forgeRouter } from '@functions/routes'
+import { z } from 'zod/v4'
 
-import * as EntriesService from '../services/entries.service'
-
-const getAllEntries = forgeController.query
+const list = forgeController.query
   .description('Get all music entries')
   .input({})
-  .callback( ({ pb }) => pb.getFullList.collection(''))
+  .callback(({ pb }) => pb.getFullList.collection('music__entries').execute())
 
-const updateEntry = forgeController
-  .route('PATCH /:id')
+const update = forgeController.mutation
   .description('Update a music entry')
-  .input({})
-  .callback(
-    async ({ pb, params: { id }, body }) =>
-      await EntriesService.updateEntry(pb, id, body)
+  .input({
+    query: z.object({
+      id: z.string()
+    }),
+    body: z.object({
+      name: z.string(),
+      author: z.string()
+    })
+  })
+  .callback(({ pb, query: { id }, body }) =>
+    pb.update.collection('music__entries').id(id).data(body).execute()
   )
 
-const deleteEntry = forgeController
-  .route('DELETE /:id')
+const remove = forgeController.mutation
   .description('Delete a music entry')
-  .input({})
-  .existenceCheck('params', {
+  .input({
+    query: z.object({
+      id: z.string()
+    })
+  })
+  .existenceCheck('query', {
     id: 'music__entries'
   })
-  .callback(async ({ pb, params: { id } }) =>
-    EntriesService.deleteEntry(pb, id)
-  )
   .statusCode(204)
+  .callback(({ pb, query: { id } }) =>
+    pb.delete.collection('music__entries').id(id).execute()
+  )
 
-const toggleFavorite = forgeController
-  .route('POST /favourite/:id')
-  .description('Toggle favorite status of a music entry')
-  .input({})
-  .existenceCheck('params', {
+const toggleFavourite = forgeController.mutation
+  .description('Toggle favourite status of a music entry')
+  .input({
+    query: z.object({
+      id: z.string()
+    })
+  })
+  .existenceCheck('query', {
     id: 'music__entries'
   })
-  .callback(
-    async ({ pb, params: { id } }) =>
-      await EntriesService.toggleFavorite(pb, id)
-  )
+  .statusCode(200)
+  .callback(async ({ pb, query: { id } }) => {
+    const entry = await pb.getOne.collection('music__entries').id(id).execute()
+
+    return pb.update
+      .collection('music__entries')
+      .id(id)
+      .data({ is_favourite: !entry.is_favourite })
+      .execute()
+  })
 
 export default forgeRouter({
-  getAllEntries,
-  updateEntry,
-  deleteEntry,
-  toggleFavorite
+  list,
+  update,
+  remove,
+  toggleFavourite
 })
