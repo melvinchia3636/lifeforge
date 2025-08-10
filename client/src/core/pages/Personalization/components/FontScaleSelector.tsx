6438a2e81b8f5d23dab7a19b87441df66d67484c
Melvin Chia <melvinchia623600@gmail.com>
2025-08-10 23:30:25 +0800

    25w32
    Former-commit-id: 3da4e1902ae54973fc529fca5ec042dcdb873c44 [formerly adcfc223c8138d82fae85f258f05867515eeba1d] [formerly 0eefbd583bfa1f82632d40567382f4f9578e9844 [formerly 5fb0767eba69722339acc795ae99fe45299e31f6]]
    Former-commit-id: 2ec7ffbeba0d1d4b99eb6f1a0b9e9bd0941fc734 [formerly 5fd07a114d26c4335c8a0516a04121863390e0a2]
    Former-commit-id: 5a68a4ce424de30c2247aa721b32d66160c46f77

--- client/src/core/pages/Personalization/components/FontScaleSelector.tsx
+++ client/src/core/pages/Personalization/components/FontScaleSelector.tsx
@@ -39,7 +39,7 @@ function FontScaleSelector() {
             changeFontScale(selectedFontScale)
           }}
         >
-          Apply
+          save
         </Button>
       </div>
     </ConfigColumn>
