fa1ece88d0099cc522daec93a11e0eccca4edbda
Melvin Chia <melvinchia623600@gmail.com>
2025-08-13 22:48:29 +0800

    refactor: Update ModuleHeader and ModalHeader components to use actionButtonProps for better prop management
    Former-commit-id: 78955284234ffe28f9b6423cfde9575b83d504a7 [formerly bb80692b3f1a83d931529d395668dd6b965a6982] [formerly f7180dffb4e2dd42b0c8aa214f68e66d908f2048 [formerly 5c1bb77875b8c4c69d4caca1587d8a8377139f84]]
    Former-commit-id: 29f8a57935e86813fe2306852506701cfb8b785a [formerly 9fc2bbdd7d2f8228ad12a30637e9222a017635d9]
    Former-commit-id: 9cab01f65aec964eaa59e380b0aa9fb6d21a8835

--- packages/lifeforge-ui/src/components/modals/core/components/ModalHeader.tsx
+++ packages/lifeforge-ui/src/components/modals/core/components/ModalHeader.tsx
@@ -1,35 +1,29 @@
-import { Button } from '@components/buttons'
 import { Icon } from '@iconify/react'
 import { useDebounce } from '@uidotdev/usehooks'
 import clsx from 'clsx'
 import _ from 'lodash'
-import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
+
+import { Button } from '../../../buttons'
 
 function ModalHeader({
   title,
-  needTranslate = true,
   icon,
-  hasAI = false,
   onClose,
-  actionButtonIcon,
-  actionButtonIsRed = false,
-  onActionButtonClick,
   className = '',
+  hasAI = false,
   appendTitle,
-  namespace = 'common.modals'
+  namespace = 'common.modals',
+  actionButtonProps
 }: {
   title: string
-  needTranslate?: boolean
   icon: string
-  hasAI?: boolean
   onClose: () => void
-  actionButtonIcon?: string
-  actionButtonIsRed?: boolean
-  onActionButtonClick?: () => void
+  hasAI?: boolean
   className?: string
   appendTitle?: React.ReactElement
   namespace?: string
+  actionButtonProps?: React.ComponentProps<typeof Button>
 }) {
   const { t } = useTranslation(namespace)
 
@@ -42,42 +36,28 @@ function ModalHeader({
       <h1 className="flex w-full min-w-0 items-center gap-3 text-2xl font-semibold">
         <Icon className="size-7 shrink-0" icon={innerIcon} />
         <span className="min-w-0 truncate">
-          {needTranslate
-            ? t([
-                `modals.${_.camelCase(innerTitle)}.title`,
-                `modals.${_.camelCase(innerTitle)}`,
-                `${_.camelCase(innerTitle)}.title`,
-                `${_.camelCase(innerTitle)}`,
-                `${innerTitle}.title`,
-                `${innerTitle}`,
-                `modals.${innerTitle}.title`,
-                `modals.${innerTitle}`,
-                innerTitle
-              ])
-            : innerTitle}
+          {t([
+            `modals.${_.camelCase(innerTitle)}.title`,
+            `modals.${_.camelCase(innerTitle)}`,
+            `${_.camelCase(innerTitle)}.title`,
+            `${_.camelCase(innerTitle)}`,
+            `${innerTitle}.title`,
+            `${innerTitle}`,
+            `modals.${innerTitle}.title`,
+            `modals.${innerTitle}`,
+            innerTitle
+          ])}
         </span>
+        {appendTitle}
         {hasAI && (
           <Icon
             className="size-5 shrink-0 text-yellow-500"
             icon="mage:stars-c"
           />
         )}
-        {appendTitle}
       </h1>
       <div className="flex items-center gap-2">
-        {actionButtonIcon && (
-          <button
-            className={clsx(
-              'hover:bg-bg-100 dark:hover:bg-bg-800 rounded-md p-2 transition-all',
-              actionButtonIsRed
-                ? 'text-red-500 hover:text-red-600'
-                : 'text-bg-500 hover:text-bg-200'
-            )}
-            onClick={onActionButtonClick}
-          >
-            <Icon className="size-6" icon={actionButtonIcon} />
-          </button>
-        )}
+        {actionButtonProps && <Button {...actionButtonProps} />}
         <Button
           icon="tabler:x"
           iconClassName="size-6"
@@ -89,13 +69,4 @@ function ModalHeader({
   )
 }
 
-export default memo(ModalHeader, (prevProps, nextProps) => {
-  return (
-    prevProps.title === nextProps.title &&
-    prevProps.icon === nextProps.icon &&
-    prevProps.actionButtonIcon === nextProps.actionButtonIcon &&
-    prevProps.actionButtonIsRed === nextProps.actionButtonIsRed &&
-    prevProps.className === nextProps.className &&
-    prevProps.appendTitle === nextProps.appendTitle
-  )
-})
+export default ModalHeader
