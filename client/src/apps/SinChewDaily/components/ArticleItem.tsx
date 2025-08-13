b0b944edde6ad6e3d1f3c9287841cac666eda214
Melvin Chia <melvinchia623600@gmail.com>
2025-08-13 10:10:40 +0800

    25w33

--- client/src/apps/SinChewDaily/components/ArticleItem.tsx
+++ client/src/apps/SinChewDaily/components/ArticleItem.tsx
@@ -1,11 +1,15 @@
 import { Icon } from '@iconify/react/dist/iconify.js'
+import { useModalStore } from 'lifeforge-ui'
 
 import type { NewsArticle } from '..'
+import ContentModal from './ContentModal'
 
 function ArticleItem({ item }: { item: NewsArticle }) {
+  const open = useModalStore(state => state.open)
+
   return (
-    <article className="shadow-custom component-bg flex flex-col items-center gap-6 rounded-xl p-4 md:flex-row">
-      <div className="component-bg-lighter relative aspect-video w-full shrink-0 overflow-hidden rounded-lg md:w-72">
+    <article className="shadow-custom component-bg-with-hover relative flex flex-col items-center gap-6 rounded-xl p-4 md:flex-row">
+      <div className="component-bg-lighter relative aspect-video w-full shrink-0 overflow-hidden rounded-lg md:w-96">
         <Icon
           className="text-bg-300 dark:text-bg-700 absolute top-1/2 left-1/2 size-16 -translate-x-1/2 -translate-y-1/2 transform"
           icon="tabler:news"
@@ -29,6 +33,14 @@ function ArticleItem({ item }: { item: NewsArticle }) {
         </p>
         <p className="text-bg-500 mt-4">{item.time_display}</p>
       </div>
+      <button
+        className="absolute inset-0 rounded-xl"
+        onClick={() => {
+          open(ContentModal, {
+            url: item.link
+          })
+        }}
+      />
     </article>
   )
 }
