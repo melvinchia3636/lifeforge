2bade403d3836e117cd8105651fb3dada48e22b4
Melvin Chia <melvinchia623600@gmail.com>
2025-08-13 22:25:54 +0800

    refactor: Remove unused props and enhance ContextMenu component's alignment and collision handling
    Former-commit-id: 81fad3ec54b2536e5e4e26a6c0fd4018375cf6a9 [formerly 480e83524c2e5a0a35013396e988664d4f4ce3c7] [formerly 52e6d7e33d29d93fddb056c61d09e7563a6a72d6 [formerly 011828a2d7edb2906b099b7a1cecd9fb04a3ab63]]
    Former-commit-id: 7f706e6ab9891d2ca3b4f1f089abdbce2643d96a [formerly e9e2e4476d05d1663dca1d0ba6862a31656b63ff]
    Former-commit-id: 5403cf19e72079ad1c117c54147ad773c736ae1f

--- packages/lifeforge-ui/src/components/buttons/ContextMenu/index.tsx
+++ packages/lifeforge-ui/src/components/buttons/ContextMenu/index.tsx
@@ -5,43 +5,30 @@ import Button from '../Button'
 
 interface MenuProps {
   children: React.ReactNode
-  anchor?: string
   classNames?: {
     wrapper?: string
     button?: string
     icon?: string
     menu?: string
   }
-  iconClassName?: string
   customIcon?: string
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
-  onClose?: () => void
-  open?: boolean
   onOpenChange?: (open: boolean) => void
   buttonComponent?: React.ReactNode
+  align?: 'start' | 'center' | 'end'
+  side?: 'top' | 'right' | 'bottom' | 'left'
 }
 
 function ContextMenu({
   children,
   classNames,
   customIcon,
-  onClick,
-  onClose,
-  open,
+  buttonComponent,
   onOpenChange,
-  buttonComponent
+  align = 'end',
+  side = 'bottom'
 }: MenuProps) {
   return (
-    <DropdownMenuPrimitive.Root
-      open={open}
-      onOpenChange={(newOpen: boolean) => {
-        onOpenChange?.(newOpen)
-
-        if (!newOpen) {
-          onClose?.()
-        }
-      }}
-    >
+    <DropdownMenuPrimitive.Root onOpenChange={onOpenChange}>
       <DropdownMenuPrimitive.Trigger asChild>
         <div className={classNames?.wrapper} role="menu">
           {buttonComponent || (
@@ -51,22 +38,28 @@ function ContextMenu({
               iconClassName={classNames?.icon}
               tabIndex={0}
               variant="plain"
-              onClick={onClick}
+              onClick={e => {
+                e.preventDefault()
+                e.stopPropagation()
+              }}
             />
           )}
         </div>
       </DropdownMenuPrimitive.Trigger>
       <DropdownMenuPrimitive.Portal>
         <DropdownMenuPrimitive.Content
-          align="end"
+          avoidCollisions
+          align={align}
+          alignOffset={0}
           className={clsx(
             'border-bg-200 bg-bg-100 text-bg-500 dark:border-bg-700 dark:bg-bg-800 z-9999 overflow-auto rounded-md border text-base shadow-lg',
             'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95',
             'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
             classNames?.menu
           )}
-          side="bottom"
-          sideOffset={4}
+          collisionPadding={16}
+          side={side}
+          sideOffset={12}
         >
           {children}
         </DropdownMenuPrimitive.Content>
