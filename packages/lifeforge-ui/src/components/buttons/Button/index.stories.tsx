39fc2c10e804dc2e5d76808b5716b7b8418dd7a2
melvinchia3636 <melvinchia623600@gmail.com>
2025-08-14 11:02:10 +0800

    docs: Added documentation for Button component

--- packages/lifeforge-ui/src/components/buttons/Button/index.stories.tsx
+++ packages/lifeforge-ui/src/components/buttons/Button/index.stories.tsx
@@ -3,7 +3,112 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 import Index from './index'
 
 const meta = {
-  component: Index
+  component: Index,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'A button component for user interactions. Should be used consistently throughout the application. When designing pages, custom defined button should be avoided as much as possible.'
+      }
+    }
+  },
+  argTypes: {
+    icon: {
+      description:
+        'The icon to display in the button. Should be a valid icon name from Iconify in the form of `<icon-library>:<icon-name>`.'
+    },
+    as: {
+      table: {
+        type: {
+          summary: 'React.ElementType'
+        },
+        defaultValue: {
+          summary: 'button'
+        }
+      },
+      description:
+        'The HTML element to render as. Can be a string representing HTML tag or a React component.'
+    },
+    children: {
+      control: false,
+      description:
+        'The content to display inside the button. Can be text or any valid React node.'
+    },
+    className: {
+      description:
+        "The class name to apply to the button. Can be used to customize the button's appearance. If it doesn't work, try putting an exclamation mark at the end (applicable for valid Tailwind CSS classes)."
+    },
+    disabled: {
+      description:
+        'Indicates whether the button is disabled. When true, user interactions are not allowed.'
+    },
+    iconClassName: {
+      description:
+        "The class name to apply to the icon. Can be used to customize the icon's appearance. If it doesn't work, try putting an exclamation mark at the end (applicable for valid Tailwind CSS classes)."
+    },
+    iconPosition: {
+      table: {
+        type: {
+          summary: '"start" | "end"'
+        },
+        defaultValue: {
+          summary: '"start"'
+        }
+      },
+      description: 'The position of the icon within the button.'
+    },
+    isRed: {
+      table: {
+        type: {
+          summary: 'boolean'
+        },
+        defaultValue: {
+          summary: 'false'
+        }
+      },
+      description: 'Indicates whether the button has a red background.'
+    },
+    loading: {
+      table: {
+        type: {
+          summary: 'boolean'
+        },
+        defaultValue: {
+          summary: 'false'
+        }
+      },
+      description:
+        'Indicates whether the button is in a loading state. When true, a spinner icon is displayed and user interactions are not allowed.'
+    },
+    namespace: {
+      table: {
+        defaultValue: {
+          summary: 'common.buttons'
+        }
+      },
+      description:
+        'The i18n namespace to use for the button content translation. See the [main documentation](https://docs.lifeforge.melvinchia.dev) for more details on internationalization.'
+    },
+    onClick: {
+      description: 'Callback function to handle button click events.'
+    },
+    tProps: {
+      description:
+        'Additional properties to pass to the translation function. Used for dynamic translations. See the [i18n documentation](https://docs.lifeforge.melvinchia.dev) for more details on internationalization.'
+    },
+    variant: {
+      table: {
+        type: {
+          summary: '"primary" | "secondary" | "tertiary" | "plain"'
+        },
+        defaultValue: {
+          summary: 'primary'
+        }
+      },
+      description:
+        'The visual style variant of the button. Can be one of "primary", "secondary", "tertiary", or "plain".'
+    }
+  }
 } satisfies Meta<typeof Index>
 
 export default meta
@@ -15,7 +120,16 @@ export const PrimaryVariant: Story = {
     as: 'button',
     icon: 'tabler:cube',
     children: 'Button',
-    tProps: {}
+    tProps: {},
+    iconPosition: 'start',
+    variant: 'primary'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'A primary button variant. Suitable for main actions.'
+      }
+    }
   },
   render: props => <Index {...props} />
 }
@@ -28,7 +142,14 @@ export const SecondaryVariant: Story = {
     tProps: {},
     variant: 'secondary'
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A secondary button variant. Suitable for less prominent actions.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
@@ -40,7 +161,13 @@ export const TertiaryVariant: Story = {
     tProps: {},
     variant: 'tertiary'
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story: 'A tertiary button variant. Suitable for less prominent actions.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
@@ -52,7 +179,14 @@ export const PlaintVariant: Story = {
     tProps: {},
     variant: 'plain'
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A plain button variant. Suitable for actions without emphasis, or button with icon only.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
@@ -62,10 +196,16 @@ export const IconAtEnd: Story = {
     icon: 'tabler:arrow-right',
     children: 'Proceed',
     tProps: {},
-    iconAtEnd: true,
+    iconPosition: 'end',
     loading: false
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story: 'A button with the icon positioned at the end.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
@@ -78,7 +218,14 @@ export const Disabled: Story = {
     loading: false,
     disabled: true
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A disabled button. Indicates an action that is not available. User input is disabled.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
@@ -88,11 +235,18 @@ export const Loading: Story = {
     icon: 'tabler:arrow-right',
     children: 'Loading',
     tProps: {},
-    iconAtEnd: true,
+    iconPosition: 'end',
     loading: true,
     disabled: false
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A loading button. Indicates an action that is in progress. User input is disabled.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
@@ -103,7 +257,13 @@ export const IconsOnly: Story = {
     children: '',
     tProps: {}
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story: 'A button that displays only an icon.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
@@ -115,19 +275,32 @@ export const IconsOnlyWithNoBg: Story = {
     tProps: {},
     variant: 'plain'
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story: 'A button that displays only an icon, with no background.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
 
 export const RedButton: Story = {
   args: {
     as: 'button',
-    icon: 'tabler:cube',
-    children: 'Button',
+    icon: 'tabler:trash',
+    children: 'Delete',
     tProps: {},
     variant: 'primary',
     isRed: true
   },
-
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A red button variant. Suitable for destructive actions like deleting an item.'
+      }
+    }
+  },
   render: props => <Index {...props} />
 }
