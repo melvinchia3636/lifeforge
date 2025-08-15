1d8e763298232a9c65c74b5248d39dc792e19cea
Melvin Chia <melvinchia623600@gmail.com>
2025-08-15 19:10:28 +0800

    refactor: Update withBodyClass to manage body class and rootElement state for improved theme handling

--- packages/lifeforge-ui/.storybook/preview.tsx
+++ packages/lifeforge-ui/.storybook/preview.tsx
@@ -9,8 +9,9 @@ import {
 } from '@storybook/addon-docs/blocks'
 import type { Preview } from '@storybook/react-vite'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { APIEndpointProvider, PersonalizationProvider } from 'shared'
+import { themes } from 'storybook/theming'
 
 import ModalManager from '../src/components/modals/core/ModalManager'
 import './i18n'
@@ -19,52 +20,70 @@ import './index.css'
 const queryClient = new QueryClient()
 
 const withBodyClass = (Story: any, context: any) => {
+  const [rootElement, setRootElement] = useState<HTMLElement | null>(null)
+
   useEffect(() => {
-    document.body.classList.remove(
+    const body = document.getElementById('body')
+    if (!body) return
+
+    body.classList.remove(
       'bg-bg-50!',
       'bg-bg-900!',
       'text-bg-800!',
       'text-bg-100!'
     )
-    document.body.classList.add(
+    body.classList.add(
       'flex',
       'items-center',
       'justify-center',
-      'min-h-dvh',
       'theme-blue',
       'bg-zinc',
+      'h-full',
       'transition-all',
+      'flex',
+      'flex-col',
       ...(context.globals.theme === 'dark'
         ? ['bg-bg-900!', 'text-bg-100!']
         : ['bg-bg-50!', 'text-bg-800!'])
     )
 
+    document.querySelectorAll('html, body').forEach(html => {
+      html.classList.add('h-full')
+    })
+
     if (context.globals.theme === 'dark') {
-      document.body.classList.add('dark')
+      body.classList.add('dark')
     } else {
-      document.body.classList.remove('dark')
+      body.classList.remove('dark')
     }
   }, [context.globals.theme])
+
+  useEffect(() => {
+    setRootElement(document.getElementById('body'))
+  }, [])
 
   return (
     <APIEndpointProvider endpoint={'https://lifeforge-api-proxy.onrender.com'}>
       <QueryClientProvider client={queryClient}>
-        <PersonalizationProvider
-          defaultValueOverride={{
-            rawThemeColor: 'theme-blue'
-          }}
-        >
-          <main className="flex flex-1 flex-col bg-white" id="app">
-            <div
-              className={`bg-zinc theme-blue flex w-full flex-1 items-center justify-center py-12 transition-all ${
-                context.globals.theme === 'dark' ? 'dark' : ''
-              } ${context.globals.theme === 'dark' ? 'bg-bg-900' : 'bg-bg-200/50'}`}
-            >
-              <Story />
-            </div>
-          </main>
-          <ModalManager />
-        </PersonalizationProvider>
+        <div id="body">
+          <PersonalizationProvider
+            defaultValueOverride={{
+              rawThemeColor: 'theme-blue',
+              rootElement: rootElement
+            }}
+          >
+            <main className="flex w-full flex-1 flex-col bg-white" id="app">
+              <div
+                className={`bg-zinc theme-blue flex w-full flex-1 items-center justify-center py-12 transition-all ${
+                  context.globals.theme === 'dark' ? 'dark' : ''
+                } ${context.globals.theme === 'dark' ? 'bg-bg-900' : 'bg-bg-200/50'}`}
+              >
+                <Story />
+                <ModalManager />
+              </div>
+            </main>
+          </PersonalizationProvider>
+        </div>
       </QueryClientProvider>
     </APIEndpointProvider>
   )
@@ -83,7 +102,7 @@ const preview: Preview = {
       expanded: true
     },
     docs: {
-      toc: true,
+      theme: themes.dark,
       page: () => (
         <>
           <Title />
