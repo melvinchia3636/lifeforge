9d1a24da11420d586688b1ebea1e76ef55763e84
Melvin Chia <melvinchia623600@gmail.com>
2025-08-18 07:15:58 +0800

    feat: migrate Listbox components to Radix UI Select
    Former-commit-id: 6dcb08bc24b4bbe9b25dfd9365366db600ec957d [formerly 8a04d999c12392c76037bdac03d08a1c80c0cf17] [formerly 140f8420a71aaea7c6e7b419152bf05c17d21fed [formerly 223cc31ce3fec35a4a47b99c3214e6ebfde36f20]]
    Former-commit-id: 5ddd406a43648b5feef477a36d467b5ce7d2e2d4 [formerly c322ca54502ae894923d98873c4a9490c7e96993]
    Former-commit-id: 36a7d5bb881ae2088ec1a1a0caecb513cbadcebb

--- packages/lifeforge-ui/src/components/inputs/ListboxInput/components/ListboxNullOption.tsx
+++ packages/lifeforge-ui/src/components/inputs/ListboxInput/components/ListboxNullOption.tsx
@@ -1,6 +1,8 @@
-import { ListboxOption } from '@headlessui/react'
 import { Icon } from '@iconify/react'
+import * as Select from '@radix-ui/react-select'
 import clsx from 'clsx'
+
+import { useListboxContext } from './ListboxContext'
 
 function ListboxNullOption({
   icon,
@@ -13,42 +15,54 @@ function ListboxNullOption({
   hasBgColor?: boolean
   text?: string
 }) {
+  const { currentValue, multiple } = useListboxContext()
+
+  // Convert value to string for Radix UI Select
+  const stringValue =
+    typeof value === 'object' && value !== null
+      ? JSON.stringify(value)
+      : String(value || '')
+
+  // Check if this option is selected when in multiple mode
+  const isSelected =
+    multiple && Array.isArray(currentValue)
+      ? currentValue.some(
+          v =>
+            (typeof v === 'object' && v !== null
+              ? JSON.stringify(v)
+              : String(v)) === stringValue
+        )
+      : false // For null option, we typically check if no value is selected
+
   return (
-    <ListboxOption
+    <Select.Item
       key="none"
-      className="flex-between hover:bg-bg-200 dark:hover:bg-bg-700/50 relative flex cursor-pointer p-4 transition-all select-none"
-      value={value}
+      className="flex-between hover:bg-bg-200 dark:hover:bg-bg-700/50 data-[highlighted]:bg-bg-200 dark:data-[highlighted]:bg-bg-700/50 relative flex cursor-pointer p-4 transition-all outline-none select-none"
+      value={stringValue}
     >
-      {({ selected }) => (
-        <>
-          <div
-            className={clsx(
-              'flex items-center font-medium',
-              hasBgColor ? 'gap-3' : 'gap-2',
-              selected && 'text-bg-800 dark:text-bg-100'
-            )}
-          >
-            <span
-              className={clsx(
-                'rounded-md',
-                hasBgColor
-                  ? 'bg-bg-200 text-bg-500 dark:bg-bg-700/50 p-2'
-                  : 'pr-2'
-              )}
-            >
-              <Icon className="size-5" icon={icon} />
-            </span>
-            <span>{text}</span>
-          </div>
-          {selected && (
-            <Icon
-              className="text-custom-500 block text-lg"
-              icon="tabler:check"
-            />
+      <div
+        className={clsx(
+          'flex items-center font-medium',
+          hasBgColor ? 'gap-3' : 'gap-2',
+          isSelected && 'text-bg-800 dark:text-bg-100'
+        )}
+      >
+        <span
+          className={clsx(
+            'rounded-md',
+            hasBgColor ? 'bg-bg-200 text-bg-500 dark:bg-bg-700/50 p-2' : 'pr-2'
           )}
-        </>
+        >
+          <Icon className="size-5" icon={icon} />
+        </span>
+        <Select.ItemText>{text}</Select.ItemText>
+      </div>
+      {(multiple ? isSelected : true) && (
+        <Select.ItemIndicator>
+          <Icon className="text-custom-500 block text-lg" icon="tabler:check" />
+        </Select.ItemIndicator>
       )}
-    </ListboxOption>
+    </Select.Item>
   )
 }
 
