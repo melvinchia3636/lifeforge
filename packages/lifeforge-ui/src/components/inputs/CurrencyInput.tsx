8baa833f246832ff85d3f44926c8b8f3372f8ae1
Melvin Chia <melvinchia623600@gmail.com>
2025-08-18 07:16:14 +0800

    feat: add autoFocus prop to input components and utility function for automatic focusing
    Former-commit-id: 2cf4a2313e7bf4e4b3bf81f218caf289e770665a [formerly 8e3737c6b997f80dffb9dc60a75d7b1d8be19ddd] [formerly 896de2a13717cf916ac740f855525e2ac2515dae [formerly 63d11cc03d92e1beca603c36ccd15e013ddc5964]]
    Former-commit-id: d73821777b638835db75bcfdd6491fd86f4874c8 [formerly 652c3f2ebba1bf67b82fa6177c817b08d0df4a7b]
    Former-commit-id: aedcf0df42ac3cccbc09e7f3df6c8c673a51bb00

--- packages/lifeforge-ui/src/components/inputs/CurrencyInput.tsx
+++ packages/lifeforge-ui/src/components/inputs/CurrencyInput.tsx
@@ -5,6 +5,7 @@ import InputIcon from './shared/components/InputIcon'
 import InputLabel from './shared/components/InputLabel'
 import InputWrapper from './shared/components/InputWrapper'
 import useInputLabel from './shared/hooks/useInputLabel'
+import { autoFocusableRef } from './shared/utils/autoFocusableRef'
 
 interface CurrencyInputProps {
   /** The label text displayed above the currency input field. */
@@ -21,10 +22,13 @@ interface CurrencyInputProps {
   required?: boolean
   /** Whether the currency input field is disabled and non-interactive. */
   disabled?: boolean
+  /** Whether the input should automatically focus when rendered. */
+  autoFocus?: boolean
   /** Additional CSS class names to apply to the currency input component. */
   className?: string
   /** The i18n namespace for internationalization. Use false to disable translation. */
   namespace?: string
+  /** Error message to display when the input is invalid. */
   errorMsg?: string
 }
 
@@ -34,8 +38,9 @@ function CurrencyInputComponent({
   placeholder,
   value,
   setValue,
-  required,
-  disabled,
+  required = false,
+  disabled = false,
+  autoFocus = false,
   className,
   namespace,
   errorMsg
@@ -68,7 +73,7 @@ function CurrencyInputComponent({
           required={required === true}
         />
         <CurrencyInput
-          ref={inputRef}
+          ref={autoFocusableRef(autoFocus, inputRef)}
           className="focus:placeholder:text-bg-500 mt-6 h-8 w-full rounded-lg bg-transparent p-6 pl-4 tracking-wider placeholder:text-transparent focus:outline-hidden"
           decimalsLimit={2}
           name={label}
