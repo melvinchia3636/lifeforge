7ac3098e142ae93217e7e011b5cab63e8687c553
Melvin Chia <melvinchia623600@gmail.com>
2025-08-17 08:05:11 +0800

    refactor: Improved client core folder structure and remove obsolete components

--- client/src/core/providers/UserPersonalizationProvider.tsx
+++ client/src/core/providers/UserPersonalizationProvider.tsx
@@ -3,7 +3,6 @@ import { createContext, useContext, useEffect } from 'react'
 import { toast } from 'react-toastify'
 import { type IBackdropFilters, type IDashboardLayout } from 'shared'
 import { usePersonalization } from 'shared'
-
 import { useAuth } from './AuthProvider'
 
 const UserPersonalizationContext = createContext<{
