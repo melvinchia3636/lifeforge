7fdc4041f06e4e4e0b5bcb952618e1c35c80e46d
Melvin Chia <melvinchia623600@gmail.com>
2025-08-24 10:57:54 +0800

    refactor: minor refactor of CodeTime module
    Former-commit-id: 318572d8b64711fdd653cd022cb40de65dac8cfc [formerly e60093eb026c7633aae007e384f4f8a54d22758e] [formerly 15f57b66775ef7bf03681fb63e8584748b9b0828 [formerly 65ab1561f4c23b9d985fe9aaea3f3922e4b51ee1]]
    Former-commit-id: c017b9f27c7489deb2f4a33db85c7370c6714955 [formerly 8515b3f015b0dfc1162e0fc012a489be62e601a7]
    Former-commit-id: 3fd8e62e04b243bea01441df535257da7260ac00

--- client/src/apps/CodeTime/components/HoursAndMinutesFromSeconds.tsx
+++ client/src/apps/CodeTime/components/HoursAndMinutesFromSeconds.tsx
@@ -9,27 +9,34 @@ export default function HoursAndMinutesFromSeconds({
 
   return (
     <>
-      {Math.floor(seconds / 60) > 0 ? (
-        <>
-          {Math.floor(seconds / 60)}
-          <span className="text-bg-500 pl-1 text-3xl font-normal">
-            {t('units.h')}
-          </span>
-        </>
+      {seconds === 0 ? (
+        <span className="text-bg-500 mr-4 block text-xl">
+          {t('units.noTime')}
+        </span>
       ) : (
-        ''
-      )}{' '}
-      {Math.floor(seconds % 60) > 0 ? (
         <>
-          {Math.floor(seconds % 60)}
-          <span className="text-bg-500 pl-1 text-3xl font-normal">
-            {t('units.m')}
-          </span>
+          {Math.floor(seconds / 60) > 0 ? (
+            <>
+              {Math.floor(seconds / 60)}
+              <span className="text-bg-500 pl-1 text-3xl font-normal">
+                {t('units.h')}
+              </span>
+            </>
+          ) : (
+            ''
+          )}{' '}
+          {Math.floor(seconds % 60) > 0 ? (
+            <>
+              {Math.floor(seconds % 60)}
+              <span className="text-bg-500 pl-1 text-3xl font-normal">
+                {t('units.m')}
+              </span>
+            </>
+          ) : (
+            ''
+          )}
         </>
-      ) : (
-        ''
-      )}{' '}
-      {seconds === 0 ? 'no time' : ''}
+      )}
     </>
   )
 }
