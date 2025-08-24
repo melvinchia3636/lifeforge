6492562a495370eef46877f0b4a82dd44c58a399
Melvin Chia <melvinchia623600@gmail.com>
2025-08-24 15:37:36 +0800

    refactor (docs): streamline Note and Warning component structure for improved readability
    Former-commit-id: 4cfee37a59dbd425518d7e7ec55212766d2263c7 [formerly 65cff392a23282beffe253813946c488a62b4999] [formerly 02f00ff050836b518a9d00286d1fb14b9bc6be8c [formerly c437698b961e05687af2f1b0d83a0ea784f03c1a]]
    Former-commit-id: 6f0de1a78c0e6ca9512d2a5b759ea3a9c0bee387 [formerly 0dbd99017df902065ea183d10b5da94da018099e]
    Former-commit-id: b22ba875af3afd50b5935f1c584b4810c37dc4ad

--- apps/docs/src/components/Warning.tsx
+++ apps/docs/src/components/Warning.tsx
@@ -9,19 +9,14 @@ function Warning({
   className?: string
 }) {
   return (
-    <div className="relative w-full before:absolute before:top-0 before:left-0 before:h-full before:w-[4px] before:rounded-l-full before:bg-amber-500">
-      <div
-        className={`notes quote bg-bg-200/50 dark:bg-bg-800/50 mt-6 w-full rounded-md p-4 pl-6 ${className}`}
-      >
-        <div className="flex items-center gap-2">
-          <Icon
-            className="h-6 w-6 text-amber-500"
-            icon="tabler:alert-triangle"
-          />
-          <h4 className="text-xl font-semibold">Warning</h4>
-        </div>
-        <p className="-mt-2 text-base">{children}</p>
+    <div
+      className={`notes quote bg-bg-200/50 dark:bg-bg-800/50 relative mt-6 w-full rounded-md p-4 pl-6 before:absolute before:top-0 before:left-0 before:h-full before:w-[4px] before:rounded-l-full before:bg-amber-500 ${className}`}
+    >
+      <div className="flex items-center gap-2">
+        <Icon className="h-6 w-6 text-amber-500" icon="tabler:alert-triangle" />
+        <h4 className="text-xl font-semibold">Warning</h4>
       </div>
+      <p className="-mt-2 text-base">{children}</p>
     </div>
   )
 }
