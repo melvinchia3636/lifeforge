315e344c6120a093e97a3cfb1bfaf2a7542c1817
melvinchia3636 <melvinchia623600@gmail.com>
2025-09-01 05:29:50 +0800

    fix(server): update color classes

--- server/src/apps/codeTime/utils/readme.ts
+++ server/src/apps/codeTime/utils/readme.ts
@@ -53,8 +53,8 @@ export default async function getReadmeHTML(pb: PBService) {
     </style>
   </head>
   
-  <body class="flex p-4 text-bg-100 flex-col w-full h-dvh border-lime border-2 bg-bg-900">
-    <header class="border-b-2 border-bg-800 p-2 w-full flex items-center justify-between">
+  <body class="flex p-4 text-zinc-100 flex-col w-full h-dvh border-lime border-2 bg-zinc-900">
+    <header class="border-b-2 border-zinc-800 p-2 w-full flex items-center justify-between">
       <div class="flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-lime">
           <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -63,7 +63,7 @@ export default async function getReadmeHTML(pb: PBService) {
         <h1 class="text-lg font-medium">Code Time Statistics</h1>
       </div>
       <div class="tracking-wider font-medium text-sm flex items-center gap-1">
-        <span class="text-bg-500">powered by</span>
+        <span class="text-zinc-500">powered by</span>
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" class="text-lime">
           <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="m11.414 10l-7.383 7.418a2.09 2.09 0 0 0 0 2.967a2.11 2.11 0 0 0 2.976 0L14.414 13m3.707 2.293l2.586-2.586a1 1 0 0 0 0-1.414l-7.586-7.586a1 1 0 0 0-1.414 0L9.121 6.293a1 1 0 0 0 0 1.414l7.586 7.586a1 1 0 0 0 1.414 0" />
@@ -72,7 +72,7 @@ export default async function getReadmeHTML(pb: PBService) {
       </div>
     </header>
     <div class="flex-1 w-full space-y-2 mt-4">
-      <div class="bg-lime w-full p-2 flex items-center justify-between rounded-sm text-bg-900">
+      <div class="bg-lime w-full p-2 flex items-center justify-between rounded-sm text-zinc-900">
         <div class="flex items-center gap-2 font-medium">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -92,7 +92,7 @@ export default async function getReadmeHTML(pb: PBService) {
         </div>
       </div>
       <div
-        class="bg-bg-800/50 border-lime border-2 text-lime w-full p-2 flex items-center justify-between rounded-sm">
+        class="bg-zinc-800/50 border-lime border-2 text-lime w-full p-2 flex items-center justify-between rounded-sm">
         <div class="flex items-center gap-2 font-medium">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -103,14 +103,14 @@ export default async function getReadmeHTML(pb: PBService) {
         <div>
           <span class="text-2xl font-semibold tracking-wider">${Math.floor(
             moment.duration(todayTime, 'minutes').asHours()
-          )}<span class="text-bg-600 text-xl">h</span> ${Math.floor(
+          )}<span class="text-zinc-600 text-xl">h</span> ${Math.floor(
             moment.duration(todayTime, 'minutes').asMinutes() % 60
           )}<span
-              class="text-bg-600 text-xl">m</span></span>
+              class="text-zinc-600 text-xl">m</span></span>
         </div>
       </div>
-      <div class="bg-bg-800/50 w-full p-2 flex items-center justify-between rounded-sm">
-        <div class="flex items-center text-bg-400 gap-2 font-medium">
+      <div class="bg-zinc-800/50 w-full p-2 flex items-center justify-between rounded-sm">
+        <div class="flex items-center text-zinc-400 gap-2 font-medium">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
               <path
@@ -124,16 +124,16 @@ export default async function getReadmeHTML(pb: PBService) {
         <div>
           <span class="text-2xl font-semibold tracking-wider">${Math.floor(
             moment.duration(statistics['Most time spent'], 'minutes').asHours()
-          )}<span class="text-bg-600 text-xl">h</span> ${Math.floor(
+          )}<span class="text-zinc-600 text-xl">h</span> ${Math.floor(
             moment
               .duration(statistics['Most time spent'], 'minutes')
               .asMinutes() % 60
           )}<span
-              class="text-bg-600 text-xl">m</span></span>
+              class="text-zinc-600 text-xl">m</span></span>
         </div>
       </div>
-      <div class="bg-bg-800/50 w-full p-2 flex items-center justify-between rounded-sm">
-        <div class="flex items-center text-bg-400 gap-2 font-medium">
+      <div class="bg-zinc-800/50 w-full p-2 flex items-center justify-between rounded-sm">
+        <div class="flex items-center text-zinc-400 gap-2 font-medium">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
               <path d="M5 13a7 7 0 1 0 14 0a7 7 0 0 0-14 0m2-9L4.25 6M17 4l2.75 2" />
@@ -147,17 +147,17 @@ export default async function getReadmeHTML(pb: PBService) {
             moment
               .duration(statistics['Average time spent'], 'minutes')
               .asHours()
-          )}<span class="text-bg-600 text-xl">h</span> ${Math.floor(
+          )}<span class="text-zinc-600 text-xl">h</span> ${Math.floor(
             moment
               .duration(statistics['Average time spent'], 'minutes')
               .asMinutes() % 60
           )}<span
-              class="text-bg-600 text-xl">m</span></span>
+              class="text-zinc-600 text-xl">m</span></span>
         </div>
       </div>
       <div class="flex items-center gap-2">
-        <div class="bg-bg-800/50 w-full p-2 flex items-center justify-between rounded-sm">
-          <div class="flex items-center text-bg-400 gap-2 font-medium">
+        <div class="bg-zinc-800/50 w-full p-2 flex items-center justify-between rounded-sm">
+          <div class="flex items-center text-zinc-400 gap-2 font-medium">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
                 <path d="M10 2c0-.88 1.056-1.331 1.692-.722c1.958 1.876 3.096 5.995 1.75 9.12l-.08.174l.012.003c.625.133 1.203-.43 2.303-2.173l.14-.224a1 1 0 0 1 1.582-.153C18.733 9.46 20 12.402 20 14.295C20 18.56 16.409 22 12 22s-8-3.44-8-7.706c0-2.252 1.022-4.716 2.632-6.301l.605-.589c.241-.236.434-.43.618-.624C9.285 5.268 10 3.856 10 2"  />
@@ -169,11 +169,11 @@ export default async function getReadmeHTML(pb: PBService) {
             <span class="text-2xl font-semibold tracking-wider">${
               statistics['Current streak']
             }<span
-                class="text-bg-600 text-xl ml-1">days</span></span>
+                class="text-zinc-600 text-xl ml-1">days</span></span>
           </div>
         </div>
-        <div class="bg-bg-800/50 w-full p-2 flex items-center justify-between rounded-sm">
-          <div class="flex items-center text-bg-400 gap-2 font-medium">
+        <div class="bg-zinc-800/50 w-full p-2 flex items-center justify-between rounded-sm">
+          <div class="flex items-center text-zinc-400 gap-2 font-medium">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="text-lime">
               <path fill="currentColor"
                 d="M10 2c0-.88 1.056-1.331 1.692-.722c1.958 1.876 3.096 5.995 1.75 9.12l-.08.174l.012.003c.625.133 1.203-.43 2.303-2.173l.14-.224a1 1 0 0 1 1.582-.153C18.733 9.46 20 12.402 20 14.295C20 18.56 16.409 22 12 22s-8-3.44-8-7.706c0-2.252 1.022-4.716 2.632-6.301l.605-.589c.241-.236.434-.43.618-.624C9.285 5.268 10 3.856 10 2" />
@@ -185,15 +185,15 @@ export default async function getReadmeHTML(pb: PBService) {
               statistics['Longest streak'],
               statistics['Current streak']
             )}<span
-                class="text-bg-600 text-xl ml-1">days</span></span>
+                class="text-zinc-600 text-xl ml-1">days</span></span>
           </div>
         </div>
       </div>
     </div>
-    <div class="border-t-2 mt-4 border-bg-800 flex items-center justify-between p-2 text-sm">
-      <p class="flex text-bg-500">[Computer Generated Report]</span>
+    <div class="border-t-2 mt-4 border-zinc-800 flex items-center justify-between p-2 text-sm">
+      <p class="flex text-zinc-500">[Computer Generated Report]</span>
       </p>
-      <p class="flex text-bg-500">Last updated: <span class="font-medium text-bg-100 pl-1">${moment().format(
+      <p class="flex text-zinc-500">Last updated: <span class="font-medium text-zinc-100 pl-1">${moment().format(
         'YYYY-MM-DD HH:mm:ss'
       )}</span>
       </p>
