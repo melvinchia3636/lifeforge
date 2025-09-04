ac2b140805f5dfbcb4a6c6a4b3c8d3939ba15772
Melvin Chia <melvinchia623600@gmail.com>
2025-09-05 07:36:53 +0800

    refactor(client): restructure EntryItem component and introduce subcomponents for better organization

---
+++ client/src/apps/Achievements/components/EntryItem/components/ActionMenu.tsx
@@ -0,0 +1,70 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import forgeAPI from '@utils/forgeAPI'
+import {
+  ConfirmationModal,
+  ContextMenu,
+  ContextMenuItem,
+  useModalStore
+} from 'lifeforge-ui'
+import { useCallback } from 'react'
+
+import type { Achievement } from '@apps/Achievements'
+
+import ModifyAchievementModal from '../../ModifyAchievementModal'
+
+function ActionMenu({ entry }: { entry: Achievement }) {
+  const open = useModalStore(state => state.open)
+
+  const queryClient = useQueryClient()
+
+  const deleteMutation = useMutation(
+    forgeAPI.achievements.entries.remove
+      .input({
+        id: entry.id
+      })
+      .mutationOptions({
+        onSuccess: () => {
+          queryClient.invalidateQueries({
+            queryKey: ['achievements']
+          })
+        }
+      })
+  )
+
+  const handleDeleteEntry = useCallback(() => {
+    open(ConfirmationModal, {
+      title: 'Delete Achievement',
+      description: 'Are you sure you want to delete this achievement?',
+      confirmationButton: 'delete',
+      onConfirm: async () => {
+        await deleteMutation.mutateAsync({})
+      }
+    })
+  }, [entry])
+
+  const handleUpdateEntry = useCallback(() => {
+    open(ModifyAchievementModal, {
+      modifyType: 'update',
+      initialData: entry,
+      currentDifficulty: entry.difficulty
+    })
+  }, [entry])
+
+  return (
+    <ContextMenu>
+      <ContextMenuItem
+        icon="tabler:pencil"
+        label="Edit"
+        onClick={handleUpdateEntry}
+      />
+      <ContextMenuItem
+        dangerous
+        icon="tabler:trash"
+        label="Delete"
+        onClick={handleDeleteEntry}
+      />
+    </ContextMenu>
+  )
+}
+
+export default ActionMenu
