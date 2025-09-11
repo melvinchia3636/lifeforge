fcb709325a2331c03635a8ff3dbad65cfb3f8f33
melvinchia3636 <melvinchia623600@gmail.com>
2025-09-11 22:08:34 +0800

    refactor(server): standardize schema keys to use snake_case naming convention
    Former-commit-id: c0b02f8db8de1ae902d7c25ef987fa9adba44118 [formerly 5f2a9cae47c57e2cf76d874dcae2fe5a608df5e2] [formerly 07148818886b9a8d4ccd16cf8d4291b689611e18 [formerly 6b983167d2180ae3ae78940b59c3ca7025d8247d]]
    Former-commit-id: c6833453d43e6f11e095cd817f62b6e24d1ed074 [formerly 7acad0eb19eed6d37c8b235a76b7650297e3a713]
    Former-commit-id: 2e59c0b6b3c3b22a03b75656dc24e81af3295f09

--- server/src/core/schema.ts
+++ server/src/core/schema.ts
@@ -1,23 +1,23 @@
 import flattenSchemas from "@functions/utils/flattenSchema";
 
 export const SCHEMAS = {
-  user: (await import("@lib/user/schema")).default,
-  todoList: (await import("@lib/todoList/schema")).default,
-  codeTime: (await import("@lib/codeTime/schema")).default,
-  ideaBox: (await import("@lib/ideaBox/schema")).default,
+  users: (await import("@lib/user/schema")).default,
+  todo_list: (await import("@lib/todoList/schema")).default,
+  code_time: (await import("@lib/codeTime/schema")).default,
+  idea_box: (await import("@lib/ideaBox/schema")).default,
   passwords: (await import("@lib/passwords/schema")).default,
   journal: (await import("@lib/journal/schema")).default,
   calendar: (await import("@lib/calendar/schema")).default,
   music: (await import("@lib/music/schema")).default,
   achievements: (await import("@lib/achievements/schema")).default,
   wallet: (await import("@lib/wallet/schema")).default,
-  scoresLibrary: (await import("@lib/scoresLibrary/schema")).default,
-  apiKeys: (await import("@lib/apiKeys/schema")).default,
-  booksLibrary: (await import("@lib/booksLibrary/schema")).default,
+  scores_library: (await import("@lib/scoresLibrary/schema")).default,
+  api_keys: (await import("@lib/apiKeys/schema")).default,
+  books_library: (await import("@lib/booksLibrary/schema")).default,
   wishlist: (await import("@lib/wishlist/schema")).default,
-  momentVault: (await import("@lib/momentVault/schema")).default,
+  moment_vault: (await import("@lib/momentVault/schema")).default,
   movies: (await import("@lib/movies/schema")).default,
-  railwayMap: (await import("@lib/railwayMap/schema")).default,
+  railway_map: (await import("@lib/railwayMap/schema")).default,
   blog: (await import("@lib/blog/schema")).default,
 };
 
