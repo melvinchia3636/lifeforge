c6cbd7851daa196d40a558b3a205730b81573578
melvinchia3636 <melvinchia623600@gmail.com>
2025-09-11 22:14:28 +0800

    refactor(client): replace Menu with ContextMenu to match new UI SDK

--- client/src/apps/04.Storage/Music/components/Bottombar/index.tsx
+++ client/src/apps/04.Storage/Music/components/Bottombar/index.tsx
@@ -1,5 +1,4 @@
-import { Menu, MenuButton, MenuItems } from '@headlessui/react'
-import { Button, ContextMenuItem, useModalStore } from 'lifeforge-ui'
+import { ContextMenu, ContextMenuItem, FAB, useModalStore } from 'lifeforge-ui'
 
 import { useMusicContext } from '@apps/04.Storage/music/providers/MusicProvider'
 
@@ -16,21 +15,19 @@ function BottomBar() {
 
   return (
     <div className="absolute bottom-8 left-0 w-full space-y-3">
-      <Menu as="div" className="static z-50 float-right block md:hidden">
-        <Button as={MenuButton} icon="tabler:plus" onClick={() => {}}></Button>
-        <MenuItems
-          transition
-          anchor="top end"
-          className="bg-bg-100 dark:bg-bg-800 overflow-hidden overscroll-contain rounded-md shadow-lg outline-hidden transition duration-100 ease-out [--anchor-gap:6px] focus:outline-hidden data-closed:scale-95 data-closed:opacity-0"
-        >
-          <ContextMenuItem
-            icon="tabler:brand-youtube"
-            label="Download from YouTube"
-            namespace="apps.music"
-            onClick={() => open(YoutubeDownloaderModal, {})}
-          />
-        </MenuItems>
-      </Menu>
+      <ContextMenu
+        buttonComponent={<FAB className="static!" visibilityBreakpoint="md" />}
+        classNames={{
+          wrapper: 'fixed bottom-6 right-6'
+        }}
+      >
+        <ContextMenuItem
+          icon="tabler:brand-youtube"
+          label="Download from YouTube"
+          namespace="apps.music"
+          onClick={() => open(YoutubeDownloaderModal, {})}
+        />
+      </ContextMenu>
       {currentMusic !== null && (
         <div className="flex-between bg-bg-50 dark:bg-bg-900 flex w-full flex-col gap-3 rounded-lg p-4 shadow-lg">
           <div className="flex-between flex w-full flex-col gap-3 md:flex-row md:gap-8">
