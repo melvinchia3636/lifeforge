5323f6f8341df023b3d5988d4fc3a9d2ac09e33d
melvinchia3636 <melvinchia623600@gmail.com>
2025-09-14 11:16:38 +0800

    fix(server): update image generation response to adhere to new SDK
    Former-commit-id: c9c209870b2244218324ce53ad33012050882115 [formerly 7c38dcbf7867eec487289eb3dfb7f4d6f82624f8] [formerly 12b6101c38146f241c33e6ff30d0def9a3b1dfb1 [formerly 632a9d3a201b4ba8c5e95e320d8982c608e2f019]]
    Former-commit-id: 4be27abe31941520977db889b0219c07d950448f [formerly 96ef40898b898392fca87b46c0b453d64d700a27]
    Former-commit-id: e5fa88d74f0c01504a581f74496787c398604d1d

--- server/src/lib/ai/routes/imageGeneration.ts
+++ server/src/lib/ai/routes/imageGeneration.ts
@@ -32,15 +32,16 @@ const generateImage = forgeController
     const response = await openai.images.generate({
       model: 'gpt-image-1',
       prompt,
-      n: 1,
       size: '1536x1024'
     })
 
-    if (!response.data?.[0].url) {
+    const image_base64 = response.data?.[0].b64_json
+
+    if (!image_base64) {
       throw new Error('No image generated')
     }
 
-    return response.data[0].url
+    return `data:image/png;base64,${image_base64}`
   })
 
 export default forgeRouter({ verifyAPIKey, generateImage })
