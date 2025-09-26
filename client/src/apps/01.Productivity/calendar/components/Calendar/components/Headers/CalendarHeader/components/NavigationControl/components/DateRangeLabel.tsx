1da8d81f10aa823d6b4d8caa2ec0af2564aabde0
melvinchia3636 <melvinchia623600@gmail.com>
2025-09-26 08:30:26 +0800

    feat(client): add YearMonthSelector modal for selecting year and month in Calendar
    Former-commit-id: 36412fc71b3b3426aefc4daf0744c64668bdba82 [formerly 4762e204dee519841b7e8de8d3193e71ffa7beac] [formerly 7fb292b2270267d89628eea7d3a8ef3d7665145f [formerly 5d9a352fa56f63bbc0142e8cd5821be04c1ed6c3]]
    Former-commit-id: 92fecd0dd6fd655abf2e0cc869442906b349f791 [formerly 1a69b9317c7331e4e0a5850bfaa6b54589598aaf]
    Former-commit-id: 6eab80f475793d8276f8b8542f02acc7d4b7529a

--- client/src/apps/01.Productivity/calendar/components/Calendar/components/Headers/CalendarHeader/components/NavigationControl/components/DateRangeLabel.tsx
+++ client/src/apps/01.Productivity/calendar/components/Calendar/components/Headers/CalendarHeader/components/NavigationControl/components/DateRangeLabel.tsx
@@ -1,12 +1,43 @@
 import dayjs from 'dayjs'
+import { useModalStore } from 'lifeforge-ui'
+import { useCallback } from 'react'
+import type { NavigateAction } from 'react-big-calendar'
 
-function DateRangeLabel({ label }: { label: string }) {
+import YearMonthSelector from '@apps/01.Productivity/calendar/components/modals/YearMonthSelector'
+
+function DateRangeLabel({
+  label,
+  onNavigate
+}: {
+  label: string
+  onNavigate?: (direction: NavigateAction, date?: Date) => void
+}) {
+  const open = useModalStore(state => state.open)
+
+  const handleOpenYearMonthSelector = useCallback(() => {
+    if (onNavigate) {
+      open(YearMonthSelector, {
+        onSelect: (year: number, month: number) => {
+          onNavigate('DATE', new Date(year, month - 1, 1))
+        }
+      })
+    }
+  }, [onNavigate])
+
   if (label.match(/^(\w+)\s(\w+)$/)) {
     return (
-      <div className="flex items-end gap-2">
-        <span>{label.split(' ')[0]}</span>
+      <button
+        className="hover:bg-bg-900 flex min-w-0 cursor-pointer items-end gap-2 rounded-md p-2 px-3 transition-all"
+        onClick={handleOpenYearMonthSelector}
+      >
+        <span className="hidden w-full min-w-0 truncate sm:inline">
+          {label.split(' ')[0]}
+        </span>
+        <span className="w-full min-w-0 truncate sm:hidden">
+          {label.split(' ')[0].split('').slice(0, 3).join('')}
+        </span>
         <span className="text-bg-500">{label.split(' ')[1]}</span>
-      </div>
+      </button>
     )
   }
 
