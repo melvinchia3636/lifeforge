f772b19a73b2083477cbfb0e645037dcc5f68f7e
melvinchia3636 <melvinchia623600@gmail.com>
2025-10-03 10:44:16 +0800

    refactor(server): remove created and updated timestamps from various schema files

--- server/src/core/schema.ts
+++ server/src/core/schema.ts
@@ -1,26 +1,26 @@
-import flattenSchemas from '@functions/utils/flattenSchema'
+import flattenSchemas from "@functions/utils/flattenSchema";
 
 export const SCHEMAS = {
-  users: (await import('@lib/user/schema')).default,
-  todo_list: (await import('@lib/todoList/schema')).default,
-  code_time: (await import('@lib/codeTime/schema')).default,
-  idea_box: (await import('@lib/ideaBox/schema')).default,
-  passwords: (await import('@lib/passwords/schema')).default,
-  journal: (await import('@lib/journal/schema')).default,
-  calendar: (await import('@lib/calendar/schema')).default,
-  music: (await import('@lib/music/schema')).default,
-  achievements: (await import('@lib/achievements/schema')).default,
-  wallet: (await import('@lib/wallet/schema')).default,
-  scores_library: (await import('@lib/scoresLibrary/schema')).default,
-  api_keys: (await import('@lib/apiKeys/schema')).default,
-  books_library: (await import('@lib/booksLibrary/schema')).default,
-  wishlist: (await import('@lib/wishlist/schema')).default,
-  moment_vault: (await import('@lib/momentVault/schema')).default,
-  movies: (await import('@lib/movies/schema')).default,
-  railway_map: (await import('@lib/railwayMap/schema')).default,
-  blog: (await import('@lib/blog/schema')).default
-}
+  users: (await import("@lib/user/schema")).default,
+  todo_list: (await import("@lib/todoList/schema")).default,
+  code_time: (await import("@lib/codeTime/schema")).default,
+  idea_box: (await import("@lib/ideaBox/schema")).default,
+  passwords: (await import("@lib/passwords/schema")).default,
+  journal: (await import("@lib/journal/schema")).default,
+  calendar: (await import("@lib/calendar/schema")).default,
+  music: (await import("@lib/music/schema")).default,
+  achievements: (await import("@lib/achievements/schema")).default,
+  wallet: (await import("@lib/wallet/schema")).default,
+  scores_library: (await import("@lib/scoresLibrary/schema")).default,
+  api_keys: (await import("@lib/apiKeys/schema")).default,
+  books_library: (await import("@lib/booksLibrary/schema")).default,
+  wishlist: (await import("@lib/wishlist/schema")).default,
+  moment_vault: (await import("@lib/momentVault/schema")).default,
+  movies: (await import("@lib/movies/schema")).default,
+  railway_map: (await import("@lib/railwayMap/schema")).default,
+  blog: (await import("@lib/blog/schema")).default,
+};
 
-const COLLECTION_SCHEMAS = flattenSchemas(SCHEMAS)
+const COLLECTION_SCHEMAS = flattenSchemas(SCHEMAS);
 
-export default COLLECTION_SCHEMAS
+export default COLLECTION_SCHEMAS;
