1e6cce9faa6bc65fae2cba90a805827b7f3aee8a
melvinchia3636 <melvinchia623600@gmail.com>
2025-10-04 15:29:43 +0800

    feat: backend migration complete
    Former-commit-id: ceb74c1c4eb1720c3fb7fd30989e5b92044f224c [formerly 20731945f71827b17abbcdd95ceaf90770b9e663] [formerly 4c19721053a662a7dbc174621e8ec28e296e980b [formerly e62a1c5ba161854777e8b9b2bc07a550428b292a]]
    Former-commit-id: 50144dc8556ce3062ea092e0a1c3005721ae551a [formerly 844a252350887b14c363a3ceb6faa89a0b29ca44]
    Former-commit-id: 19c7019f1fb67c8e8a8940b5e07eba14c4474791

--- server/src/core/routes/app.routes.ts
+++ server/src/core/routes/app.routes.ts
@@ -2,26 +2,26 @@ import { forgeRouter } from '@functions/routes'
 
 const appRoutes = forgeRouter({
   achievements: (await import('@lib/achievements/server')).default,
-  calendar: (await import('@lib/calendar')).default,
-  todoList: (await import('@lib/todoList')).default,
-  ideaBox: (await import('@lib/ideaBox')).default,
-  'code-time': (await import('@lib/codeTime')).default,
-  booksLibrary: (await import('@lib/booksLibrary')).default,
+  calendar: (await import('@lib/calendar/server')).default,
+  todoList: (await import('@lib/todoList/server')).default,
+  ideaBox: (await import('@lib/ideaBox/server')).default,
+  'code-time': (await import('@lib/codeTime/server')).default,
+  booksLibrary: (await import('@lib/booksLibrary/server')).default,
   wallet: (await import('@lib/wallet/server')).default,
-  wishlist: (await import('@lib/wishlist')).default,
-  scoresLibrary: (await import('@lib/scoresLibrary')).default,
-  passwords: (await import('@lib/passwords')).default,
-  sudoku: (await import('@lib/sudoku')).default,
-  momentVault: (await import('@lib/momentVault')).default,
-  movies: (await import('@lib/movies')).default,
-  railwayMap: (await import('@lib/railwayMap')).default,
-  youtubeSummarizer: (await import('@lib/youtubeSummarizer')).default,
-  blog: (await import('@lib/blog')).default,
-  changiAirportFlightStatus: (await import('@lib/changiAirportFlightStatus'))
-    .default,
-  music: (await import('@lib/music')).default,
-  sinChewDaily: (await import('@lib/sinChewDaily')).default,
-  journal: (await import('@lib/journal')).default
+  wishlist: (await import('@lib/wishlist/server')).default,
+  scoresLibrary: (await import('@lib/scoresLibrary/server')).default,
+  passwords: (await import('@lib/passwords/server')).default,
+  sudoku: (await import('@lib/sudoku/server')).default,
+  momentVault: (await import('@lib/momentVault/server')).default,
+  movies: (await import('@lib/movies/server')).default,
+  railwayMap: (await import('@lib/railwayMap/server')).default,
+  youtubeSummarizer: (await import('@lib/youtubeSummarizer/server')).default,
+  blog: (await import('@lib/blog/server')).default,
+  changiAirportFlightStatus: (
+    await import('@lib/changiAirportFlightStatus/server')
+  ).default,
+  music: (await import('@lib/music/server')).default,
+  sinChewDaily: (await import('@lib/sinChewDaily/server')).default
 })
 
 export default appRoutes
