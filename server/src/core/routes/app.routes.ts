07c5ed0801f9bd3c47460cb5d2685733e1ffacdf
Melvin Chia <melvinchia623600@gmail.com>
2025-10-05 20:13:06 +0800

    refactor: remove module imports from uninstalled modules
    Former-commit-id: 8daa906beedea5077cc36b31307dfbaecef08440 [formerly 193ae63be27d769191b666946a7c797c9ef0314e] [formerly 016565821f842ee55ca818ec38eed651c72917ab [formerly 923f7052cce4e1435f347b9c68e9031dd56b4fea]]
    Former-commit-id: c83934a092354418b1f5392fa42a57c69594d14e [formerly c59e7130fec22d8c8901fb01f75ad7bf77169cf5]
    Former-commit-id: 78e7b63e4aa5a0597ea830ac875ed0916a74cbaa

--- server/src/core/routes/app.routes.ts
+++ server/src/core/routes/app.routes.ts
@@ -1,27 +1,5 @@
 import { forgeRouter } from '@functions/routes'
 
-const appRoutes = forgeRouter({
-  achievements: (await import('@lib/achievements/server')).default,
-  calendar: (await import('@lib/calendar/server')).default,
-  todoList: (await import('@lib/todoList/server')).default,
-  ideaBox: (await import('@lib/ideaBox/server')).default,
-  'code-time': (await import('@lib/codeTime/server')).default,
-  booksLibrary: (await import('@lib/booksLibrary/server')).default,
-  wallet: (await import('@lib/wallet/server')).default,
-  wishlist: (await import('@lib/wishlist/server')).default,
-  scoresLibrary: (await import('@lib/scoresLibrary/server')).default,
-  passwords: (await import('@lib/passwords/server')).default,
-  sudoku: (await import('@lib/sudoku/server')).default,
-  momentVault: (await import('@lib/momentVault/server')).default,
-  movies: (await import('@lib/movies/server')).default,
-  railwayMap: (await import('@lib/railwayMap/server')).default,
-  youtubeSummarizer: (await import('@lib/youtubeSummarizer/server')).default,
-  blog: (await import('@lib/blog/server')).default,
-  changiAirportFlightStatus: (
-    await import('@lib/changiAirportFlightStatus/server')
-  ).default,
-  music: (await import('@lib/music/server')).default,
-  sinChewDaily: (await import('@lib/sinChewDaily/server')).default
-})
+const appRoutes = forgeRouter({})
 
 export default appRoutes
