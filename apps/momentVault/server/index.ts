e91c9ef8ccf96f3905a291d12fbb93a57371fe02
Melvin Chia <melvinchia623600@gmail.com>
2025-10-05 19:28:20 +0800

    feat: officially remove modules from the core

--- apps/momentVault/server/index.ts
+++
@@ -1,9 +0,0 @@
-import { forgeRouter } from '@functions/routes'
-
-import entriesRouter from './routes/entries'
-import transcriptionRouter from './routes/transcription'
-
-export default forgeRouter({
-  entries: entriesRouter,
-  transcribe: transcriptionRouter
-})
