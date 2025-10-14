f3d16f05247e1867093195a1b91877c4f5e6a5fe
Melvin Chia <melvinchia623600@gmail.com>
2025-10-14 17:26:30 +0800

    feat(cli): add initialization command for PocketBase and enhance schema generation

---
+++ tools/forgeCLI/src/commands/db-commands/commands/initialize-database.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+
+import {
+  checkRunningPBInstances,
+  validateEnvironment
+} from '../../../utils/helpers'
+import {
+  completeInitialization,
+  createPocketBaseSuperuser,
+  ensureEnvironmentFile,
+  runDatabaseMigrations,
+  setupDefaultData,
+  startPocketBaseAndGetPid,
+  updateEnvironmentFile,
+  validatePocketBaseNotInitialized
+} from '../functions/database-initialization'
+import { validatePocketBaseSetup } from '../utils'
+
+export async function initializeDatabaseHandler(
+  email: string,
+  password: string
+) {
+  const envPath = ensureEnvironmentFile()
+
+  validateEnvironment(['PB_DIR'])
+
+  const { pbInstancePath, pbDir } = await validatePocketBaseSetup(
+    process.env.PB_DIR!
+  )
+
+  checkRunningPBInstances()
+  validatePocketBaseNotInitialized(pbDir)
+  createPocketBaseSuperuser(pbInstancePath, email, password)
+  runDatabaseMigrations(pbInstancePath)
+  await updateEnvironmentFile(envPath, email, password)
+  dotenv.config({ path: envPath, quiet: true })
+  validateEnvironment(['PB_HOST', 'PB_EMAIL', 'PB_PASSWORD'])
+
+  const pbPid = await startPocketBaseAndGetPid(pbInstancePath)
+
+  await setupDefaultData(email, password)
+  completeInitialization(pbPid)
+}
