61f867b6d4845c3c125f80498351c5a511e504a0
melvinchia3636 <melvinchia623600@gmail.com>
2025-10-18 17:00:14 +0800

    -
    Former-commit-id: 77550e039028b22bd9e2da91695d13f6b092b851 [formerly 6828928c5cd5473c6c8db24acf63da6bae8c5883] [formerly 3d8caa2a874d149b8a7a2b797363a4b4d5289b15 [formerly 0348d2f9977cf5746a28ff6ce2ae5998ade0dfa0]]
    Former-commit-id: c673cd51e8f3688b0b23fbf45c4ef48d1de8fab5 [formerly 6f962fb18ac42596b40f8b24bbcbf8c6c64ef6a8]
    Former-commit-id: 983fb5eb215781dbbb5e7cacd244154b72c97ea1

--- server/src/core/routes/app.routes.ts
+++ server/src/core/routes/app.routes.ts
@@ -1,3 +1,5 @@
 import { forgeRouter } from '@functions/routes';
-const appRoutes = forgeRouter({});
+const appRoutes = forgeRouter({
+  achievements: (await import('@lib/achievements/server')).default
+});
 export default appRoutes;
--- server/src/core/schema.ts
+++ server/src/core/schema.ts
@@ -1,10 +1,8 @@
-import flattenSchemas from '@functions/utils/flattenSchema'
-
+import flattenSchemas from '@functions/utils/flattenSchema';
 export const SCHEMAS = {
   user: (await import('@lib/user/schema')).default,
-  api_keys: (await import('@lib/apiKeys/schema')).default
-}
-
-const COLLECTION_SCHEMAS = flattenSchemas(SCHEMAS)
-
-export default COLLECTION_SCHEMAS
+  api_keys: (await import('@lib/apiKeys/schema')).default,
+  achievements: (await import('@lib/achievements/server/schema')).default
+};
+const COLLECTION_SCHEMAS = flattenSchemas(SCHEMAS);
+export default COLLECTION_SCHEMAS;
