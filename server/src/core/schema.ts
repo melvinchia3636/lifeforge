b6580b74e779a13d3782350bcf5f0f51d69dc51b
melvinchia3636 <melvinchia623600@gmail.com>
2025-10-18 22:01:38 +0800

    -
    Former-commit-id: c43704003247a20e35e0986042cddae6a9cac962 [formerly 98d7e22990a7f82edb9cd1915a42d0a60d431334] [formerly 31647996784718320349c2d895c16abe751751c7 [formerly a5a5da99ce24ff425f9e3c5819da2a1e4b5ee827]]
    Former-commit-id: b079fc63fb1a9e955be5d412ba0622a99d5d73d1 [formerly 88ced62a4d6f63f227718ec6bcbb4c82df63f76d]
    Former-commit-id: 22f2532a10109c1c77efb6e50dc4fb5fa8fbef16

--- server/src/core/schema.ts
+++ server/src/core/schema.ts
@@ -1,8 +1,23 @@
-import flattenSchemas from '@functions/utils/flattenSchema';
+import flattenSchemas from '@functions/utils/flattenSchema'
+
 export const SCHEMAS = {
   user: (await import('@lib/user/schema')).default,
+  todo_list: (await import('@lib/todoList/server/schema')).default,
+  code_time: (await import('@lib/codeTime/server/schema')).default,
+  idea_box: (await import('@lib/ideaBox/server/schema')).default,
+  passwords: (await import('@lib/passwords/server/schema')).default,
+  calendar: (await import('@lib/calendar/server/schema')).default,
+  music: (await import('@lib/music/server/schema')).default,
+  achievements: (await import('@lib/achievements/server/schema')).default,
+  wallet: (await import('@lib/wallet/server/schema')).default,
+  scores_library: (await import('@lib/scoresLibrary/server/schema')).default,
   api_keys: (await import('@lib/apiKeys/schema')).default,
-  achievements: (await import('@lib/achievements/server/schema')).default
-};
-const COLLECTION_SCHEMAS = flattenSchemas(SCHEMAS);
-export default COLLECTION_SCHEMAS;
+  books_library: (await import('@lib/booksLibrary/server/schema')).default,
+  moment_vault: (await import('@lib/momentVault/server/schema')).default,
+  movies: (await import('@lib/movies/server/schema')).default,
+  railway_map: (await import('@lib/railwayMap/server/schema')).default
+}
+
+const COLLECTION_SCHEMAS = flattenSchemas(SCHEMAS)
+
+export default COLLECTION_SCHEMAS
