3d83194f149d80a73812ba9f32a593d0a585b8a1
Melvin Chia <melvinchia623600@gmail.com>
2025-10-25 16:14:59 +0800

    feat(ui): add retry button to ErrorScreen component
    Former-commit-id: 7feb5fee61ff5c769b533d15133e563c3aacec55 [formerly 569c34ff82b8decca0ddb3c743ada62086d67f3d] [formerly 7ba771527ec4ebc58f136177aface206c98a7537 [formerly c7a72162a326633c5504e5dbe78a831cc6479a4f]]
    Former-commit-id: e78506827ef889dc5949093f89684905eb507a16 [formerly 06674ae024cfbc9d81822ce871a867a97cace61f]
    Former-commit-id: be41b74140d98decd3d53a15bd382e0d4c029f86

--- packages/lifeforge-ui/src/components/screens/ErrorScreen.tsx
+++ packages/lifeforge-ui/src/components/screens/ErrorScreen.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@components/buttons'
 import { Icon } from '@iconify/react'
 
 function ErrorScreen({ message }: { message: string }) {
@@ -5,6 +6,9 @@ function ErrorScreen({ message }: { message: string }) {
     <div className="flex-center size-full flex-col gap-6">
       <Icon className="size-12 text-red-500" icon="tabler:alert-triangle" />
       <p className="text-center text-lg font-medium text-red-500">{message}</p>
+      <Button variant="secondary" onClick={() => window.location.reload()}>
+        Retry
+      </Button>
     </div>
   )
 }
