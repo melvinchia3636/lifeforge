3e02418f9d4baa91a8af91f5147e8296f20abb80
Melvin Chia <melvinchia623600@gmail.com>
2025-11-16 00:02:19 +0800

    fix(ui): ensure 'block' class is conditionally applied based on className prop in ItemWrapper

--- packages/lifeforge-ui/src/components/layouts/ItemWrapper.tsx
+++ packages/lifeforge-ui/src/components/layouts/ItemWrapper.tsx
@@ -25,10 +25,11 @@ function ItemWrapper<C extends React.ElementType = 'div'>({
     <Component
       {...props}
       className={clsx(
-        'shadow-custom relative block rounded-lg p-4',
+        'shadow-custom relative rounded-lg p-4',
         isInteractive
           ? 'component-bg-with-hover cursor-pointer transition-all'
           : 'component-bg',
+        !className?.includes('flex') && 'block',
         className
       )}
     >
