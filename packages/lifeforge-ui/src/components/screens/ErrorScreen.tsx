72a89b242a71c950d266a5380b9f322be3265525
Melvin Chia <melvinchia623600@gmail.com>
2025-11-26 11:54:07 +0800

    feat(ui): add showRetryButton prop to ErrorScreen and WithQuery components

--- packages/lifeforge-ui/src/components/screens/ErrorScreen.tsx
+++ packages/lifeforge-ui/src/components/screens/ErrorScreen.tsx
@@ -1,18 +1,26 @@
 import { Button } from '@components/buttons'
 import { Icon } from '@iconify/react'
 
-function ErrorScreen({ message }: { message: string }) {
+function ErrorScreen({
+  message,
+  showRetryButton
+}: {
+  message: string
+  showRetryButton?: boolean
+}) {
   return (
     <div className="flex-center size-full flex-col gap-6">
       <Icon className="size-12 text-red-500" icon="tabler:alert-triangle" />
       <p className="text-center text-lg font-medium text-red-500">{message}</p>
-      <Button
-        icon="tabler:refresh"
-        variant="secondary"
-        onClick={() => window.location.reload()}
-      >
-        Retry
-      </Button>
+      {showRetryButton && (
+        <Button
+          icon="tabler:refresh"
+          variant="secondary"
+          onClick={() => window.location.reload()}
+        >
+          Retry
+        </Button>
+      )}
     </div>
   )
 }
