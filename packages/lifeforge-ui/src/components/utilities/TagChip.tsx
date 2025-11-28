c308577efe817bf480017e73b89c1bf0cc6e5926
Melvin Chia <melvinchia623600@gmail.com>
2025-11-28 22:56:11 +0800

    feat(ui): add onClick prop and refactor color handling in TagChip component

--- packages/lifeforge-ui/src/components/utilities/TagChip.tsx
+++ packages/lifeforge-ui/src/components/utilities/TagChip.tsx
@@ -1,13 +1,14 @@
-import { oklchToHex } from '@components/inputs/ColorInput/ColorPickerModal/components/modals/TailwindCSSColorsModal/utils/colors'
 import { Icon } from '@iconify/react'
 import clsx from 'clsx'
+import { converter, formatHex, parse } from 'culori/fn'
 import tinycolor from 'tinycolor2'
 
 function TagChip({
   label,
   icon,
   color,
-  actionButtonProps
+  actionButtonProps,
+  onClick
 }: {
   label: string
   icon?: string
@@ -16,27 +17,33 @@ function TagChip({
     icon: string
     onClick: () => void
   }
+  onClick?: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void
 }) {
   return (
     <span
       className={clsx(
-        'flex-center shadow-custom gap-1 rounded-full border px-3 py-1 text-sm',
+        'flex-center shadow-custom shrink-0 gap-1 rounded-full border px-3 py-1 text-sm whitespace-nowrap!',
         color === undefined &&
-          `text-bg-500 dark:text-bg-400 border-bg-200 dark:border-bg-700/50 component-bg-lighter bg-bg-50`
+          `text-bg-500 dark:text-bg-400 border-bg-200 dark:border-bg-700/50 component-bg-lighter bg-bg-50`,
+        onClick !== undefined &&
+          'cursor-pointer transition-all hover:brightness-120'
       )}
       style={
         color !== undefined
           ? {
-              borderColor: tinycolor(oklchToHex(color))
+              borderColor: tinycolor(formatHex(converter('rgb')(parse(color))))
                 .setAlpha(0.25)
                 .toString(),
-              backgroundColor: tinycolor(oklchToHex(color))
+              backgroundColor: tinycolor(
+                formatHex(converter('rgb')(parse(color)))
+              )
                 .setAlpha(0.125)
                 .toString(),
               color
             }
           : {}
       }
+      onClick={onClick}
     >
       {(() => {
         if (!icon) return null
