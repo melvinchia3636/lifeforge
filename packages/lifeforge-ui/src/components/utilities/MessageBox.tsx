ed1b6473d2712fdd4114f2b36de5f46887fb179c
Melvin Chia <melvinchia623600@gmail.com>
2025-11-28 17:59:04 +0800

    feat(ui): expand MessageBox styles to include caution, tip, and important types

--- packages/lifeforge-ui/src/components/utilities/MessageBox.tsx
+++ packages/lifeforge-ui/src/components/utilities/MessageBox.tsx
@@ -1,4 +1,5 @@
 import { Icon } from '@iconify/react'
+import clsx from 'clsx'
 import React from 'react'
 
 const STYLES = {
@@ -15,6 +16,27 @@ const STYLES = {
     bgColor: 'bg-yellow-500',
     textColor: 'text-yellow-500',
     before: 'before:bg-yellow-500'
+  },
+  caution: {
+    title: 'Caution',
+    icon: 'tabler:alert-hexagon',
+    bgColor: 'bg-orange-500',
+    textColor: 'text-orange-500',
+    before: 'before:bg-orange-500'
+  },
+  tip: {
+    title: 'Tip',
+    icon: 'tabler:bulb',
+    bgColor: 'bg-green-500',
+    textColor: 'text-green-500',
+    before: 'before:bg-green-500'
+  },
+  important: {
+    title: 'Important',
+    icon: 'tabler:message-exclamation',
+    bgColor: 'bg-purple-500',
+    textColor: 'text-purple-500',
+    before: 'before:bg-purple-500'
   }
 }
 
@@ -23,15 +45,15 @@ function MessageBox({
   className,
   children
 }: {
-  type: 'note' | 'warning'
+  type: typeof STYLES extends Record<infer K, unknown> ? K : never
   className?: string
   children: React.ReactNode
 }) {
   return (
     <div
-      className={`relative w-full space-y-4 rounded-md p-2 pl-6 before:absolute before:left-0 before:top-0 before:h-full before:w-[4px] before:rounded-full ${STYLES[type].before} ${className}`}
+      className={`relative w-full space-y-4 rounded-md p-2 pl-6 before:absolute before:top-0 before:left-0 before:h-full before:w-1 before:rounded-full ${STYLES[type].before} ${className}`}
     >
-      <div className="flex items-center gap-2 text-blue-500">
+      <div className={clsx('flex items-center gap-2', STYLES[type].textColor)}>
         <Icon className="h-6 w-6" icon={STYLES[type].icon} />
         <h4 className="text-lg font-medium">{STYLES[type].title}</h4>
       </div>
