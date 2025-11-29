9a38e23de1ac4e6aef41f33bdccd7fb0c095fc52
Melvin Chia <melvinchia623600@gmail.com>
2025-11-29 11:30:21 +0800

    feat(ui): refactor ViewModeSelector to use currentMode and onModeChange props

--- packages/lifeforge-ui/src/components/utilities/ViewModeSelector.tsx
+++ packages/lifeforge-ui/src/components/utilities/ViewModeSelector.tsx
@@ -1,15 +1,21 @@
 import { Icon } from '@iconify/react'
 import clsx from 'clsx'
 
-function ViewModeSelector<T extends string>({
-  viewMode,
-  setViewMode,
+/**
+ * A view mode selector for switching between different view modes. Nothing too fancy.
+ */
+function ViewModeSelector<
+  T extends ReadonlyArray<{ value: string; icon: string }>,
+  TKey = T[number]['value']
+>({
+  currentMode,
+  onModeChange,
   options,
   className
 }: {
-  viewMode: T
-  setViewMode: (value: T) => void
-  options: Array<{ value: T; icon: string }>
+  currentMode: TKey
+  onModeChange: (value: TKey) => void
+  options: T
   className?: string
 }) {
   return (
@@ -24,12 +30,12 @@ function ViewModeSelector<T extends string>({
           key={value}
           className={clsx(
             'flex items-center gap-2 rounded-md p-3 transition-all',
-            value === viewMode
+            value === currentMode
               ? 'bg-bg-200/50 dark:bg-bg-800/70 shadow-custom'
               : 'text-bg-500 hover:text-bg-800 dark:hover:text-bg-50'
           )}
           onClick={() => {
-            setViewMode(value)
+            onModeChange(value as TKey)
           }}
         >
           <Icon className="size-6" icon={icon} />
