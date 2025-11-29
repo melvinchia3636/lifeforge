c3f7e9704586612f9a6b3be995a4f622c851ed1b
Melvin Chia <melvinchia623600@gmail.com>
2025-11-29 11:26:08 +0800

    fix(ui): correct export of Card component in layouts index file

--- packages/lifeforge-ui/src/components/layouts/index.ts
+++ packages/lifeforge-ui/src/components/layouts/index.ts
@@ -2,6 +2,6 @@ export { default as ModuleWrapper } from './ModuleWrapper'
 
 export { default as ModuleHeader } from './ModuleHeader'
 
-export { default as ItemWrapper } from './Card'
+export { default as Card } from './Card'
 
 export { useModuleSidebarState } from './ModuleSidebarStateProvider'
