1dfabd6001df822955ab2785f028b6b679942412
Melvin Chia <melvinchia623600@gmail.com>
2025-11-29 15:27:45 +0800

    feat(ui): add stories for Sidebar components

--- packages/lifeforge-ui/src/components/navigation/sidebar/SidebarTitle.tsx
+++ packages/lifeforge-ui/src/components/navigation/sidebar/SidebarTitle.tsx
@@ -3,38 +3,36 @@ import clsx from 'clsx'
 import _ from 'lodash'
 import { useTranslation } from 'react-i18next'
 
-interface PropsWithActionButton {
-  actionButtonIcon: string | undefined
-  actionButtonOnClick: (() => void) | undefined
-}
-
-interface PropsWithoutActionButton {
-  actionButtonIcon?: never
-  actionButtonOnClick?: never
-}
-
-type SidebarItemProps = {
+type SidebarTitleProps = {
+  /** Label string or React element to display for the sidebar title. */
   label: string
-  namespace?: string
+  /** Additional CSS classes to apply to the sidebar title. */
   className?: string
-  customActionButton?: React.ReactElement
-} & (PropsWithActionButton | PropsWithoutActionButton)
+  /** Action button to display on the right side of the sidebar title. */
+  actionButton?:
+    | React.ReactElement
+    | {
+        icon: string
+        onClick: () => void
+      }
+  /** The i18n namespace for internationalization. See the [main documentation](https://docs.lifeforge.melvinchia.dev) for more details. */
+  namespace?: string
+}
 
+/** A title for a section in the sidebar navigation. */
 function SidebarTitle({
   label,
   className,
-  actionButtonIcon,
-  actionButtonOnClick,
-  customActionButton,
+  actionButton,
   namespace
-}: SidebarItemProps) {
+}: SidebarTitleProps) {
   const { t } = useTranslation([namespace, 'common.sidebar'])
 
   return (
     <li
       className={clsx(
         'flex-between flex gap-3 pt-2 pr-5 pl-8 transition-all',
-        actionButtonIcon !== undefined ? 'pb-2' : 'pb-4'
+        actionButton !== undefined ? 'pb-2' : 'pb-4'
       )}
     >
       <h3
@@ -49,18 +47,19 @@ function SidebarTitle({
           label
         ])}
       </h3>
-      {customActionButton ??
-        (actionButtonIcon !== undefined && (
-          <button
-            className={clsx(
-              'text-bg-600 flex items-center rounded-md p-2 transition-all',
-              'hover:bg-bg-100 dark:hover:bg-bg-800 dark:hover:text-bg-50'
-            )}
-            onClick={actionButtonOnClick}
-          >
-            <Icon className="size-5" icon={actionButtonIcon} />
-          </button>
-        ))}
+      {actionButton && 'icon' in actionButton ? (
+        <button
+          className={clsx(
+            'text-bg-400 dark:text-bg-600 flex items-center rounded-md p-2 transition-all',
+            'hover:bg-bg-100 dark:hover:bg-bg-800 dark:hover:text-bg-50'
+          )}
+          onClick={actionButton.onClick}
+        >
+          <Icon className="size-5" icon={actionButton.icon} />
+        </button>
+      ) : (
+        actionButton
+      )}
     </li>
   )
 }
