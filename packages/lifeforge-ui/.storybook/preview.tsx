c740b8d3f47997aa87c1cae7260bd01f7f4b0ef3
Melvin Chia <melvinchia623600@gmail.com>
2025-11-29 11:25:18 +0800

    feat(ui): add JetBrains Mono and Onest fonts, update Storybook manager styles, and improve theme handling in preview

--- packages/lifeforge-ui/.storybook/preview.tsx
+++ packages/lifeforge-ui/.storybook/preview.tsx
@@ -1,5 +1,4 @@
 import {
-  Canvas,
   Controls,
   Description,
   Primary,
@@ -49,7 +48,12 @@ const withBodyClass = (Story: any, context: any) => {
   useEffect(() => {
     const body = document.body
     document.querySelectorAll('.sbdocs-preview').forEach(preview => {
-      preview.classList.add('bg-white!', 'border-bg-800!')
+      preview.classList.remove('bg-black!', 'bg-white!', 'border-bg-800!')
+
+      preview.classList.add(
+        context.globals.theme === 'dark' ? 'bg-black!' : 'bg-white!',
+        'border-bg-800!'
+      )
     })
 
     const sbDocsPreviews = document.querySelectorAll(
@@ -58,7 +62,7 @@ const withBodyClass = (Story: any, context: any) => {
     if (!body) return
 
     body.classList.remove(
-      'bg-bg-950!',
+      'bg-bg-900/50!',
       'text-bg-50',
       'bg-bg-200/50!',
       'text-bg-800'
@@ -66,7 +70,7 @@ const withBodyClass = (Story: any, context: any) => {
 
     sbDocsPreviews.forEach(preview => {
       preview.classList.remove(
-        'bg-bg-900!',
+        'bg-bg-900/50!',
         'text-bg-50',
         'bg-bg-200/50!',
         'text-bg-800'
@@ -74,7 +78,7 @@ const withBodyClass = (Story: any, context: any) => {
 
       preview.classList.add(
         ...(context.globals.theme === 'dark'
-          ? ['bg-bg-900!', 'text-bg-50']
+          ? ['bg-bg-900/50!', 'text-bg-50']
           : ['bg-bg-200/50!', 'text-bg-800'])
       )
     })
@@ -89,7 +93,7 @@ const withBodyClass = (Story: any, context: any) => {
       ...(!document.querySelector('#storybook-docs:not([hidden])')
         ? [
             ...(context.globals.theme === 'dark'
-              ? ['bg-bg-950!', 'text-bg-50']
+              ? ['bg-bg-900/50!', 'text-bg-50']
               : ['bg-bg-200/50!', 'text-bg-800'])
           ]
         : [])
@@ -98,6 +102,13 @@ const withBodyClass = (Story: any, context: any) => {
     document.querySelectorAll('html, body').forEach(html => {
       html.classList.add('h-full')
     })
+
+    const html = document.documentElement
+    if (!html) return
+    html.classList.remove('bg-white!', 'bg-black!')
+    html.classList.add(
+      context.globals.theme === 'dark' ? 'bg-black!' : 'bg-white!'
+    )
   }, [context.globals.theme])
 
   return (
