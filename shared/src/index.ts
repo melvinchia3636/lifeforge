2b2a6687c84f316342750dc84352042fedf0f038
Melvin Chia <melvinchia623600@gmail.com>
2025-12-02 09:41:22 +0800

    feat(shared): organize main exporting file

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,5 +1,7 @@
+/* eslint-disable padding-line-between-statements */
 export * from 'react-router'
 
+// Re-export nuqs hooks and parsers due to version mismatch issues
 export {
   useQueryState,
   useQueryStates,
@@ -9,64 +11,50 @@ export {
   parseAsInteger
 } from 'nuqs'
 
+// Context providers
 export { useAPIEndpoint } from './providers/APIEndpointProvider'
-
 export { useAPIOnlineStatus } from './providers/APIOnlineStatusProvider'
-
 export { usePersonalization } from './providers/PersonalizationProvider'
-
 export { useMainSidebarState as useMainSidebarState } from './providers/SidebarStateProvider'
-
+export { useAuth } from './providers/AuthProvider'
 export { default as AuthProvider } from './providers/AuthProvider'
-
 export { default as APIEndpointProvider } from './providers/APIEndpointProvider'
-
 export { default as PersonalizationProvider } from './providers/PersonalizationProvider'
-
 export { default as BackgroundProvider } from './providers/BackgroundProvider'
-
 export { default as APIOnlineStatusProvider } from './providers/APIOnlineStatusProvider'
-
 export { default as MainSidebarStateProvider } from './providers/SidebarStateProvider'
-
 export { default as ToastProvider } from './providers/ToastProvider'
-
 export { default as SSOAuthProvider } from './providers/SSOAuthProvider'
-
 export { default as SocketProvider } from './providers/SocketProvider'
-
 export { default as NuqsProvider } from './providers/NuqsProvider'
-
-export { default as usePromiseLoading } from './hooks/usePromiseLoading'
-
 export { type SocketEvent, useSocketContext } from './providers/SocketProvider'
 
-export { useAuth } from './providers/AuthProvider'
+// Useful hooks
+export { default as usePromiseLoading } from './hooks/usePromiseLoading'
+export { default as useDivSize } from './hooks/useDivSize'
 
+// Useful utility functions
 export { default as forceDown } from './utils/forceDown'
-
 export { encrypt, decrypt } from './utils/encryption'
-
 export { default as getFormFileFieldInitialData } from './utils/getFormFileFieldInitialData'
 
+// Forge API client and types
 export {
   createForgeAPIClient,
   ForgeAPIClientController
 } from './api/core/forgeAPIClient'
-
 export type {
   InferClientControllerOutput as InferOutput,
   InferClientControllerInput as InferInput
 } from './api/typescript/forge_api_client.types'
 
+// Some shared types
 export type {
   IDashboardLayout,
   IBackdropFilters
 } from './providers/PersonalizationProvider/interfaces/personalization_provider_interfaces'
-
 export type {
   ModuleConfig,
   ModuleCategory
 } from './interfaces/module_config.types'
-
 export type { default as WidgetConfig } from './interfaces/widget_config.types'
