a8de0df88948fd84a890cc28f29b627faf98f969
Melvin Chia <melvinchia623600@gmail.com>
2025-12-05 12:11:16 +0800

    refactor(server): rename checkDB.ts to dbUtils.ts and move it into database function folder

--- server/src/core/functions/database/index.ts
+++ server/src/core/functions/database/index.ts
@@ -2,6 +2,4 @@ export { default as getAPIKey } from './getAPIKey'
 
 export { default as checkExistence } from './validation'
 
-export { default as getPBSuperUserInstance } from './getSuperuserInstance'
-
 export { default as PBService } from './PBService'
