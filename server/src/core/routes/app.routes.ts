cc8dc68c6c5d52b0cc6824e3985196ac9095b1cb
melvinchia3636 <melvinchia623600@gmail.com>
2025-12-12 11:38:09 +0800

    -

---
+++ server/src/core/routes/app.routes.ts
@@ -0,0 +1,25 @@
+import { forgeRouter } from '@functions/routes';
+const appRoutes = forgeRouter({
+  achievements: (await import('@lib/achievements/server')).default,
+  codeTime: (await import('@lib/codeTime/server')).default,
+  'code-time': (await import('@lib/codeTime/server')).default,
+  passwords: (await import('@lib/passwords/server')).default,
+  ideaBox: (await import('@lib/ideaBox/server')).default,
+  scoresLibrary: (await import('@lib/scoresLibrary/server')).default,
+  sinChewDaily: (await import('@lib/sinChewDaily/server')).default,
+  railwayMap: (await import('@lib/railwayMap/server')).default,
+  music: (await import('@lib/music/server')).default,
+  calendar: (await import('@lib/calendar/server')).default,
+  booksLibrary: (await import('@lib/booksLibrary/server')).default,
+  sudoku: (await import('@lib/sudoku/server')).default,
+  changiAirportFlightStatus: (await import('@lib/changiAirportFlightStatus/server')).default,
+  movies: (await import('@lib/movies/server')).default,
+  momentVault: (await import('@lib/momentVault/server')).default,
+  todoList: (await import('@lib/todoList/server')).default,
+  modrinth: (await import('@lib/modrinth/server')).default,
+  localIpWidget: (await import('@lib/localIpWidget/server')).default,
+  rentalPaymentTracker: (await import('@lib/rentalPaymentTracker/server')).default,
+  invoiceMaker: (await import('@lib/invoiceMaker/server')).default,
+  pomodoroTimer: (await import('@lib/pomodoroTimer/server')).default
+});
+export default appRoutes;
---
+++ server/src/core/schema.ts
@@ -0,0 +1,22 @@
+import flattenSchemas from '@functions/utils/flattenSchema';
+export const SCHEMAS = {
+  user: (await import('@lib/user/schema')).default,
+  todo_list: (await import('@lib/todoList/server/schema')).default,
+  idea_box: (await import('@lib/ideaBox/server/schema')).default,
+  passwords: (await import('@lib/passwords/server/schema')).default,
+  calendar: (await import('@lib/calendar/server/schema')).default,
+  music: (await import('@lib/music/server/schema')).default,
+  achievements: (await import('@lib/achievements/server/schema')).default,
+  scores_library: (await import('@lib/scoresLibrary/server/schema')).default,
+  code_time: (await import('@lib/codeTime/server/schema')).default,
+  api_keys: (await import('@lib/apiKeys/schema')).default,
+  books_library: (await import('@lib/booksLibrary/server/schema')).default,
+  moment_vault: (await import('@lib/momentVault/server/schema')).default,
+  movies: (await import('@lib/movies/server/schema')).default,
+  railway_map: (await import('@lib/railwayMap/server/schema')).default,
+  rental_payment_tracker: (await import('@lib/rentalPaymentTracker/server/schema')).default,
+  invoice_maker: (await import('@lib/invoiceMaker/server/schema')).default,
+  pomodoro_timer: (await import('@lib/pomodoroTimer/server/schema')).default
+};
+const COLLECTION_SCHEMAS = flattenSchemas(SCHEMAS);
+export default COLLECTION_SCHEMAS;
