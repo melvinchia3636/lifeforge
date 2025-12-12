fa19bec6af6a8d57b84ebfa5f68b5e43ffb4a5e8
Melvin Chia <melvinchia623600@gmail.com>
2025-12-12 18:14:10 +0800

    feat(ui): integrate WithQueryData component for API key validation in Pixabay

--- packages/lifeforge-ui/src/components/inputs/FileInput/FilePickerModal/components/Pixabay/index.tsx
+++ packages/lifeforge-ui/src/components/inputs/FileInput/FilePickerModal/components/Pixabay/index.tsx
@@ -9,6 +9,7 @@ import {
   LoadingScreen
 } from '@components/feedback'
 import { Button, SearchInput } from '@components/inputs'
+import { WithQueryData } from '@components/utilities'
 
 import SearchFilterModal from './components/SearchFilterModal'
 import SearchResults from './components/SearchResults'
@@ -97,105 +98,124 @@ function Pixabay({
   })
 
   return (
-    <>
-      <div className="flex w-full min-w-0 flex-col items-center gap-2 sm:flex-row">
-        <SearchInput
-          actionButtonProps={{
-            icon: 'tabler:filter',
-            onClick: () => {
-              setIsSearchFilterModalOpen(true)
-            },
-            children: [
-              filters.imageType !== 'all',
-              filters.category,
-              filters.colors,
-              filters.isEditorsChoice
-            ].filter(e => e).length
-          }}
-          className="component-bg-lighter-with-hover"
-          namespace="common.modals"
-          searchTarget="imagePicker.items.pixabay"
-          value={query}
-          onChange={setQuery}
-          onKeyUp={e => {
-            if (e.key === 'Enter') {
-              setPage(1)
-              onSearch(1).catch(console.error)
-            }
-          }}
-        />
-        <Button
-          className="w-full sm:w-auto"
-          icon="tabler:arrow-right"
-          iconPosition="end"
-          loading={loading}
-          onClick={() => {
-            setPage(1)
-            onSearch(1).catch(console.error)
-          }}
-        >
-          Search
-        </Button>
-      </div>
-      <div className="mt-6 flex h-full flex-1 flex-col">
-        {(() => {
-          switch (results) {
-            case 'error':
-              return (
-                <div className="flex-center size-full flex-1">
-                  <ErrorScreen message="Failed to fetch data" />
-                </div>
-              )
-            case null:
-              return loading ? (
-                <div className="flex-center size-full flex-1">
-                  <LoadingScreen />
-                </div>
-              ) : (
-                <div className="flex-center my-6 size-full flex-1">
-                  <EmptyStateScreen
-                    icon="simple-icons:pixabay"
-                    message={{
-                      id: 'pixabay',
-                      namespace: 'common.modals',
-                      tKey: 'imagePicker'
-                    }}
-                  />
-                </div>
-              )
-            default:
-              return results.total === 0 ? (
-                <EmptyStateScreen
-                  icon="tabler:photo-off"
-                  message={{
-                    id: 'results',
-                    namespace: 'common.modals',
-                    tKey: 'imagePicker'
-                  }}
-                />
-              ) : (
-                <SearchResults
-                  file={file}
-                  page={page}
-                  results={results}
-                  setFile={setFile}
-                  setPage={setPage}
-                  setPreview={setPreview}
-                  onSearch={onSearch}
-                />
-              )
-          }
-        })()}
-      </div>
-      <SearchFilterModal
-        filters={filters}
-        isOpen={isSearchFilterModalOpen}
-        updateFilters={updateFilters}
-        onClose={() => {
-          setIsSearchFilterModalOpen(false)
-        }}
-      />
-    </>
+    <WithQueryData
+      controller={forgeAPI.apiKeys.entries.checkKeys.setHost(apiHost).input({
+        keys: 'pixabay'
+      })}
+    >
+      {exists =>
+        exists ? (
+          <>
+            <div className="flex w-full min-w-0 flex-col items-center gap-2 sm:flex-row">
+              <SearchInput
+                actionButtonProps={{
+                  icon: 'tabler:filter',
+                  onClick: () => {
+                    setIsSearchFilterModalOpen(true)
+                  },
+                  children: [
+                    filters.imageType !== 'all',
+                    filters.category,
+                    filters.colors,
+                    filters.isEditorsChoice
+                  ].filter(e => e).length
+                }}
+                className="component-bg-lighter-with-hover"
+                namespace="common.modals"
+                searchTarget="imagePicker.items.pixabay"
+                value={query}
+                onChange={setQuery}
+                onKeyUp={e => {
+                  if (e.key === 'Enter') {
+                    setPage(1)
+                    onSearch(1).catch(console.error)
+                  }
+                }}
+              />
+              <Button
+                className="w-full sm:w-auto"
+                icon="tabler:arrow-right"
+                iconPosition="end"
+                loading={loading}
+                onClick={() => {
+                  setPage(1)
+                  onSearch(1).catch(console.error)
+                }}
+              >
+                Search
+              </Button>
+            </div>
+            <div className="mt-6 flex h-full flex-1 flex-col">
+              {(() => {
+                switch (results) {
+                  case 'error':
+                    return (
+                      <div className="flex-center size-full flex-1">
+                        <ErrorScreen message="Failed to fetch data" />
+                      </div>
+                    )
+                  case null:
+                    return loading ? (
+                      <div className="flex-center size-full flex-1">
+                        <LoadingScreen />
+                      </div>
+                    ) : (
+                      <div className="flex-center my-6 size-full flex-1">
+                        <EmptyStateScreen
+                          icon="simple-icons:pixabay"
+                          message={{
+                            id: 'pixabay',
+                            namespace: 'common.modals',
+                            tKey: 'imagePicker'
+                          }}
+                        />
+                      </div>
+                    )
+                  default:
+                    return results.total === 0 ? (
+                      <EmptyStateScreen
+                        icon="tabler:photo-off"
+                        message={{
+                          id: 'results',
+                          namespace: 'common.modals',
+                          tKey: 'imagePicker'
+                        }}
+                      />
+                    ) : (
+                      <SearchResults
+                        file={file}
+                        page={page}
+                        results={results}
+                        setFile={setFile}
+                        setPage={setPage}
+                        setPreview={setPreview}
+                        onSearch={onSearch}
+                      />
+                    )
+                }
+              })()}
+            </div>
+            <SearchFilterModal
+              filters={filters}
+              isOpen={isSearchFilterModalOpen}
+              updateFilters={updateFilters}
+              onClose={() => {
+                setIsSearchFilterModalOpen(false)
+              }}
+            />
+          </>
+        ) : (
+          <EmptyStateScreen
+            icon="tabler:key-off"
+            message={{
+              id: 'noAPIKey',
+              namespace: 'common.modals',
+              tKey: 'imagePicker'
+            }}
+          />
+        )
+      }
+    </WithQueryData>
   )
 }
 
