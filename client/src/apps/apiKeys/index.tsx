d8a788c105544286e788891563fe48008ae4bebb
Melvin Chia <melvinchia623600@gmail.com>
2025-12-12 15:54:23 +0800

    fix(apiKeys): remove WithOTP component and simplify authentication handling

--- client/src/apps/apiKeys/index.tsx
+++ client/src/apps/apiKeys/index.tsx
@@ -1,5 +1,5 @@
 import forgeAPI from '@/utils/forgeAPI'
-import { WithMasterPassword, WithOTP } from 'lifeforge-ui'
+import { WithMasterPassword } from 'lifeforge-ui'
 import { useAuth } from 'shared'
 
 import ContentContainer from './components/ContentContainer'
@@ -8,28 +8,16 @@ function APIKeys() {
   const { userData } = useAuth()
 
   return (
-    <>
-      <WithOTP
-        controllers={{
-          generateOTP: forgeAPI.user.auth.generateOTP,
-          getChallenge: forgeAPI.apiKeys.auth.getChallenge,
-          verifyOTP: forgeAPI.apiKeys.auth.verifyOTP
-        }}
-      >
-        <WithMasterPassword
-          controllers={{
-            createPassword: forgeAPI.apiKeys.auth.createOrUpdate,
-            getChallenge: forgeAPI.apiKeys.auth.getChallenge,
-            verifyPassword: forgeAPI.apiKeys.auth.verify
-          }}
-          hasMasterPassword={!!userData?.hasAPIKeysMasterPassword}
-        >
-          {masterPassword => (
-            <ContentContainer masterPassword={masterPassword} />
-          )}
-        </WithMasterPassword>
-      </WithOTP>
-    </>
+    <WithMasterPassword
+      controllers={{
+        createPassword: forgeAPI.apiKeys.auth.createOrUpdate,
+        getChallenge: forgeAPI.apiKeys.auth.getChallenge,
+        verifyPassword: forgeAPI.apiKeys.auth.verify
+      }}
+      hasMasterPassword={!!userData?.hasAPIKeysMasterPassword}
+    >
+      {masterPassword => <ContentContainer masterPassword={masterPassword} />}
+    </WithMasterPassword>
   )
 }
 
