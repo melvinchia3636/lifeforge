fe52952ac4ebeb7e39ce18b05350082bd02d0b7e
Melvin Chia <melvinchia623600@gmail.com>
2025-12-13 09:36:10 +0800

    fix(ui): change listbox options container from portal to local absolute positioning due to z-index issue

--- packages/lifeforge-ui/src/components/inputs/ListboxInput/components/ListboxOptions.tsx
+++ packages/lifeforge-ui/src/components/inputs/ListboxInput/components/ListboxOptions.tsx
@@ -13,14 +13,14 @@ function ListboxOptions({
   return (
     <HeadlessListboxOptions
       transition
-      anchor="bottom start"
       className={clsx(
         customWidth ?? 'w-[var(--button-width)]',
-        'divide-bg-200 border-bg-200 dark:border-bg-700 z-9 divide-y overflow-auto rounded-md border',
+        'divide-bg-200 border-bg-200 dark:border-bg-700 z-99 divide-y overflow-auto rounded-md border',
         lighter ? 'bg-bg-50' : 'bg-bg-100',
-        'text-bg-500 text-base shadow-lg transition duration-100 ease-out [--anchor-gap:12px]',
+        'text-bg-500 absolute top-22 left-0 text-base shadow-lg transition duration-100 ease-out [--anchor-gap:12px]',
         'dark:divide-bg-700/50 dark:border-bg-700 dark:bg-bg-800 empty:invisible focus:outline-hidden data-closed:scale-95 data-closed:opacity-0'
       )}
+      portal={false}
     >
       {children}
     </HeadlessListboxOptions>
