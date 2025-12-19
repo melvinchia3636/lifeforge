ad1c1ddad6679953a0c85cd2846af1e03c6b4833
Melvin Chia <melvinchia623600@gmail.com>
2025-12-20 07:11:44 +0800

    fix(ui): update Button import path in ModalHeader component

--- packages/lifeforge-ui/src/components/overlays/modals/core/components/ModalHeader.tsx
+++ packages/lifeforge-ui/src/components/overlays/modals/core/components/ModalHeader.tsx
@@ -5,7 +5,7 @@ import _ from 'lodash'
 import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Button } from '@components/inputs'
+import Button from '../../../../inputs/Button'
 
 function ModalHeader({
   title,
