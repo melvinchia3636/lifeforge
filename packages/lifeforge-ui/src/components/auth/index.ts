7ea84b91990753234c635d7a4f36df69c9523c63
Melvin Chia <melvinchia623600@gmail.com>
2025-12-31 06:39:59 +0800

    feat(ui): restored WithMasterPassword component

--- packages/lifeforge-ui/src/components/auth/index.ts
+++ packages/lifeforge-ui/src/components/auth/index.ts
@@ -4,5 +4,7 @@ export { default as OTPInputBox } from './OTPInputBox'
 
 export { default as ResendOTPButton } from './ResendOTPButton'
 
+export { default as WithMasterPassword } from './WithMasterPassword'
+
 // SSO components
 export * from './sso'
