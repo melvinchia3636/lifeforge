5816ec1082fae4da95e36cfe2756dc12b6852551
Melvin Chia <melvinchia623600@gmail.com>
2025-12-31 06:31:29 +0800

    feat(ui)!: remove deprecated auth related component

--- packages/lifeforge-ui/src/components/auth/index.ts
+++ packages/lifeforge-ui/src/components/auth/index.ts
@@ -1,13 +1,8 @@
 // Authentication & security components
-export { default as WithMasterPassword } from './WithMasterPassword'
 
-export { default as WithOTP } from './WithOTP'
+export { default as OTPInputBox } from './OTPInputBox'
 
-export { default as OTPScreen } from './WithOTP/components/OTPScreen'
-
-export { default as OTPInputBox } from './WithOTP/components/OTPScreen/components/OTPInputBox'
-
-export { default as ResendOTPButton } from './WithOTP/components/OTPScreen/components/ResendOTPButton'
+export { default as ResendOTPButton } from './ResendOTPButton'
 
 // SSO components
 export * from './sso'
