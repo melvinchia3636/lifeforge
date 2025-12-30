3d73e29b1d183382b9bc3231ae671137bed41a11
Melvin Chia <melvinchia623600@gmail.com>
2025-12-30 21:14:31 +0800

    feat(docs): add PR component for displaying pull request links

---
+++ docs/src/contents/04.progress/components/PR.tsx
@@ -0,0 +1,18 @@
+import { Icon } from '@iconify/react'
+
+function PR({ number }: { number: string }) {
+  return (
+    <a
+      className="bg-bg-200 dark:bg-bg-800 hover:bg-bg-300/80 dark:hover:bg-bg-700/50 commit text-bg-600 dark:text-bg-400 mr-2 inline-flex translate-y-1 items-center gap-1.5 rounded-full py-1 pr-3 pl-2 text-sm font-semibold transition-all"
+      href={`https://github.com/lifeforge-app/lifeforge/pull/${number}`}
+      referrerPolicy="no-referrer"
+      rel="noreferrer"
+      target="_blank"
+    >
+      <Icon className="size-5" icon="tabler:git-pull-request" />
+      <code className="text-sm!">#{number}</code>
+    </a>
+  )
+}
+
+export default PR
