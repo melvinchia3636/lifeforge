5f2b426a61a5d9a4f349af0f6904f818d3ff3e38
Melvin Chia <melvinchia623600@gmail.com>
2026-01-10 19:46:53 +0800

    feat(client): add weekOfYear plugin to dayjs for date widget

--- client/src/core/dashboard/widgets/Date.tsx
+++ client/src/core/dashboard/widgets/Date.tsx
@@ -1,11 +1,14 @@
 import clsx from 'clsx'
 import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
 import { useRef } from 'react'
 import { useDivSize, usePersonalization } from 'shared'
 import type { WidgetConfig } from 'shared'
 import tinycolor from 'tinycolor2'
 
 import { arabicToChinese } from '../utils/arabicToChineseNumber'
+
+dayjs.extend(weekOfYear)
 
 export default function DateWidget({
   dimension: { w, h }
