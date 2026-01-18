229110c5effd976d74c8e695792183ef428cd0e2
Melvin Chia <melvinchia623600@gmail.com>
2026-01-18 21:57:10 +0800

    feat(forge): specify user scope in createForge function

--- server/src/lib/user/forge.ts
+++ server/src/lib/user/forge.ts
@@ -2,6 +2,6 @@ import { createForge } from '@lifeforge/server-utils'
 
 import schema from './schema'
 
-const forge = createForge(schema)
+const forge = createForge(schema, 'user')
 
 export default forge
