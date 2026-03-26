bbdd870ad9a680b3fb5de4f71cf8e1c33967ab12
melvinchia3636 <melvinchia623600@gmail.com>
2026-03-26 19:26:52 +0800

    refactor(ui): enhance sidebar cancel button component structure and styles

--- packages/lifeforge-ui/src/components/navigation/sidebar/SidebarItem/components/SidebarCancelButton.tsx
+++ packages/lifeforge-ui/src/components/navigation/sidebar/SidebarItem/components/SidebarCancelButton.tsx
@@ -1,20 +1,36 @@
 import { Icon } from '@iconify/react'
 import { useModuleSidebarState } from 'shared'
 
+import { Box, Text } from '@components/primitives'
+
 function SidebarCancelButton({ onClick }: { onClick: () => void }) {
   const { setIsSidebarOpen } = useModuleSidebarState()
 
   return (
-    <button
-      className="text-bg-500 hover:bg-bg-200/50 hover:text-bg-800 dark:hover:bg-bg-700/50 dark:hover:text-bg-50 z-9999 overscroll-contain rounded-md p-2"
-      onClick={e => {
-        e.stopPropagation()
-        onClick()
-        setIsSidebarOpen(false)
+    <Box
+      asChild
+      bg={{
+        hover: 'bg-200',
+        darkHover: 'bg-700'
+      }}
+      p="sm"
+      rounded="md"
+      style={{
+        transition: 'all 0.2s'
       }}
     >
-      <Icon className="size-5" icon="tabler:x" />
-    </button>
+      <Text
+        as="button"
+        color={{ base: 'bg-500', hover: 'bg-800', darkHover: 'bg-50' }}
+        onClick={e => {
+          e.stopPropagation()
+          onClick()
+          setIsSidebarOpen(false)
+        }}
+      >
+        <Icon className="size-5" icon="tabler:x" />
+      </Text>
+    </Box>
   )
 }
 
