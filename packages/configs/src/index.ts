a360a769fd1082643fc96d2e0adefc2b262414ce
melvinchia3636 <melvinchia623600@gmail.com>
2026-06-08 20:12:46 +0800

    refactor: improve folder structure for @lifeforge/configs

--- packages/configs/src/index.ts
+++ packages/configs/src/index.ts
@@ -1,3 +1,7 @@
-export { defineModuleConfig } from './module-vite.config'
-export { defineClientConfig, SHARED_PACKAGES } from './client-vite.config'
-export { customResolver } from './resolver'
+export { defineModuleConfig } from './vite/module-vite.config'
+
+export { defineClientConfig } from './vite/client-vite.config'
+
+export { SHARED_PACKAGES } from './constants/shared-packages'
+
+export { aliasResolver as customResolver } from './resolvers/alias-resolver'
