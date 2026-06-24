020decb016448ca1e6df374d908a951df2547ad1
melvinchia3636 <melvinchia623600@gmail.com>
2026-06-25 01:03:14 +0800

    fix: fix locales preloading

---
+++ apps/web/src/providers/features/I18nCommonNameSpacePreloadProvider.tsx
@@ -0,0 +1,42 @@
+import type React from 'react'
+import { getI18n } from 'react-i18next'
+
+function I18nCommonNameSpacePreloadProvider({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const i18n = getI18n()
+
+  i18n.on('loaded', () => {
+    const loadedLangs = i18n.languages || ['en']
+
+    for (const lang of loadedLangs) {
+      const commonBundle = i18n.getResourceBundle(lang, 'common') as
+        | Record<string, unknown>
+        | undefined
+
+      if (commonBundle) {
+        const subnamespaces = Object.keys(commonBundle)
+
+        for (const sub of subnamespaces) {
+          if (i18n.hasResourceBundle(lang, `common.${sub}`)) {
+            return
+          }
+
+          i18n.addResourceBundle(
+            lang,
+            `common.${sub}`,
+            (commonBundle[sub] as Record<string, unknown>) || {},
+            true,
+            true
+          )
+        }
+      }
+    }
+  })
+
+  return children
+}
+
+export default I18nCommonNameSpacePreloadProvider
