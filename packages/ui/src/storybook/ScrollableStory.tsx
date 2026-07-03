84377d55f7f4513d2f9ebf09706b2c0bea2828ae
melvinchia3636 <melvinchia623600@gmail.com>
2026-07-03 18:03:25 +0800

    fix(ui): fix story container height issue in storybook docs

--- packages/ui/src/storybook/ScrollableStory.tsx
+++ packages/ui/src/storybook/ScrollableStory.tsx
@@ -3,8 +3,8 @@ import { Scrollbar } from '@/components/utilities'
 
 export function ScrollableStory({ children }: { children: React.ReactNode }) {
   return (
-    <Box height="100%" minHeight="0" py="3xl" width="100%">
-      <Scrollbar minHeight="0" width="100%">
+    <Box asChild height="100%" minHeight="0" py="3xl" width="100%">
+      <Scrollbar>
         <Flex direction="column" gap="md">
           {children}
         </Flex>
